b3f6b8007018997f6cc0b391406b48078af2d40e
Will Scott <willscott@gmail.com>
2012-12-27 17:24:07 -0800

    Use the tcp-server class correctly.

--- launch.js
+++ launch.js
@@ -1,14 +1,16 @@
 var tcpServer;
 
-function handleClientEvent(d) {
-	chrome.socket.read(d.socketId, null, function(data) {
-		console.log(data);		
-	});
+function handleClientData(data) {
+	console.log(data);
+}
+
+function handleClientAccept(d) {
+	d.addDataReceivedListener(handleClientData);
 };
 
 function startup() {
 	tcpServer = new TcpServer("127.0.0.1", 8123);
-	tcpServer.listen(handleClientEvent);
+	tcpServer.listen(handleClientAccept);
 }
 
 
