00daa10839656c6e16220b75247a0c39953e04db
Will Scott <willscott@gmail.com>
2013-01-03 18:43:57 -0800

    Initial drop of xmpp browser.  Fails on DNS currently, but might be massagable into something that loads

--- launch.js
+++ launch.js
@@ -9,6 +9,21 @@ function handleClientAccept(client) {
 function startup() {
 	tcpServer = new TcpServer("127.0.0.1", 8123);
 	tcpServer.listen(handleClientAccept);
+
+	var un = document.createElement("input");
+	un.placeholder = "username";
+	document.body.appendChild(document.createElement("br"));
+	document.body.appendChild(un);
+	document.body.appendChild(document.createElement("br"));
+	var pw = document.createElement("input");
+	pw.type = "password";
+	pw.placeholder = "password";
+	document.body.appendChild(pw);
+	var b = document.createElement("button");
+	b.innerHTML="Connect";
+	b.addEventListener('click', function() {
+	}, true);
+	document.body.appendChild(b);
 }
 
 window.addEventListener('load', startup, true);
