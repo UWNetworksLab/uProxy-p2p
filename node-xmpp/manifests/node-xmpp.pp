9d0030da8ddf79a57278d9f45cd97a0e84fdbb90
Will Scott <willscott@gmail.com>
2013-01-13 23:46:41 -0800

    Initial stab at setting up a vagrant VM to allow automatic compilation of the node-xmpp script.

---
+++ node-xmpp/manifests/node-xmpp.pp
@@ -0,0 +1,19 @@
+include nodejs
+
+Exec {
+    user => "vagrant",
+    cwd => "/home/vagrant",
+    logoutput => on_failure,
+    environment => ["PWD=/home/vagrant", "HOME=/home/vagrant"],
+    timeout => 0
+}
+
+class { 'nodejs':
+  node_ver => 'v0.8.4'
+}
+
+package { 'browserify':
+  ensure => present,
+  provider => 'npm',
+  require => Class['nodejs']
+}
---
+++ node-xmpp/manifests/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Directory in which librarian-puppet should manage its modules directory
+PUPPET_DIR=/home/vagrant/.puppet/modules
+
+# NB: librarian-puppet might need git installed. If it is not already installed
+# in your basebox, this will manually install it at this point using apt or yum
+GIT=/usr/bin/git
+APT_GET=/usr/bin/apt-get
+YUM=/usr/sbin/yum
+if [ ! -x $GIT ]; then
+    if [ -x $YUM ]; then
+        yum -q -y install git
+    elif [ -x $APT_GET ]; then
+        apt-get -q -y install git
+    else
+        echo "No package installer available. You may need to install git manually."
+    fi
+fi
+
+mkdir -p $PUPPET_DIR
+cp /vagrant/puppet/Puppetfile $PUPPET_DIR
+
+if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
+  gem install librarian-puppet
+  cd $PUPPET_DIR && librarian-puppet install --clean
+else
+  cd $PUPPET_DIR && librarian-puppet update
+fi
+
