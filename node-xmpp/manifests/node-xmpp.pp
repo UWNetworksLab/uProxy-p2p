48ecf7ffbb83c4cb7f5d0a5844de0063b17571df
Will Scott <willscott@gmail.com>
2013-01-16 10:59:19 -0800

    Verbose flag to browserify makes it return success exit code & work.

--- node-xmpp/manifests/node-xmpp.pp
+++ node-xmpp/manifests/node-xmpp.pp
@@ -46,7 +46,7 @@ class node-xmpp {
     require => Exec['npm-browserify']
   }
   
-  exec { "/usr/local/bin/browserify -p browserify-override -o node-xmpp-browser.js lib/node-xmpp-browserify.js":
+  exec { "/usr/local/bin/browserify -p browserify-override -o node-xmpp-browser.js -v lib/node-xmpp-browserify.js":
     alias => "compile",
     cwd => "/home/vagrant/node_modules/node-xmpp",
     require => Exec['rules']
