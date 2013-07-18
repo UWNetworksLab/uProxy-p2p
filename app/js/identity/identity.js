61ea621c1278cae4a9ffb9f3f6075796eae25d12
Will Scott <willscott@gmail.com>
2013-07-17 18:37:06 -0700

    update identity module to ask for a view

--- app/js/identity/identity.js
+++ app/js/identity/identity.js
@@ -9,20 +9,46 @@ function loadSupport() {
 
   window = {};
 
-  importScripts('/node-xmpp-browser.js');
-  importScripts('/xmppDaemon.js');
+  importScripts('node-xmpp-browser.js');
+  importScripts('xmppDaemon.js');
 }
 
 function IdentityProvider() {
   if (typeof XmppDaemon == "undefined") {
     loadSupport();
   }
+  this.credentials = null;
+  this.client = null;
 }
 
 // Get my id.
 IdentityProvider.prototype.get = function(continuation) {
-  continuation({"id": "me"});
-}
+  if (!this.credentials) {
+    var view = freedom['core.view']();
+    var promise = view.open({
+      file: "view.html"
+    });
+    promise.done(function() {
+      view.show();
+    });
+
+    var retVal = false;
+    view.on('close', function() {
+      if (retVal === false) {
+        continuation({});
+      }
+    });
+
+    view.on('message', function(identity) {
+      view.close();
+      var sanitized_email = identity.email.toLowerCase().trim();
+      this.credentials = sanitized_email;
+      continuation({me: this.credentials});
+    }.bind(this));
+  } else {
+    continuation({me: this.credentials});
+  }
+};
 
 // Send a message to someone.
 IdentityProvider.prototype.send = function(to, msg, continuation) {
