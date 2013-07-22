399fecd5f737f3279baa13e730cbd1427cb6fe17
Paul Ellenbogen <epaul9@cs.washington.edu>
2013-07-22 12:47:32 -0700

    Change toolbar initialization in FF.

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,16 +1,20 @@
 var self = require("sdk/self");
 
-// create toolbarbutton
-var tbb = require("toolbarbutton").ToolbarButton({
-  id: "UProxyItem",
-  label: "UProxy",
-  image: self.data.url("uproxy-19.png"),
-  onCommand: function () {
-    //tbb.destroy(); // kills the toolbar button
-  }
-});
+var initToolbar = function() {
+  // create toolbarbutton
+  var tbb = require("toolbarbutton").ToolbarButton({
+    id: "UProxyItem",
+    label: "UProxy",
+    image: self.data.url("uproxy-19.png"),
+    onCommand: function () {
+      //tbb.destroy(); // kills the toolbar button
+    }
+  });
  
-tbb.moveTo({
-  toolbarID: "nav-bar",
-  forceMove: false // only move from palette
-});
+  tbb.moveTo({
+    toolbarID: "nav-bar",
+    forceMove: false // only move from palette
+  });
+};
+
+initToolbar();
