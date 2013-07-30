e7ecc546ef36a4b2cf9c15a6b960c4d50099ebaf
Paul Ellenbogen <epaul9@cs.washington.edu>
2013-07-30 13:15:02 -0700

    Change filter for popup.

---
+++ firefox/data/js/popup_init.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var freedom = new freedomShim("toolbar");
+
+var OAUTH_CONFIG = {
+  "client_id": "814927071113-ri9amn1jl73c7rbh2dvif2g78fok8vs9.apps.googleusercontent.com",
+  "client_secret": "JxrMEKHEk9ELTTSPgZ8IfZu-",
+  "api_scope": "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/googletalk"
+};
+
+addon.port.emit("show");
+angular.module('UProxyChromeExtension', []);
+addon.port.on("l10n", function(l10n) {
+  console.log("Initializing popup");
+  app(l10n);
+  popup();
+});
+
+
