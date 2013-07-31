edf70b652df1a03b5d36e20e54d28a7415c2fa49
Will Scott <willscott@gmail.com>
2013-07-31 14:24:43 -0700

    note for how to debug workers

--- chrome/app/js/plumbing.js
+++ chrome/app/js/plumbing.js
@@ -8,6 +8,8 @@ window.freedomcfg = function(register) {
 
 var script = document.createElement('script');
 script.setAttribute('data-manifest', 'submodules/uproxy-common/uproxy.json');
+// Uncomment for clearer but less portable module error messages.
+//script.textContent = '{"nonblob": true}';
 script.src = 'submodules/uproxy-common/submodules/freedom/freedom.js';
 
 document.head.appendChild(script);
