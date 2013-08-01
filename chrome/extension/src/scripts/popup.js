29279fdea2f47784048753309e205785e89341ea
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-07-31 18:12:45 -0700

    Added chatting Fixed bugs with plumbing Fixed oauth credential ordering bug

--- chrome/extension/src/scripts/popup.js
+++ chrome/extension/src/scripts/popup.js
@@ -49,7 +49,17 @@ angular.module('UProxyChromeExtension')
   ])
   .controller('DebugCtrl', ['$scope', 'freedom', function($scope, freedom) {
     $scope.sendMsg = function () {
-      freedom.emit('send-message', {to: '', message: $scope.msg});
+      if ($scope.chatbuddy && $scope.chatbuddy.devices) {
+        var devices = $scope.chatbuddy.devices;
+        for (var i = 0; i < devices.length; i++) {
+          if (devices[i].status == 'online') {
+            freedom.emit('send-message', {to: devices[i].deviceId, message: $scope.msg})
+            $scope.msg = '';
+            return;
+          }
+        }
+      }
+      freedom.emit('echo', "No available recipient: " + $scope.msg);
       $scope.msg = '';
     };
   }]);
