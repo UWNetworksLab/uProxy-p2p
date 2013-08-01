b08f8cbb3b63ea5b41317c1a19eca7dd6cfa22cf
Will Scott <willscott@gmail.com>
2013-08-01 15:51:48 -0700

    fix some grunt warnings

--- chrome/extension/src/scripts/popup.js
+++ chrome/extension/src/scripts/popup.js
@@ -2,21 +2,20 @@
 
 angular.module('UProxyChromeExtension')
   .controller('MainCtrl', [function () {
-    }
-  ])
+}])
   .controller('DebugCtrl', ['$scope', 'freedom', function($scope, freedom) {
     $scope.sendMsg = function () {
       if ($scope.chatbuddy && $scope.chatbuddy.devices) {
         var devices = $scope.chatbuddy.devices;
         for (var i = 0; i < devices.length; i++) {
-          if (devices[i].status == 'online') {
-            freedom.emit('send-message', {to: devices[i].deviceId, message: $scope.msg})
+          if (devices[i].status === 'online') {
+            freedom.emit('send-message', {to: devices[i].deviceId, message: $scope.msg});
             $scope.msg = '';
             return;
           }
         }
       }
-      freedom.emit('echo', "No available recipient: " + $scope.msg);
+      freedom.emit('echo', 'No available recipient: ' + $scope.msg);
       $scope.msg = '';
     };
   }]);
