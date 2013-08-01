2f456eae00733cc8649b96d18816791a7ead411e
_pants <_pants@getlantern.org>
2013-07-31 20:46:45 -0400

    code + style improvements, add option button
    move application-wide logic from popup.js into app.js

--- chrome/extension/src/scripts/popup.js
+++ chrome/extension/src/scripts/popup.js
@@ -1,50 +1,7 @@
 'use strict';
 
-// TODO: client secret should not be public.
-var OAUTH_CONFIG = {
-  'client_id': '814927071113-ri9amn1jl73c7rbh2dvif2g78fok8vs9.apps.googleusercontent.com',
-  'client_secret': 'JxrMEKHEk9ELTTSPgZ8IfZu-',
-  'api_scope': 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/googletalk'
-};
-
 angular.module('UProxyChromeExtension')
-  .constant('googleAuth', new OAuth2('google', OAUTH_CONFIG))
-  .constant('GOOG_PROFILE_URL', 'https://www.googleapis.com/oauth2/v1/userinfo')
-  .controller('MainCtrl', [
-    '$http',
-    '$rootScope',
-    'GOOG_PROFILE_URL',
-    'bg',
-    'freedom',
-    'googleAuth',
-    'model',
-    function ($http, $rootScope, GOOG_PROFILE_URL, bg, freedom, googleAuth, model) {
-      $rootScope.model = model;
-
-      // XXX move to run block once https://github.com/angular/angular.js/issues/2963 is fixed
-      $rootScope.$watch('model.options.mode', function (mode) {
-        if (!mode) return;
-        $rootScope.inGiveMode = mode === 'give';
-        $rootScope.inGetMode = mode === 'get';
-      });
-
-      $rootScope.sendCredentials = function () {
-        if (!model.accessToken || !model.email) {
-          googleAuth.authorize(function () {
-            var accessToken = googleAuth.getAccessToken();
-            $http({method: 'GET', url: GOOG_PROFILE_URL, params: {'oauth_token': accessToken}}).then(
-              function getProfileSuccessHandler(resp) {
-                var email = resp.data.email;
-                freedom.emit('gtalk-credentials', {email: email, token: accessToken});
-              },
-              function getProfileFailureHandler(resp) {
-                console.error('request for', GOOG_PROFILE_URL, 'failed:', resp);
-              });
-          });
-        } else {
-          freedom.emit('gtalk-credentials', {email: model.email, token: model.accessToken});
-        }
-      };
+  .controller('MainCtrl', [function () {
     }
   ])
   .controller('DebugCtrl', ['$scope', 'freedom', function($scope, freedom) {
