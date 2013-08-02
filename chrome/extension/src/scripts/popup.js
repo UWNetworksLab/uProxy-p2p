9b26cede35c8e3d0b13266283d36360dc2a8eb99
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-08-02 10:54:06 -0700

    Added invitation process and UProxy-to-UProxy chatting

--- chrome/extension/src/scripts/popup.js
+++ chrome/extension/src/scripts/popup.js
@@ -6,17 +6,25 @@ angular.module('UProxyChromeExtension')
   ])
   .controller('DebugCtrl', ['$scope', 'freedom', function($scope, freedom) {
     $scope.sendMsg = function () {
+      console.log($scope.chatbuddy);
       if ($scope.chatbuddy && $scope.chatbuddy.devices) {
+      //Look for a messageable device
         var devices = $scope.chatbuddy.devices;
         for (var i = 0; i < devices.length; i++) {
-          if (devices[i].status == 'online') {
-            freedom.emit('send-message', {to: devices[i].deviceId, message: $scope.msg})
+          if (devices[i].status == 'messageable') {
+            freedom.emit('send-message', {to: devices[i].deviceId, message: $scope.msg});
             $scope.msg = '';
+            //TODO: Currently only sending to first UProxy instance
             return;
           }
         }
+      } 
+      if ($scope.chatbuddy && $scope.chatbuddy.userId) {
+        //Invite friend to UProxy
+        freedom.emit('echo', "Invitation send to "+$scope.chatbuddy.userId);
+        freedom.emit('invite-friend', $scope.chatbuddy.userId);
+      } else {
+        freedom.emit('echo', "Please select a friend");
       }
-      freedom.emit('echo', "No available recipient: " + $scope.msg);
-      $scope.msg = '';
     };
   }]);
