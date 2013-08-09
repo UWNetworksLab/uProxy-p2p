8e7750401917dd817ce98c6ef85e1376f371c29d
_pants <_pants@getlantern.org>
2013-08-09 16:22:40 -0400

    hook up more messaging
    accompanies UWNetworksLab/uproxy-common@a7b6874

--- chrome/extension/src/scripts/popup.js
+++ chrome/extension/src/scripts/popup.js
@@ -3,27 +3,21 @@
 angular.module('UProxyChromeExtension')
   .controller('MainCtrl', [function () {
 }])
-  .controller('DebugCtrl', ['$scope', 'freedom', function($scope, freedom) {
-    $scope.sendMsg = function () {
-      console.log($scope.chatbuddy);
-      if ($scope.chatbuddy && $scope.chatbuddy.clients) {
-      //Look for a messageable device
-        var clients = $scope.chatbuddy.clients;
-        for (var i in clients) {
-          if (clients.hasOwnProperty(i) && clients[i].status == 'messageable') {
-            freedom.emit('send-message', {to: clients[i].clientId, message: $scope.msg});
-            $scope.msg = '';
-            //TODO: Currently only sending to first UProxy instance
-            return;
-          }
-        }
+  .controller('DebugCtrl', ['$filter', '$scope', 'freedom', 'model', function ($filter, $scope, freedom, model) {
+    var messageable = $filter('messageable');
+
+    $scope.submitChat = function () {
+      var contact = model.roster[$scope.userId];
+      if (!contact) {
+        console.error('not on roster:', $scope.userId);
+        return;
       }
-      if ($scope.chatbuddy && $scope.chatbuddy.userId) {
-        //Invite friend to UProxy
-        freedom.emit('echo', "Invitation send to "+$scope.chatbuddy.userId);
-        freedom.emit('invite-friend', $scope.chatbuddy.userId);
+      if (messageable(contact)) {
+        // only sends to UProxy clients
+        $scope.sendMessage(contact, $scope.msg);
       } else {
-        freedom.emit('echo', "Please select a friend");
+        freedom.emit('send-message', {to: contact.userId, message: $scope.msg});
       }
+      $scope.msg = '';
     };
   }]);
