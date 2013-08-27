5f75dcbdc9172ec3208774a69c894fbbf609fda8
Paul Ellenbogen <epaul9@cs.washington.edu>
2013-08-26 17:28:50 -0700

    Update Chrome socket implementation.

--- chrome/app/js/socket_chrome.js
+++ chrome/app/js/socket_chrome.js
@@ -19,41 +19,44 @@ Socket_chrome.prototype.connect = function(socketId, hostname, port, callback) {
   chrome.socket.connect(socketId, hostname, port, function connectCallback(result) {
     callback(result);
     var dataRead = function (readInfo) {
-      if (readInfo.resultCode === 0) {
+      if (readInfo.resultCode > 0) {
 	this.dispatchEvent('onData', {socketId: socketId, data: readinfo.data});
 	readLoop();
-      }  else if (readInfo.resultCode !== -15) {
+      } else if (readInfo.resultcode === 0 && readInfo.resultCode === -15) {
 	// The result code is -15 if the connection was closed, which can
 	// can happen in usual program flow, so we will not log the error.
-	console.error('Error occured when reading from socket ' + socketId);
+	this.dispatchEvent('onDisconnect', {socketId: socketId});
+      } else {
+	console.error('Error with result code ' + readInfo.resultCode +
+		      ' occured when reading from socket ' + socketId);
       };
-    };
+    }.bind(this);
     var readLoop = function () {
-      chrome.socket.read(socketId, dataRead);
+      chrome.socket.read(socketId, null, dataRead);
     };
     readLoop();
-  });
+  }.bind(this));
 };
 
 Socket_chrome.prototype.listen = function(socketId, address, port, callback) {
-  chrome.socket.listen(socketId, address, port, function listenCallback(result) {
+  chrome.socket.listen(socketId, address, port, null, function listenCallback(result) {
     callback(result);
     if (result === 0) {
       var acceptCallback = function (acceptInfo) {
-	if (acceptInfo.resutCode === 0) {
+	if (acceptInfo.resultCode === 0) {
 	  this.dispatchEvent('onConnection', [socketId, acceptInfo.socketId]);
 	  acceptLoop();
-	} else if (readInfo.resultCode !== -15) {
+	} else if (acceptInfo.resultCode !== -15) {
 	  console.error('Error ' + acceptInfo.resultCode
 			+ ' while trying to accept connection on socket ' + socketId);
 	}
-      };
+      }.bind(this);
       var acceptLoop = function() {
 	chrome.socket.accept(socketId, acceptCallback);
       };
       acceptLoop();
     }
-  });
+  }.bind(this));
 };
 
 Socket_chrome.prototype.destroy = function(socketId, continuation) {
