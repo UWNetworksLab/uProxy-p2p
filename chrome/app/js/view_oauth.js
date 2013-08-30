15a9677a761bd541cbdcef99874d6259ed1b567e
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-08-29 22:35:32 -0700

    updating view_oauth for manual identity provider

--- chrome/app/js/view_oauth.js
+++ chrome/app/js/view_oauth.js
@@ -1,5 +1,7 @@
 var View_oauth = function(channel) {
   this.channel = channel;
+  this.manualdialog = null;
+  this.manualMsgQueue = [];
 };
 
 View_oauth.prototype.open = function(args, continuation) {
@@ -32,6 +34,27 @@ View_oauth.prototype.open = function(args, continuation) {
       xhr.open('get', 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token, true);
       xhr.send();
     }).bind(this));
+  } else if (file == 'manualdialog.html') {
+    chrome.app.window.create(
+      'submodules/uproxy-common/identity/manual/manualdialog.html',
+      {
+        id: 'manualdialog',
+        minWidth: 600,
+        minHeight: 400,
+        maxWidth: 600,
+        maxHeight: 400
+      },
+      (function(child_win) {
+        this.manualdialog = child_win;
+        for (var i=0; i<this.manualMsgQueue.length; i++) {
+          this.manualdialog.postMessage(this.manualMsgQueue[i]);
+        }
+        this.manualMsgQueue = [];
+        this.manualdialog.onClosed(function() {
+          this.manualdialog = null;
+        });
+      }).bind(this);
+    ); 
   } else if (file == "oauth.html") {
     //@TODO{ryscheng} Get rid of this block
     if (!this.listening) {
@@ -54,9 +77,14 @@ View_oauth.prototype.show = function(continuation) {
 }
 
 View_oauth.prototype.postMessage = function(args, continuation) {
+  if (this.manualdialog) {
+    this.manualdialog.postMessage(args);
+  } else {
+    this.manualMsgQueue.push(args);
+  }
   continuation();
 }
 
-View_oauth.prototype.close = function() {
-  this.callback = null;
+View_oauth.prototype.close = function(continuation) {
+  continuation();
 }
