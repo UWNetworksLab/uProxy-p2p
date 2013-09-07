901efa04c27aa69e09d88594c85b7f400aeff4f4
Inf Mally <infmally@gmail.com>
2013-09-06 22:03:16 -0400

    fixed broken lodash, incorrect karma setup, and removed not relevant bower_components

--- chrome/extension/src/bower_components/angular-lodash/test/apiSpec.js
+++
@@ -1,8 +0,0 @@
-describe('angular-lodash: API', function() {
-  beforeEach(module('_'));
-  _.each(_.functions(_), function(fnName) {
-    it(fnName + " should adapt to lodash's "+ fnName, inject(function($rootScope) {
-      expect($rootScope[fnName].toString()).toBe(_[fnName].toString());
-    }));
-  });
-});
