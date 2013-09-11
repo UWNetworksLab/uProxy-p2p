e451ed82abb5685bc94cbabba7e8a21cbedde174
Paul Ellenbogen <epaul9@cs.washington.edu>
2013-09-11 18:40:43 -0400

    Get popup.html working in Firefox again.

--- firefox/data/js/app.js
+++ firefox/data/js/app.js
@@ -1,15 +1,21 @@
+// The angular module and defintions for state associated with the underlying
+// app. This code defines
 'use strict';
 
 // TODO: client secret should not be public.
+/**
 var OAUTH_CONFIG = {
   'client_id': '814927071113-ri9amn1jl73c7rbh2dvif2g78fok8vs9.apps.googleusercontent.com',
   'client_secret': 'JxrMEKHEk9ELTTSPgZ8IfZu-',
   'api_scope': 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/googletalk'
 };
+**/
+
+var bg = chrome.extension.getBackgroundPage();
 
 angular.module('UProxyChromeExtension', ['angular-lodash'])
-  .constant('googleAuth', new OAuth2('google', OAUTH_CONFIG))
-  .constant('GOOG_PROFILE_URL', 'https://www.googleapis.com/oauth2/v1/userinfo')
+  //.constant('googleAuth', new OAuth2('google', OAUTH_CONFIG))
+  //.constant('GOOG_PROFILE_URL', 'https://www.googleapis.com/oauth2/v1/userinfo')
   // can remove once https://github.com/angular/angular.js/issues/2963 is fixed:
   .config(function ($provide) {
     $provide.decorator('$sniffer', ['$delegate', function ($sniffer) {
@@ -40,22 +46,28 @@ angular.module('UProxyChromeExtension', ['angular-lodash'])
     };
   })
   .constant('bg', chrome.extension.getBackgroundPage())
+  .constant('chrome', chrome)
   .constant('freedom', chrome.extension.getBackgroundPage().freedom)
-  .constant('model', {}) // application state. determined by backend (packaged app)
+  // application state. determined by backend (packaged app)
+  .constant('model', {})
+  // Run gets called every time the popup is openned. This initialises the main
+  // extension UI and makes sure it is in sync with the app.
   .run([
     '$filter',
     '$http',
     '$rootScope',
-    'GOOG_PROFILE_URL',
+    //'GOOG_PROFILE_URL',
     'bg',
+    'chrome',
     'freedom',
-    'googleAuth',
+    //'googleAuth',
     'model',
-    function($filter, $http, $rootScope, GOOG_PROFILE_URL, bg, freedom, googleAuth, model) {
+    function($filter, $http, $rootScope, /*GOOG_PROFILE_URL,*/ bg, chrome, freedom, /*googleAuth,*/ model) {
       var filter = $filter('filter'),
           messageable = $filter('messageable'),
           onlineNotMessageable = $filter('onlineNotMessageable');
 
+      $rootScope.extensionID = chrome.runtime.id;
       $rootScope.model = model;
 
       $rootScope.$watch('model.roster', function (roster) {
@@ -79,25 +91,29 @@ angular.module('UProxyChromeExtension', ['angular-lodash'])
         });
       }
 
-      $rootScope.freedomEmit = function (msgName, data) {
-        freedom.emit(msgName, data);
-      };
+      $rootScope.resetState = function (msgName, data) {
+        localStorage.clear();
+        freedom.emit('reset', null);
+      }
 
       $rootScope.sendMessage = function (contact, msg) {
         // XXX freedom.emit('send-message', {to: contact.userId, msg})
         //     gets intercepted by non-freedom clients and is not received by uproxy clients
-        _(contact.clients).filter({status: 'messageable'}).each(function (client) {
+        _(contact.clients).filter({status: 'messageable'}).each(
+            function (client) {
           freedom.emit('send-message', {
             to: client.clientId,
             toUserId: contact.userId,
             message: msg});
         });
-      };
+      }
 
       $rootScope.changeOption = function (key, value) {
         freedom.emit('change-option', {key: key, value: value});
-      };
+      }
 
+      var clearedAndRetried = false;
+      /**
       $rootScope.authGoog = function () {
         googleAuth.authorize(function () {
           var accessToken = googleAuth.getAccessToken();
@@ -105,28 +121,75 @@ angular.module('UProxyChromeExtension', ['angular-lodash'])
             function getProfileSuccessHandler(resp) {
               var email = resp.data.email;
               freedom.emit('goog-credentials', {email: email, token: accessToken});
+              clearedAndRetried = false;
             },
             function getProfileFailureHandler(resp) {
-              console.error('request for', GOOG_PROFILE_URL, 'failed:', resp);
+              if (resp.status === 401) {
+                console.debug('request for', GOOG_PROFILE_URL, 'yielded 401 response');
+                if (clearedAndRetried) {
+                  console.debug('already cleared access token and tried again');
+                } else {
+                  console.debug('clearing access token and trying again');
+                  clearedAndRetried = true;
+                  googleAuth.clearAccessToken();
+                  //$rootScope.authGoog();
+                }
+              } else {
+                console.debug('request for', GOOG_PROFILE_URL, 'failed:', resp);
+              }
             });
         });
-      };
+      }
+      **/
 
-      bg.clearPopupListeners();
-      freedom.emit('open-popup');
-
-      var JSONPatch = jsonpatch.JSONPatch;
-      bg.addPopupListener('state-change', function (patch) {
-        console.debug('got state change:', patch);
+      // TODO(): change the icon/text shown in the broswer action, and maybe
+      // add a butter-bar. This is important for when someone is proxying
+      // through you. See:
+      //   * chrome.browserAction.setBadgeText(...)
+      //   * chrome.browserAction.setIcon
+      //   * https://developer.chrome.com/extensions/desktop_notifications.html
+      $rootScope.onStateChange = function (patch) {
         $rootScope.$apply(function () {
+          console.info('got state change:', patch);
+          $rootScope.connectedToApp = true;
           // XXX jsonpatch can't mutate root object https://github.com/dharmafly/jsonpatch.js/issues/10
           if (patch[0].path === '') {
             angular.copy(patch[0].value, model);
           } else {
-            patch = new JSONPatch(patch, true); // mutate = true
-            patch.apply(model);
+            if (_.isEmpty(model)) {
+              console.info('model init patch not yet received, ignoring non init patch:', patch);
+            } else {
+              patch = new jsonpatch.JSONPatch(patch, true); // mutate = true
+              patch.apply(model);
+            }
           }
         });
-      });
-    }
+      }
+
+      // Can be called from nonUI threads (i.e. without a defined window
+      // object.).
+      $rootScope.startUI = function() {
+        // call these in the Angular scope so that window is defined.
+        $rootScope.$apply(function() {
+          freedom.onConnected.removeListener($rootScope.startUI);
+          bg.onFreedomStateChange.addListener($rootScope.onStateChange);
+          window.onunload = function() {
+            bg.onFreedomStateChange.removeListener($rootScope.onStateChange);
+          };
+          freedom.emit('open-popup');
+          //$rootScope.authGoog();
+          $rootScope.connectedToApp = true;
+        });
+      }
+
+      $rootScope.connectedToApp = false;
+
+      if(freedom.connected) {
+        $rootScope.connectedToApp = true;
+        $rootScope.startUI();
+      } else {
+        freedom.onConnected.addListener($rootScope.startUI);
+        freedom.connect();
+      }
+    }  // run function
   ]);
