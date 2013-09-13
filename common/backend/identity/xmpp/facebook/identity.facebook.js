871114aa0bbde72b68b11944f67d944dfc2d8693
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-09-13 12:07:18 -0700

    Updating Facebook identity to newest xmpp client

--- common/backend/identity/xmpp/facebook/identity.facebook.js
+++ common/backend/identity/xmpp/facebook/identity.facebook.js
@@ -1,4 +1,11 @@
 var NETWORK_ID = 'facebook';
 var NETWORK_NAME = 'Facebook Identity Provider';
-
-
+var CONNECT_OPTS = function(id, token) {
+  return {
+    xmlns:'jabber:client',
+    jid: id,
+    access_token: token,
+    api_key: '161927677344933',
+    host: "chat.facebook.com"
+  };
+};
