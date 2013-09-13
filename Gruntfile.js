a6e8afc5528230140bd7281f1c5002e53ae03783
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-09-13 08:29:42 -0700

    Removing old files, adding grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,36 @@
 path = require("path");
-var sources = ['common/backend/util.js'];
-var testSources = sources.slice(0);
+var chrome_app_files = [
+  {src: 'common/ui/icons/**', dest: 'chrome/app/'},
+  {src: [
+    'common/backend/**', 
+    '!common/backend/spec/**', 
+    '!common/backend/identity/xmpp/node-xmpp/**'
+    ], dest: 'chrome/app/'},
+  {src: 'common/freedom/freedom.js', dest: 'chrome/app/'}
+];
+var chrome_ext_files = [
+  {src:'common/ui/**', dest:'chrome/extension/src/'},
+  {src:'common/bower_components/**', dest:'chrome/extension/src/'}
+];
+var firefox_files = [];
+var sources = ['common/backend/util.js']; //TODO fix
+var testSources = sources.slice(0);       //TODO fix
 
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     copy: {
-      chrome_app: {
-        files: [
-          {src: 'common/ui/icons/**', dest: 'chrome/app/'},
-          {src: 'common/backend/client/**', dest: 'chrome/app/'},
-          {src: 'common/backend/identity/**', dest: 'chrome/app/'},
-          {src: 'common/backend/server/**', dest: 'chrome/app/'},
-          {src: 'common/backend/storage/**', dest: 'chrome/app/'},
-          {src: 'common/backend/transport/**', dest: 'chrome/app/'},
-          {src: 'common/backend/*.js', dest: 'chrome/app/'},
-          {src: 'common/backend/*.json', dest: 'chrome/app/'},
-          {src: 'common/freedom/freedom.js', dest: 'chrome/app/'}
-        ]
-      },
-      chrome_ext: {
-        files: [
-	        {src:'common/ui/**', dest:'chrome/extension/src/'},
-	        {src:'common/bower_components/**', dest:'chrome/extension/src/'}
-        ]
-		  },
-      firefox: {}
+      chrome_app: {files: chrome_app_files},
+      chrome_ext: {files: chrome_ext_files},
+      firefox: {files: firefox_files},
+      watch: {files: []},
+    },
+    watch: {
+      all: {
+        files: ['common/**'],
+        task: ['copy:watch'],
+        options: {spawn: false}
+      }
     },
     shell: {
       git_submodule: {
@@ -65,7 +70,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-shell');
+
+  grunt.event.on('watch', function(action, filepath, target) {
+    //grunt.config(['copy', 'watch', 'files'], );
+  });
 
   grunt.registerTask('setup', [
     'shell:git_submodule', 
