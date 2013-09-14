c6d0c3fe37322b6e3ab48cdcb5b31475d3423c37
Inf Mally <infmally@gmail.com>
2013-09-13 22:13:04 -0400

    cleaned up

--- chrome/app/js/background.js
+++ chrome/app/js/background.js
@@ -1,9 +1,15 @@
+/*
+ * background.js is evaluated when the app is loaded.
+ */
+
+// We use the launch window for debugging.
+// TODO: Have a version of the UI here also.
 function launchWindow(launchData) {
-  chrome.app.window.create('launch.html', {
+  chrome.app.window.create('scraps/launch.html', {
     id: "uproxy",
     minWidth: 640,
     minHeight: 480
   });
 }
-
 chrome.app.runtime.onLaunched.addListener(launchWindow);
+
