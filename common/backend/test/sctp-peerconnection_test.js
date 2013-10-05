2adde4f50c0000c2d4460d7c52b4c579ecb79091
Inf Mally <inf.mally@gmail.com>
2013-10-05 18:01:38 -0400

    tweaked bug example code

--- common/backend/test/sctp-peerconnection_test.js
+++ common/backend/test/sctp-peerconnection_test.js
@@ -6,37 +6,37 @@ console.log('LOCATION: sctp-peerconnection_test: ' + self.location.href);
 var peerA;
 var peerB;
 
-var onload = function() {
-
-  // Send signal now if ready, or queue.
-  function sendSignalToPeer(peer, msg) {
-    if (peer.channel) {
-      peer.channel.emit('message', msg);
-    } else {
-      peer.msgQueue.push(msg);
-    }
+// Send signal now if ready, or queue.
+function sendSignalToPeer(peer, msg) {
+  if (peer.channel) {
+    peer.channel.emit('message', msg);
+  } else {
+    peer.msgQueue.push(msg);
   }
+}
 
-  // sendFn : function (msg) {...}
-  // Assigns peer.channel to a channel to send signal messages to the other
-  // peer.
-  function setupPeer(peer, sendSignalFn) {
-    peer.sctpPc.on('onReceived', function(message) {
-      console.log(peer.name + ": onReceived: " +
-          JSON.stringify(message));
+// sendFn : function (msg) {...}
+// Assigns peer.channel to a channel to send signal messages to the other
+// peer.
+function setupPeer(peer, sendSignalFn) {
+  peer.sctpPc.on('onReceived', function(message) {
+    console.log(peer.name + ": onReceived: " +
+        JSON.stringify(message));
+  });
+  freedom.core().createChannel().done(function(chan) {
+    peer.sctpPc.setup(chan.identifier, peer.name, false);
+    chan.channel.done(function(channel) {
+      peer.channel = channel;
+      peer.channel.on('message', sendSignalFn);
+      console.log(peer.name + ": signalling channel setup. Queued messages being sent: " + peer.msgQueue.length);
+      while(peer.msgQueue.length > 0) {
+        peer.channel.emit('message', peer.msgQueue.shift());
+      }
     });
-    freedom.core().createChannel().done(function(chan) {
-      peer.sctpPc.setup(chan.identifier, peer.peerName, false);
-      chan.channel.done(function(channel) {
-        peer.channel = channel;
-        peer.channel.on('message', sendSignalFn);
-        console.log(peer.name + ": signalling channel setup. Queued messages being sent: " + peer.msgQueue.length);
-        while(peer.msgQueue.length > 0) {
-          peer.channel.emit('message', peer.msgQueue.shift());
-        }
-      });
-    });
-  };
+  });
+};
+
+var onload = function() {
 
   peerA =
     { name : "peerA",
@@ -52,12 +52,20 @@ var onload = function() {
       msgQueue: []
     };
 
-  /* BUG: uncommenting these lines puts freedom in an infinite loop?
-  setupPeer(peerA, sendSignalToPeer.bind(null, peerB));
-  setupPeer(peerB, sendSignalToPeer.bind(null, peerA));
-  */
+  // TODO: FIX freedom BUG: uncommenting these lines puts freedom in an
+  // infinite loop? When there is a delay between this it works ok.
+  //
+  // setupPeer(peerA, sendSignalToPeer.bind(null, peerB));
+  // setupPeer(peerB, sendSignalToPeer.bind(null, peerA));
 
-  // peerA.peer.send({'channelLabel': "a", 'text': "hello?"});
+  setTimeout(function () {
+      setupPeer(peerA, sendSignalToPeer.bind(null, peerB));
+    }, 0);
+  setTimeout(function () {
+      setupPeer(peerB, sendSignalToPeer.bind(null, peerA));
+    }, 500);
+
+  peerA.sctpPc.send({'channelLabel': "a", 'text': "hello?"});
 
   console.log("loaded sctp-peerconnection_test");
   console.log('LOCATION: sctp-peerconnection_test: ' + self.location.href);
