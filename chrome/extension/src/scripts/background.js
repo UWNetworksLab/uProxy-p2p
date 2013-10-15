705474b3cf76ab03682a2bc07a7e51bb6f8bdec5
Inf Mally <inf.mally@gmail.com>
2013-10-15 15:08:16 -0400

    cleaning up events on freedom connector

--- chrome/extension/src/scripts/background.js
+++ chrome/extension/src/scripts/background.js
@@ -253,7 +253,7 @@ function checkRunningProxy() {
         if (model.instances[k].status.proxy == 'running') {
           proxyConfig.startUsingProxy();
           return;
-        } 
+        }
       }
     }
   }
