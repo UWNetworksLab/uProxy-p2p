f13c7d29c2f9cb1126e49a2cf1c481916efed7b2
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-10-15 09:50:57 -0700

    Updating proxy switcher in extension

--- chrome/extension/src/scripts/background.js
+++ chrome/extension/src/scripts/background.js
@@ -7,10 +7,18 @@ console.log('Initializing chrome extension background page...');
 // Chrome App Id for UProxy Packaged Chrome App.
 var FREEDOM_CHROME_APP_ID = 'hilnpmepiebcjhibkbkfkjkacnnclkmi';
 
+//Proxy Configer
+var proxyConfig = new window.BrowserProxyConfig();
+proxyConfig.clearConfig();
+
 // TODO(): remove this if there's no use for it.
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('onInstalled: previousVersion', details.previousVersion);
 });
+chrome.runtime.onSuspend.addListener(function () {
+  console.log('onSuspend');
+  //proxyConfig.stopUsingProxy();
+})
 
 var onStateChange = new chrome.Event();
 
@@ -161,8 +169,6 @@ var ui = new UI();  // This singleton is referenced in both options and popup.
 var appChannel = new FreedomConnector(FREEDOM_CHROME_APP_ID, {
     name: 'uproxy-extension-to-app-port' });
 
-
-
 function wireUItoApp() {
   console.log('Wiring UI to backend...');
   appChannel.on('state-change', function(patchMsg) {
@@ -175,6 +181,7 @@ function wireUItoApp() {
     }
 
     ui.synchronize();
+    checkRunningProxy();
     /*
     // Run through roster if necessary.
     if (patch[0].path.indexOf('roster') >= 0) {
@@ -226,6 +233,21 @@ function reconnectToApp() {
 function initialize() {
   // ui-ready tells uproxy.js to send over *all* the state.
   appChannel.emit('ui-ready');
+}
+
+function checkRunningProxy() {
+  if (model && model.instances) {
+    for (var k in model.instances) {
+      if (model.instances.hasOwnProperty(k) && model.instances[k].status &&
+          model.instances[k].status.proxy) {
+        if (model.instances[k].status.proxy == 'running') {
+          proxyConfig.startUsingProxy();
+          return;
+        } 
+      }
+    }
+  }
+  proxyConfig.stopUsingProxy();
 }
 
 // Automatically attempt to reconnect when disconnected.
--- chrome/extension/src/scripts/proxy-config.js
+++ chrome/extension/src/scripts/proxy-config.js
@@ -10,14 +10,12 @@
   var local_network_addresses = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "fc00::/7"];
   var other_non_routable = ["0.0.0.0/8", "100.64.0.0/10", "127.0.0.0/8", "192.0.0.0/29", "192.0.2.0/24", "198.18.0.0/15", "198.51.100.0/24", "203.0.113.0/24", "2001:10::/28", "2001:db8::/32"]
 
-  function BrowerProxyConfig() {
+  function BrowserProxyConfig() {
     // Save initial settings.
     // Check: do we need to do this? will clear work?
     this.preUproxyConfig = null;
-    chrome.proxy.settings.get({incognito:false},
-      function(details) { this.preUproxyConfig = details.value; } );
 
-    // use locahost
+    // use localhost
     this.uproxyConfig = {
       mode: "fixed_servers",
       rules: {
@@ -27,28 +25,47 @@
           port: 9999
         },
         // List of domains to bypass the proxy
-        bypassList: ["<local>"]
+        // Bypass Google/Facebook RESTful API endpoint (used by XMPP for auth)
+        bypassList: ["<local>", "www.googleapis.com", "graph.facebook.com"]
       }
     };
 
     this.currentConfig = {};
+    this.running = false;
   }
 
-  BrowerProxyConfig.prototype.clearConfig = function () {
+  BrowserProxyConfig.prototype.clearConfig = function () {
     chrome.proxy.settings.clear({scope: 'regular'}, function() {});
   };
 
-  BrowerProxyConfig.prototype.startUsingProxy = function () {
-    this.currentConfig = chrome.proxy.settings.set(
-        {value: this.uproxyConfig, scope: 'regular'},
-        function() {});
+  BrowserProxyConfig.prototype.startUsingProxy = function () {
+    if (this.running == false) {
+      console.log('Directing Chrome proxy settings to UProxy');
+      this.running = true;
+      chrome.proxy.settings.get({
+        incognito:false
+      }, (function(details) { 
+        this.preUproxyConfig = details.value; 
+        this.currentConfig = chrome.proxy.settings.set({
+          value: this.uproxyConfig, 
+          scope: 'regular'
+        }, (function() {}).bind(this));
+      }).bind(this));
+    }
   };
 
-  BrowerProxyConfig.prototype.stopUsingProxy = function () {
-      chrome.proxy.settings.set(
-        {value: this.preUproxyConfig, scope: 'regular'},
-        function() {});
-  }
+  BrowserProxyConfig.prototype.stopUsingProxy = function () {
+    if (this.running == true) {
+      console.log('Reverting Chrome proxy settings');
+      this.running = false;
+      chrome.proxy.settings.set({
+        value: this.preUproxyConfig, 
+        scope: 'regular'
+      },(function() {
+        this.currentConfig = {};
+      }).bind(this));
+    }
+  };
 
-  exports.BrowerProxyConfig = BrowerProxyConfig;
+  exports.BrowserProxyConfig = BrowserProxyConfig;
 })(window);
