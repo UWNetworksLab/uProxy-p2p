a0c3b6b75ff2272a8c733761097e59e21663f44b
Molly Ling <lingmolly67@yahoo.com>
2013-10-16 15:11:37 -0400

    fgrep fixes for avoiding more unnecessary junk.

--- fgrep.sh
+++ fgrep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . \( -type d -a \( -name bower_components -o -name node_modules \) -prune \) -o \( -type f \( -name '*.js' ! -name freedom.js ! -name freedom.min.js \) -exec grep -n "$@" {} \; -printf "---- %p\n\n"  \) 
+find chrome common firefox scraps \( -type d -a \( -name tmp -o -name bower_components -o -name node_modules \) -prune \) -o \( -type f \( -name '*.js' ! -name freedom.js ! -name freedom.min.js \) -exec grep -n "$@" {} \; -printf "---- %p\n\n"  \) 
