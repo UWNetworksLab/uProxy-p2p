4626d514713b375c9307a30dabc879310c799ae5
Lucas Dixon <ldixon@google.com>
2014-03-06 16:17:11 -0500

    tweaked w.r.t. new discussion

--- src/interfaces/freedom-social-v3.d.ts
+++ src/interfaces/freedom-social-v3.d.ts
@@ -9,27 +9,14 @@ declare module Freedom.social {
   // strings.
   enum EVENT {
     onMessage,       // receives a |IncomingMessage| value
-    onRosterUpdate,  // receives a |State| value for a contact
-    onUserUpdate     // The user's state has changed (typically disconnected)
-  }
-
-  // Status indicates whether a the user or a contact is online, offline, or
-  // online with another client which means they can receve chat messages, but
-  // are not expected to react to them in whatever way this application does.
-  enum Status {
-    // Not connected to any social network. There are no guarantees other
-    // methods or events will work until the user calls 'login'
-    OFFLINE,
-    // Online and using the same application (we can send them messages)
-    ONLINE,
-    // Messages will appear as chat to the client
-    ONLINE_WITH_OTHER_CLIENT,
+    onRosterUpdate,  // receives a new |RosterEntry| value for a contact
+    onUserUpdate     // receives a new |UserState| value for the user
   }
 
   // The state of the user, or that of a contact, on the social network
-  interface State {
-    // Is the user/roster entry online/offline/online with another client
-    status: Status;
+  interface UserState {
+    // Is the user online
+    online: boolean;
     // The userId for the chat network
     userId: string;
     // Ephemeral client id for the network
@@ -43,8 +30,37 @@ declare module Freedom.social {
     imageDataUri?: string;
   }
 
-  // Roster is a map from clientIds to their status
-  interface Roster { [clientId: string] : State; }
+  // Status indicates whether a client is online, offline, or online with
+  // another client which means they can receve chat messages, but are not
+  // expected to react to them in whatever way this application does.
+  enum Status {
+    // Not connected to any social network. There are no guarantees other
+    // methods or events will work until the user calls 'login'
+    OFFLINE,
+    // Online and using the same application (we can send them messages)
+    ONLINE,
+    // Messages will appear as chat to the client
+    ONLINE_WITH_OTHER_CLIENT,
+  }
+
+  // The state of a contact on the social network.
+  interface RosterEntry {
+    // The userId for the chat network
+    userId: string;
+    // Optional social network specific details
+    // Name of the user.
+    name?: string;
+    // Homepage URL (e.g. https://alice.com)
+    url?: string;
+    // Image Data URI (e.g. data:image/png;base64,adkwe329...)
+    imageDataUri?: string;
+    // All clients & their status. Offline clients will be listed as offline,
+    // and the next time a roster event is fired, they will not be listed.
+    clients: { [clientId:string] : Status }
+  }
+
+  // Roster is a map from userIds to the relevant rosterEntry
+  interface Roster { [userId:string] : RosterEntry; }
 
   /**
    * Event for an incoming messages
@@ -59,9 +75,14 @@ declare module Freedom.social {
 
   // A request to login to a specific network as a specific agent
   interface LoginRequest {
-    agent: string;    // Name of the application connecting to the network
-    version: string;  // Version of application
-    url: string;      // URL of application
+    // Name of the application connecting to the network. Other logins with the
+    // same agent field will be listed as having status |ONLINE|, where those
+    // with different agents will be listed as |ONLINE_WITH_OTHER_CLIENT|
+    agent: string;
+    // Version of application
+    version: string;
+    // URL of application
+    url: string;
     // When |interactive === true| social will always prompt user for login.
     // Promise fails if the user did not login or provided invalid credentials.
     // When |interactive === false|, promise fails unless the social provider
@@ -69,13 +90,21 @@ declare module Freedom.social {
     interactive: boolean;
     // When true, social provider will remember the token/credentials.
     rememberLogin: boolean;
-    userId?: string;   // optional user Id to login as
+    // Optional user Id to login as.
+    userId?: string;
   }
 }  // declare module Freedom.social
 
 
 declare module Freedom {
   class social {
+    /**
+     * Forms of usage for events:
+     *   on(EVENT.onMessage, handler: (UserState) => void);
+     *   on(EVENT.onRosterUpdate, handler: (RosterEntry) => void);
+     *   on(EVENT.onUserUpdate, handler: (IncomingMessage) => void);
+     **/
+
     /**
      * Generic Freedom Event stuff.
      * Bind an event handler to event type |eventType|. Every time |eventType|
@@ -90,13 +119,6 @@ declare module Freedom {
     once(eventType:string, f:Function);
 
     /**
-     * Forms of usage for events:
-     *   on(EVENT.onMessage, handler: (State) => void);
-     *   on(EVENT.onRosterUpdate, handler: (State) => void);
-     *   on(EVENT.onMessage, handler: (IncomingMessage) => void);
-     **/
-
-    /**
      * Log into the network. The promise succeeds once the user is logged in,
      * and online, and the state is known. The state value in the successful
      * promise is guarenteed to be filly filled out. Note: this will log the
@@ -106,7 +128,12 @@ declare module Freedom {
      * logging in with loginRequest.interactive=false will fail if credentials
      * are needed).
      **/
-    login(loginRequest:social.LoginRequest) : Promise<social.State>;
+    login(loginRequest:social.LoginRequest) : Promise<social.UserState>;
+
+    /**
+     * Return the current user state.
+     **/
+    getUserState() : Promise<social.UserState>;
 
     /**
      * Returns all the Status for each roster enrty.
