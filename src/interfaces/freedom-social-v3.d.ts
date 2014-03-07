257002fa7acfeedbe52c3060867d8524b7ac35c7
Lucas Dixon <ldixon@google.com>
2014-03-06 19:05:31 -0500

    Updated w.r.t. discussion with Ray.
    Key data structures are: Status and Profile. Added extra event type for our own status and profile. Clarified event types in typescript. Getting there!

--- src/interfaces/freedom-social-v3.d.ts
+++ src/interfaces/freedom-social-v3.d.ts
@@ -8,43 +8,40 @@ declare module Freedom.social {
   // event types for each provider. That way code can avoid depending on
   // strings.
   enum EVENT {
-    onMessage,       // receives a |IncomingMessage| value
-    onRosterUpdate,  // receives a new |RosterEntry| value for a contact
-    onUserUpdate     // receives a new |UserState| value for the user
+    onMessage, // A message is received.
+    onRosterProfile, // someone else's roster is recieved or changes
+    onMyProfile, // user's profile is received
+    onMyStatus // user's status changes
   }
 
-  // The state of the user, or that of a contact, on the social network
-  interface UserState {
-    // Is the user online
-    online: boolean;
-    // The userId for the chat network
-    userId: string;
-    // Ephemeral client id for the network
-    clientId?: string;
-    // Optional social network specific details
-    // Name of the user.
-    name?: string;
-    // Homepage URL (e.g. https://alice.com)
-    url?: string;
-    // Image Data URI (e.g. data:image/png;base64,adkwe329...)
-    imageDataUri?: string;
-  }
+  // Error numbers for things that can go wrong and be given in an promise
+  // rejection.
+  //
+  // enum ERROR {
+  //   ...
+  // }
 
   // Status indicates whether a client is online, offline, or online with
   // another client which means they can receve chat messages, but are not
   // expected to react to them in whatever way this application does.
-  enum Status {
-    // Not connected to any social network. There are no guarantees other
-    // methods or events will work until the user calls 'login'
+  enum OnlineState {
+    // Not connected.
     OFFLINE,
-    // Online and using the same application (we can send them messages)
+    // Online with the same application (we can send them messages)
     ONLINE,
     // Messages will appear as chat to the client
     ONLINE_WITH_OTHER_CLIENT,
   }
 
-  // The state of a contact on the social network.
-  interface RosterEntry {
+  // Status of a client connected to a social network.
+  interface Status {
+    userId: string;
+    clientId: string;
+    status: OnlineState;
+  }
+
+  // The profile of a user on a social network.
+  interface Profile {
     // The userId for the chat network
     userId: string;
     // Optional social network specific details
@@ -60,17 +57,16 @@ declare module Freedom.social {
   }
 
   // Roster is a map from userIds to the relevant rosterEntry
-  interface Roster { [userId:string] : RosterEntry; }
+  interface Roster { [userId:string] : Profile; }
 
   /**
    * Event for an incoming messages
    **/
   interface IncomingMessage {
-    toUserId: string;     // userId of user message is to
-    toClientId: string;   // clientId of user message is to
-    fromUserId: string;   // userId of user message is from
-    fromClientId: string; // clientId of user message is from
-    message: string;      // message contents
+    // UserID/ClientID/status of user from whom the message comes from.
+    from: Status;
+    // Message contents.
+    message: string;
   }
 
   // A request to login to a specific network as a specific agent
@@ -99,45 +95,43 @@ declare module Freedom.social {
 declare module Freedom {
   class social {
     /**
-     * Forms of usage for events:
-     *   on(EVENT.onMessage, handler: (UserState) => void);
-     *   on(EVENT.onRosterUpdate, handler: (RosterEntry) => void);
-     *   on(EVENT.onUserUpdate, handler: (IncomingMessage) => void);
+     * Generic Freedom Event stuff. |on| binds an event handler to event type
+     * |eventType|. Every time |eventType|  event is raised, the function |f|
+     * will be called.
      **/
-
-    /**
-     * Generic Freedom Event stuff.
-     * Bind an event handler to event type |eventType|. Every time |eventType|
-     *  event is raised, the function |f| will be called.
-     **/
-    on(eventType:string, f:Function);
-
+    // Message type |onMessage| happens when the user receives a message from
+    // another contact.
+    on(eventType:string, f:Function) : void;
+    on(eventType:'onMessage', f:(message:social.IncomingMessage)=>void) : void;
+    // Message type |onRosterProfile| events are received when another user's
+    // profile is received or when a client changes status.
+    on(eventType:'onRosterProfile', f:(profile:social.Profile)=>void) : void;
+    // Message type |onMyProfile| events are received when the user's profile
+    // changes; e.g. image is received or changes
+    on(eventType:'onMyProfile', f:(profile:social.Profile)=>void) : void;
+    // Message type |onMyStatus| is received when the user's client's status
+    // changes, e.g. when disconnected and online status becomes offline.
+    on(eventType:'onMyStatus', f:(status:social.Status)=>void) : void;
     /**
      * Do a singleton event binding: |f| will only be called once, on the next
-     * event of type |eventType|
+     * event of type |eventType|. Same events as above.
      **/
-    once(eventType:string, f:Function);
+    once(eventType:string, f:Function) : void;
 
     /**
-     * Log into the network. The promise succeeds once the user is logged in,
-     * and online, and the state is known. The state value in the successful
-     * promise is guarenteed to be filly filled out. Note: this will log the
-     * user out of any existing userId, and if the last userId is different to
-     * the current one, the social provider gaurentees that the old credentials
-     * are no longer in the social provider's cache (any subsiquent attempts at
-     * logging in with loginRequest.interactive=false will fail if credentials
-     * are needed).
+     * Log into the network. The promise succeeds once the user is logged in
+     * and gives back the userId and clientId.
      **/
-    login(loginRequest:social.LoginRequest) : Promise<social.UserState>;
+    login(loginRequest:social.LoginRequest) : Promise<social.Status>;
 
     /**
      * Return the current user state.
      **/
-    getUserState() : Promise<social.UserState>;
+    getMyProfileStatus() : Promise<social.Status>;
 
     /**
-     * Returns all the Status for each roster enrty.
-     * Note: the user's own Status will be somewhere in this list.
+     * Returns the current snapshot of the roster with all current known
+     * profiles. Note: the user's own profile will be somewhere in this list.
      **/
     getRoster() : Promise<social.Roster>;
 
