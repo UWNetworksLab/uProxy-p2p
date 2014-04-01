cda108924a28d319fa004477919afd34dbb374b4
Lally Singh <lally@pandatop>
2014-04-01 11:42:34 -0400

    Fixed some missing dependencies.

--- setup.sh
+++ setup.sh
@@ -6,9 +6,33 @@ START_DIR="$(pwd)"
 ROOT_DIR="$(cd "$(dirname $0)"; pwd)"
 PRG="$(basename "$0")"
 
+if ! which npm >/dev/null; then
+  echo "npm is required.  http://nodejs.org"
+  exit 1
+fi
+
+if ! which gem >/dev/null; then
+  echo "ruby is required.  http://www.ruby-lang.org"
+  exit 1
+fi
+
+if ! gem query -i -n sass >/dev/null; then
+  echo "The 'sass' ruby gem needs installation. Try:"
+  echo "sudy gem install sass"
+  exit 1
+fi
+
 echo "START_DIR=$START_DIR"
 echo "ROOT_DIR=$ROOT_DIR"
 echo "PRG=$PRG"
+
+if ! which grunt >/dev/null; then
+  npm install -g grunt
+fi
+
+if ! which bower >/dev/null; then
+  npm install -g bower
+fi
 
 runcmd() {
   # For command printout messages.
