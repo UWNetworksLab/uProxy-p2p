4d87f59a74a9e40eca7d9ac2e34f42674e8cc405
Serene H <keroserene+git@gmail.com>
2014-04-06 22:10:19 -0400

    move chrome notifications into its own file

--- src/chrome_extension/scripts/background.ts
+++ src/chrome_extension/scripts/background.ts
@@ -167,22 +167,6 @@ chrome.runtime.onSuspend.addListener(function () {
 })
 
 
-class ChromeNotifications implements INotifications {
-  ICON_DIR:string = 'icons/';
-  setIcon(iconFile : string) {
-    // TODO: make this not require chrome
-    chrome.browserAction.setIcon({
-      path: this.ICON_DIR + iconFile
-    });
-  }
-  setLabel(text : string) {
-    chrome.browserAction.setBadgeText({ text: '' + text });
-  }
-  setColor(color) {
-    chrome.browserAction.setBadgeBackgroundColor({color: color});
-  }
-}
-
 
 // ---------------------------- Initialization ---------------------------------
 // Called when appChannel is connected.
