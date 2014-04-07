1938f69aa90132c98dc1cf26373400bc1127d4f0
Serene H <keroserene+git@gmail.com>
2014-04-06 22:42:07 -0400

    typescripting for app's background.ts

--- src/chrome_app/scripts/background.ts
+++ src/chrome_app/scripts/background.ts
@@ -1,7 +1,7 @@
 /*
- * background.js is evaluated when the app is loaded.
+ * background.ts is evaluated when the chrome app is loaded.
  */
-'use strict';
+/// <reference path='../../../third_party/DefinitelyTyped/chrome/chrome-app.d.ts' />
 
 // We use the launch window for debugging.
 // TODO: Have a version of the UI here also.
