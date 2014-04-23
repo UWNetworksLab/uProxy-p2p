bb1bf32816b83634fa6638e9827470241c5652d7
Daniel Borkan <dborkan@google.com>
2014-04-23 18:10:09 -0400

    Remove scraps directories, move launch.html to debug.html

--- src/chrome/app/scraps/socksWebRtc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var socksServer = new window.SocksServer("127.0.0.1", 9999, window.relayOverWebRtc);
-
-console.log("Starting test...");
-socksServer.tcpServer.listen();
--- src/chrome/app/scripts/background.ts
+++ src/chrome/app/scripts/background.ts
@@ -5,20 +5,11 @@
 
 // We use the launch window for debugging.
 // TODO: Have a version of the UI here also.
-function launchWindow(launchData) {
-  chrome.app.window.create('scraps/launch.html', {
+function launchDebugWindow(launchData) {
+  chrome.app.window.create('debug.html', {
     id: 'uproxy',
     minWidth: 640,
     minHeight: 480
   });
 }
-chrome.app.runtime.onLaunched.addListener(launchWindow);
-
-// We use the launch window for debugging WebRTC.
-function launchWebRtcTestWindow() {
-  chrome.app.window.create('scraps/webrtc-test.html', {
-    id: 'webrtc-test',
-    minWidth: 640,
-    minHeight: 480
-  });
-}
+chrome.app.runtime.onLaunched.addListener(launchDebugWindow);
