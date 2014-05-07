dbc3d0d2edc38ecb37549db2bf427d43abceb290
Serene H <keroserene+git@gmail.com>
2014-05-07 17:47:26 -0400

    attach consent directive commands to CoreConnector

--- src/generic_ui/scripts/app.ts
+++ src/generic_ui/scripts/app.ts
@@ -125,66 +125,60 @@ angular.module('UProxyExtension', ['angular-lodash', 'dependencyInjector'])
       }
     }  // run function
   ])
+
   /*
-   * uProxy Consent directive handles all consent commands.
+   * The uProxy Consent directive handles all consent commands from the UI to
+   * the Core, which handles passing consent bits over the wire.
    */
   .directive('uproxyConsent', () => {
+    // TODO: Specify the scoping of the 'current user' in a better way.
+    var link = ($s, element, attrs) => {
+      var _modifyConsent = (action:Consent.UserAction) => {
+        $s.core.modifyConsent(<uProxy.ConsentCommand>{
+          network:    $s.ui['network'],
+          userId:     $s.ui.user.userId,
+          instanceId: $s.ui.instance,
+          action:     action
+        });
+      }
+      // Consent to access through a friend:
+      $s.requestAccess = () => {
+        _modifyConsent(Consent.UserAction.REQUEST);
+      };
+      $s.cancelRequest = () => {
+        _modifyConsent(Consent.UserAction.CANCEL_REQUEST);
+      };
+      $s.acceptOffer = () => {
+        _modifyConsent(Consent.UserAction.ACCEPT_OFFER);
+      };
+      $s.ignoreOffer = () => {
+        _modifyConsent(Consent.UserAction.IGNORE_OFFER);
+      };
+      // Consent to provide access for a friend:
+      $s.offerAccess = () => {
+        _modifyConsent(Consent.UserAction.OFFER);
+      };
+      $s.cancelOffer = () => {
+        _modifyConsent(Consent.UserAction.CANCEL_OFFER);
+      };
+      $s.allowRequest = () => {
+        _modifyConsent(Consent.UserAction.ALLOW_REQUEST);
+      };
+      $s.ignoreRequest = () => {
+        _modifyConsent(Consent.UserAction.IGNORE_REQUEST);
+      };
+    };
     return {
       restrict: 'E',
-      templateUrl: 'consent.html'
+      templateUrl: 'consent.html',
+      link: link
     };
-  })
+  });
   // This controller deals with modification of consent bits and the actual
   // starting/stopping of proxying for one particular instance.
+  // TODO: Create a proxy/client angular directive.
+  /*
   .controller('InstanceActions', ['$scope', 'ui', function($s, ui) {
-    // TODO: below is ALL obsolete once the CoreConnector consent bits work.
-
-    // Helper which changes consent bits.
-    // These work the same even if |client| is an instance - so long as it
-    // contains the attribute |clientId|.
-    // |id| can be either a client id or a user id.
-    /*
-    var _modifyConsent = function (id, action) {
-      setTimeout(function() {
-        ui.modifyConsent(id, action);
-      }, 0); // TODO: why is this a timeout?
-    };
-    var _modifyProxyConsent = function(instance, action) {
-      _modifyConsent(instance.instanceId, action);
-      ui.pendingProxyTrustChange = true;
-    }
-    var _modifyClientConsent = function(instance, action) {
-      _modifyConsent(instance.instanceId, action);
-      ui.pendingClientTrustChange = true;
-    }
-    */
-    // Consent to access through a friend.
-    /*
-    $s.requestAccess = function(instance) {
-      _modifyProxyConsent(instance, 'request-access');
-    };
-    $s.cancelRequest = function(instance) {
-      _modifyProxyConsent(instance, 'cancel-request');
-    }
-    $s.acceptOffer = function(instance) {
-      _modifyProxyConsent(instance, 'accept-offer');
-    };
-    $s.declineOffer = function(instance) {
-      _modifyProxyConsent(instance, 'decline-offer');
-    };
-
-    // Consent to provide access for a friend:
-    $s.offerAccess = function(instance) {
-      _modifyClientConsent(instance, 'offer');
-    };
-    $s.grantAccess = function(instance) {
-      _modifyClientConsent(instance, 'allow');
-    };
-    $s.revokeAccess = function(instance) {
-      _modifyClientConsent(instance, 'deny');
-    };
-    $s.denyAccess = $s.revokeAccess;
-
     $s.startAccess = function(instance) {
       // We don't need to tell them we'll start proxying, we can just try to
       // start. The SDP request will go through chat/identity network on its
@@ -195,5 +189,5 @@ angular.module('UProxyExtension', ['angular-lodash', 'dependencyInjector'])
     $s.stopAccess = function(instance) {
       ui.stopProxying();
     };
-    */
   }]);
+  */
