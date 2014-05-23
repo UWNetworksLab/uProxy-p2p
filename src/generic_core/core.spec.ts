055126cc497a6eb284d92daf4680f49fbf62d285
Serene H <keroserene+git@gmail.com>
2014-05-23 15:50:34 -0400

    fix broken user specs

--- src/generic_core/core.spec.ts
+++ src/generic_core/core.spec.ts
@@ -18,6 +18,8 @@ describe('Core', () => {
   network.getUser = null;
   var user = <Core.User><any>jasmine.createSpy('user');
   user.getInstance = null;
+  user.notifyUI = () => {};
+  user.getStorePath = () => { return 'fake/userpath'; };
   var alice = new Core.RemoteInstance(user, {
     instanceId: 'instance-alice',
     keyHash:    'fake-hash-alice',
