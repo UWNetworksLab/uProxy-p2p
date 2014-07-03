11bf8319890c3b122307053357e88c914cea6af3
Serene H <keroserene+git@gmail.com>
2014-07-03 01:53:52 -0400

    fix specs using more freedom mocks

--- src/generic_core/auth.spec.ts
+++ src/generic_core/auth.spec.ts
@@ -1,29 +1,7 @@
 /// <reference path='../interfaces/lib/jasmine/jasmine.d.ts' />
 /// <reference path='auth.ts' />
 
-/**
- * Create a mock core.peerconnection class, pretending to be the freedom
- * provider.
- */
-class MockCorePeerConnection {
-
-  public createOffer = () => {
-    var mockDesc = {
-      sdp: 'a=fingerprint:sha-256 foobar '
-    };
-    return Promise.resolve(mockDesc);
-  }
-
-}  // class MockPeerConnection
-
-
 describe('Authentication', () => {
-
-  beforeEach(() => {
-    freedom['core.peerconnection'] = () => {
-      return new MockCorePeerConnection();
-    };
-  });
 
   it('returns a fingerprint', (done) => {
     Auth.getLocalFingerprint().then((fingerprint) => {
