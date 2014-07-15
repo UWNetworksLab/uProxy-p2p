c66cc4bff68ac860c5541f09777b04346a139daf
keroserene <keroserene+git@gmail.com>
2014-07-15 16:29:24 -0400

    split main.sass into many smaller sass files, remove obsolete rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function(grunt) {
       generic_ui: {
         files: [
           { expand: true, cwd: 'src/generic_ui',
-            src: ['**/*.sass'],
+            src: ['**/main.sass'],
             dest: 'build/generic_ui/',
             ext: '.css'
           }]
