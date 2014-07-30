f3983d5f061cdf2552b16ba8a73a55d691b0b2eb
Salome Vakhtangadze <salomegeo@google.com>
2014-07-30 13:52:52 -0400

    remove view_oauth

--- src/firefox/data/core/view_oauth.js
+++
@@ -1,5 +0,0 @@
-/**
- * Firefox and Chrome use the same manifest file to load core freedom modules
- * In Firefox we handle authentication from main add-on environment.
- * This file still needs so exists in order for freedom to load successfully.
- */
