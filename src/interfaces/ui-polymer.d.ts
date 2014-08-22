4df2c703766e0f6f47bfcfdb2b46ed8196ace62b
Serene H <keroserene+git@gmail.com>
2014-08-22 19:06:45 -0400

    fix broken reference to newest d.ts files

---
+++ src/interfaces/ui-polymer.d.ts
@@ -0,0 +1,8 @@
+/**
+ * ui-polymer.d.ts
+ *
+ * Interface specific to the Polymer-Project specific aspects of the UI.
+ */
+
+// TODO(#345): Actually have typescript for polymer.
+declare var Polymer :any;
