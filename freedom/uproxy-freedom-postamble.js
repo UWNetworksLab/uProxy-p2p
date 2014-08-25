b9d5a156e7b453d639812f7b9085a1c6eb80e438
Lucas Dixon <ldixon@google.com>
2014-08-24 20:23:00 -0400

    fixed typo

--- freedom/uproxy-freedom-postamble.js
+++ freedom/uproxy-freedom-postamble.js
@@ -1,8 +1,8 @@
 // This file allows uproxy to provide programatic access to internal freedom
 // modules for debugging/analysis as needed. You can bind things here under the
-// variable this.global. e.g.
+// variable |global|. e.g.
 //
-//   this.global.freedom_UproxyPeerConnection = freedom_UproxyPeerConnection;
+//   global.freedom_UproxyPeerConnection = freedom_UproxyPeerConnection;
 //
-// would put freedom_UproxyPeerConnection core provide module back into the
+// would put freedom_UproxyPeerConnection core provider module back into the
 // global namespace.
