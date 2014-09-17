ca51b57ef39de2bead060c43f5702b28cb7366e9
Trevor Johnston <trevj@google.com>
2014-09-17 15:27:06 -0400

    freedomchat-chromeapp should just be freedomchat-webpage

--- freedom/samples/freedomchat-chromeapp/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {
-    id: 'main',
-    bounds: {
-      width: 800,
-      height: 400
-    }
-  });
-});
