a4ad1acac272b8fa8fb1bea96eb1a0ba6766aa1f
Serene H <keroserene+git@gmail.com>
2014-09-25 16:50:43 -0400

    consent for proxy works through polymer

--- src/generic_ui/polymer/instance.ts
+++ src/generic_ui/polymer/instance.ts
@@ -1,4 +1,13 @@
 Polymer({
+
+  ready: function() {
+    this.path = <InstancePath>{
+      network: 'google',  // TODO: Make network reflect actual social network.
+      userId: this.userId,
+      instanceId: this.instance.instanceId
+    };
+  },
+
   getting: function() {
     // return UI.Gestalt.GETTING == window['$ui'].gestalt;
     return true;
@@ -10,15 +19,11 @@ Polymer({
     // var ui = roster.parentNode.host;
     // return ui.GETTING == ui.gestalt;
   },
+
   start: function() {
-    var path = <InstancePath>{
-      network: 'google',  // TODO: Make network reflect actual social network.
-      userId: this.userId,
-      instanceId: this.instance.instanceId
-    };
-    console.log('[polymer] calling core.start(', path, ')');
+    console.log('[polymer] calling core.start(', this.path, ')');
     // this.access.asProxy = true;
-    core.start(path).then(() => {
+    core.start(this.path).then(() => {
       console.log('[polymer] received core.start promise fulfillment.');
       // TODO: Use BrowserAction to set the extension icon to 'proxy mode'.
     });
@@ -27,5 +32,33 @@ Polymer({
     console.log('[polymer] calling core.stop()');
     // this.access.asProxy = false;
     core.stop();
+  },
+
+  // |action| is the string end for a Consent.UserAction
+  modifyConsent: function(action :Consent.UserAction) {
+    // var actionEnumStr = action;
+    // var action :Consent.UserAction = Consent.UserAction[action];
+    var command = <uProxy.ConsentCommand>{
+      path: this.path,
+      action: action
+    };
+    console.log('[polymer] consent command', command)
+    core.modifyConsent(command);
+  },
+
+  // Proxy UserActions.
+  request: function() { this.modifyConsent(Consent.UserAction.REQUEST) },
+  cancel: function() { this.modifyConsent(Consent.UserAction.CANCEL_REQUEST) },
+  accept: function() { this.modifyConsent(Consent.UserAction.ACCEPT_OFFER) },
+  ignore: function() { this.modifyConsent(Consent.UserAction.IGNORE_OFFER) },
+
+  // Client UserActions
+  offer: function() { this.modifyConsent(Consent.UserAction.OFFER) },
+
+  getProxyConsentState: function() : string {
+    return Consent.ProxyState[this.instance.consent.asProxy];
+  },
+  getClientConsentState: function() : string {
+    return Consent.ClientState[this.instance.consent.asClient];
   }
 });
