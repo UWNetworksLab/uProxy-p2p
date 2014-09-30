b9fa321ad8e9265833469fdb1ec7e9adece37d5a
Daniel Borkan <dborkan@google.com>
2014-09-30 16:36:42 -0400

    UI cleanup

--- src/chrome/extension/scripts/chrome_connector.ts
+++ src/chrome/extension/scripts/chrome_connector.ts
@@ -157,10 +157,8 @@ class ChromeConnector implements uProxy.CoreBrowserConnector {
     this.status.connected = false;
     this.appPort_ = null;
 
-    if (ui.isProxying()) {
-      ui.showNotification('The uProxy app has exited, stopping proxy.')
-      ui.stopProxyingInUiAndConfig();
-    }
+    // Ensure that proxying has stopped.
+    ui.stopProxyingInUiAndConfig();
 
     console.warn('Retrying connection in ' + (SYNC_TIMEOUT/1000) + 's...');
     setTimeout(this.connect, SYNC_TIMEOUT);
