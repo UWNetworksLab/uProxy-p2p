179e56aaf76651e7ec7e736a54e6907ee0f719eb
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2014-10-16 10:11:28 -0400

    remove app background.ts

--- src/chrome/app/scripts/background.ts
+++
@@ -1,7 +0,0 @@
-/*
- * background.ts is evaluated when the chrome app is loaded.
- */
-/// <reference path='../../../third_party/typings/chrome/chrome-app.d.ts' />
-
-// Do nothing when the user tries to launch the application.
-chrome.app.runtime.onLaunched.addListener(() => {});
