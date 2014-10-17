c03efee06c683b29c24a5c5d91f2d6f7fad5bdee
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2014-10-17 12:24:04 -0400

    removed white space

--- webrtc/peerconnection.d.ts
+++ webrtc/peerconnection.d.ts
@@ -81,7 +81,6 @@ declare module WebRtc {
     // change from |WAITING| state to |CONNECTING|)
     public openDataChannel :(channelLabel: string,
                              options?: RTCDataChannelInit) => DataChannel;
-
     // Or handle data channels opened by the peer (these events will )
     public peerOpenedChannelQueue :Handler.Queue<DataChannel, void>;
 
