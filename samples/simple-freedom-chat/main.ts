579749724b4ccfb726dd7f3650b75435cc263d7d
Trevor Johnston <trevj@google.com>
2014-10-24 17:13:20 -0400

    fewer indentations

--- samples/simple-freedom-chat/main.ts
+++ samples/simple-freedom-chat/main.ts
@@ -14,40 +14,35 @@ var sendAreaB = <HTMLInputElement>document.getElementById("sendAreaB");
 var receiveAreaA = <HTMLInputElement>document.getElementById("receiveAreaA");
 var receiveAreaB = <HTMLInputElement>document.getElementById("receiveAreaB");
 
-freedom(
-    'freedom-module.json',
-    {
-      'debug': 'log'
-    })
-  .then(function(interface:any) {
-    // TODO: typings for the freedom module
-    var chat :any = interface();
+freedom('freedom-module.json', { 'debug': 'log' }).then(function(interface:any) {
+  // TODO: typings for the freedom module
+  var chat :any = interface();
 
-    chat.on('ready', function() {
-      log.info('peer connection established!');
-      sendAreaA.disabled = false;
-      sendAreaB.disabled = false;
-    });
-
-    chat.on('error', function() {
-      log.error('something went wrong with the peer connection');
-      sendAreaA.disabled = true;
-      sendAreaB.disabled = true;
-    });
-
-    function send(suffix:string, textArea:HTMLInputElement) {
-      chat.emit('send' + suffix, {
-        message: textArea.value || '(empty message)'
-      });
-    }
-    sendButtonA.onclick = send.bind(null, 'A', sendAreaA);
-    sendButtonB.onclick = send.bind(null, 'B', sendAreaB);
-
-    function receive(textArea:HTMLInputElement, msg:Chat.Message) {
-      textArea.value = msg.message;
-    }
-    chat.on('receiveA', receive.bind(null, receiveAreaA));
-    chat.on('receiveB', receive.bind(null, receiveAreaB));
-  }, (e:Error) => {
-    log.error('could not load freedom: ' + e.message);
+  chat.on('ready', function() {
+    log.info('peer connection established!');
+    sendAreaA.disabled = false;
+    sendAreaB.disabled = false;
   });
+
+  chat.on('error', function() {
+    log.error('something went wrong with the peer connection');
+    sendAreaA.disabled = true;
+    sendAreaB.disabled = true;
+  });
+
+  function send(suffix:string, textArea:HTMLInputElement) {
+    chat.emit('send' + suffix, {
+      message: textArea.value || '(empty message)'
+    });
+  }
+  sendButtonA.onclick = send.bind(null, 'A', sendAreaA);
+  sendButtonB.onclick = send.bind(null, 'B', sendAreaB);
+
+  function receive(textArea:HTMLInputElement, msg:Chat.Message) {
+    textArea.value = msg.message;
+  }
+  chat.on('receiveA', receive.bind(null, receiveAreaA));
+  chat.on('receiveB', receive.bind(null, receiveAreaB));
+}, (e:Error) => {
+  log.error('could not load freedom: ' + e.message);
+});
