9478c45588c9cbe4f43e8e7538fbf16c8a62c650
Daniel Borkan <dborkan@google.com>
2014-10-29 15:49:59 -0400

    Get Facebook integrated into uProxy

--- src/chrome/extension/scripts/google_auth.ts
+++ src/chrome/extension/scripts/google_auth.ts
@@ -1,128 +1,90 @@
-/// <reference path='../../../generic_ui/scripts/core_connector.ts'/>
-/// <reference path='../../../interfaces/authentication-manager.d.ts' />
-/// <reference path='../../../third_party/typings/chrome/chrome.d.ts'/>
+
 /// <reference path='../../../uproxy.ts' />
+/// <reference path='chrome_tab_auth.ts' />
 
 var CLIENT_ID =
     "746567772449-bp2g60f0mq4g8u02pqcepm2mjttogrrt.apps.googleusercontent.com";
 var CLIENT_SECRET = "Bmlc90_i2GFcaP26Fneq9UnO";
 
-var REDIRECT_URI = "https://www.uproxy.org/";
-
-declare var core :CoreConnector;
-
-class GoogleAuth {
-  private tabId_ :number = -1;
-
+class GoogleAuth extends ChromeTabAuth {
   constructor() {
+    super();
   }
 
-  public login = () : void => {
-    if (this.tabId_ === -1) {
-      this.getAccessCode_();
-    } else {
-      chrome.tabs.update(this.tabId_, {active:true});
-    }
-  }
-
-  private getAccessCode_ = () : void => {
-    var extractCode = function(tabId, changeInfo, tab) {
-      if (tab.id === this.tabId_ && tab.url.indexOf(REDIRECT_URI) === 0) {
-        chrome.tabs.onUpdated.removeListener(extractCode);
-        chrome.tabs.onRemoved.removeListener(onTabClose);
-        this.tabId_ = -1;
-        chrome.tabs.remove(tabId);
-        if (tab.url.indexOf('error=access_denied') > 0) {
-          this.onError_('User denied access.');
-          return;
-        }
-        var code = tab.url.match(/code=([^&]+)/)[1];
-        this.getToken_(code);
-      }
-    }.bind(this);
+  public getOauthUrl = (redirctUrl) : string => {
     var googleOAuth2Url = "https://accounts.google.com/o/oauth2/auth?" +
         "scope=email%20https://www.googleapis.com/auth/googletalk" +
-        "&redirect_uri=" + REDIRECT_URI +
+        "&redirect_uri=" + redirctUrl +
         "&response_type=code" +
         "&client_id=" + CLIENT_ID;
-    var accountChooserUrl =
-        'https://accounts.google.com/accountchooser?continue=' +
+    return 'https://accounts.google.com/accountchooser?continue=' +
         encodeURIComponent(googleOAuth2Url);
-    var onTabClose = function(tabId, removeInfo) {
-        if (tabId == this.tabId_) {
-          chrome.tabs.onUpdated.removeListener(extractCode);
-          chrome.tabs.onRemoved.removeListener(onTabClose);
-          this.tabId_ = -1;
-          this.onError_('Login abandoned.');
+  }
+
+  public extractCode = (url) : Promise<any> => {
+    if (url.indexOf('error=access_denied') > 0) {
+      return Promise.reject('User denied access.');
+    }
+
+    var code = url.match(/code=([^&]+)/)[1];
+    return this.getToken_(code).then((token) => {
+      return this.getCredentials_(token);
+    });
+  }
+
+  private getToken_ = (code :string) : Promise<string> => {
+    return new Promise<string>((fulfill, reject) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://accounts.google.com/o/oauth2/token", false);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      var params = "code=" + code +
+          "&client_id=" + CLIENT_ID +
+          "&client_secret=" + CLIENT_SECRET +
+          "&redirect_uri=" + REDIRECT_URL +
+          "&grant_type=authorization_code";
+      xhr.addEventListener('load', (event) => {
+        if (xhr.status != 200) {
+          reject('Error getting credentials ' + xhr.status);
+          return;
         }
-    }.bind(this);
-    chrome.tabs.create({url: accountChooserUrl},
-                       function(tab: chrome.tabs.Tab) {
-      this.tabId_ = tab.id;
-      chrome.tabs.onRemoved.addListener(onTabClose);
-      chrome.tabs.onUpdated.addListener(extractCode);
-    }.bind(this));
+        var resp = JSON.parse(xhr.response);
+        fulfill(resp.access_token);
+      }, false);
+
+      xhr.addEventListener('error', function(evt) {
+        reject('Error occurred while trying to get Google oAuth token');
+      }, false);
+
+      xhr.send(params);
+    });
   }
 
-  private getToken_ = (code :string) : void => {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://accounts.google.com/o/oauth2/token", false);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    var params = "code=" + code +
-        "&client_id=" + CLIENT_ID +
-        "&client_secret=" + CLIENT_SECRET +
-        "&redirect_uri=" + REDIRECT_URI +
-        "&grant_type=authorization_code";
-    xhr.addEventListener('load', (event) => {
-      if (xhr.status != 200) {
-        this.onError_('Error getting credentials ' + xhr.status);
-        return;
-      }
-      var resp = JSON.parse(xhr.response);
-      this.getCredentials_(resp.access_token);
-    }, false);
-
-    xhr.addEventListener('error', function(evt) {
-      this.onError_('Error occurred while trying to get Google oAuth token');
-    }, false);
-
-    xhr.send(params);
-  }
-
-  private getCredentials_ = (token :string) : void => {
-    // Make googleapis request to get user's email address, then pass
-    // credentials back to social provider.
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', (evt) => {
-      if (xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        var credentials :GoogleTalkCredentials = {
-            userId: response.email,
-            jid: response.email,
-            oauth2_token: token,
-            oauth2_auth: 'http://www.google.com/talk/protocol/auth',
-            host: 'talk.google.com'
-        };
-        this.sendCredentials_(credentials);
-      } else {
-      this.onError_('Error validating Google oAuth token');
-      }
-    }, false);
-    xhr.addEventListener('error', (evt) => {
-      this.onError_('Error occurred while validating Google oAuth token');
-    }, false);
-    xhr.open('GET', "https://www.googleapis.com/oauth2/v1/userinfo?alt=json", false);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-    xhr.send();
-  }
-
-  private onError_ = (errorText :string) : void => {
-    core.sendCommand(uProxy.Command.SEND_CREDENTIALS,
-                     {cmd: 'error', message: errorText});
-  }
-
-  private sendCredentials_ = (credentials :GoogleTalkCredentials) : void => {
-    core.sendCommand(uProxy.Command.SEND_CREDENTIALS,
-                     {cmd: 'auth', message: credentials});
+  private getCredentials_ = (token :string) : Promise<any> => {
+    return new Promise<GoogleTalkCredentials>((fulfill, reject) => {
+      // Make googleapis request to get user's email address, then pass
+      // credentials back to social provider.
+      var xhr = new XMLHttpRequest();
+      xhr.addEventListener('load', (evt) => {
+        if (xhr.status == 200) {
+          var response = JSON.parse(xhr.responseText);
+          var credentials :GoogleTalkCredentials = {
+              userId: response.email,
+              jid: response.email,
+              oauth2_token: token,
+              oauth2_auth: 'http://www.google.com/talk/protocol/auth',
+              host: 'talk.google.com'
+          };
+          fulfill(credentials);
+        } else {
+        reject('Error validating Google oAuth token');
+        }
+      }, false);
+      xhr.addEventListener('error', (evt) => {
+        reject('Error occurred while validating Google oAuth token');
+      }, false);
+      xhr.open('GET', "https://www.googleapis.com/oauth2/v1/userinfo?alt=json", false);
+      xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+      xhr.send();
+    });
   }
 }
--- src/chrome/extension/scripts/oauth.ts
+++ src/chrome/extension/scripts/oauth.ts
@@ -1,15 +1,21 @@
+/// <reference path='facebook_auth.ts' />
 /// <reference path='google_auth.ts' />
 
+// TODO: refactor this code so that it is the same in Firefox and Chrome
 class OAuth {
+  private facebookAuth_ :FacebookAuth;
   private googleAuth_ :GoogleAuth;
 
   constructor() {
     this.googleAuth_ = new GoogleAuth();
+    this.facebookAuth_ = new FacebookAuth();
   }
 
   public getCredentials(network :string) {
     if (network === 'Google') {
       this.googleAuth_.login();
+    } else if (network === 'Facebook') {
+      this.facebookAuth_.login();
     }
   }
 }
