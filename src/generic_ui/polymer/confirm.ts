53bacf3694451b255f66d0f17f4f53552e7547fd
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2014-11-05 18:05:29 -0500

    Change tabs to spaces.

--- src/generic_ui/polymer/confirm.ts
+++ src/generic_ui/polymer/confirm.ts
@@ -2,11 +2,11 @@
 declare var proxyConfig :IBrowserProxyConfig;
 
 Polymer({
-	undoProxyConfig: function() {
-		this.proxyConfig.stopUsingProxy(false);
-		this.$.confirmButton.disabled = true;
-		this.$.proxyReverted.hidden = false;
-	},
+  undoProxyConfig: function() {
+    this.proxyConfig.stopUsingProxy(false);
+    this.$.confirmButton.disabled = true;
+    this.$.proxyReverted.hidden = false;
+  },
   ready: function() {
     // Expose global proxyConfig object in this context.
     this.proxyConfig = proxyConfig;
