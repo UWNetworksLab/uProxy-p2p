0dcf4615d9eccbe737fc9cada984eb93123445b9
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2014-11-14 10:38:33 -0500

    Add button to toggle sharing state.

--- src/generic_ui/polymer/roster.ts
+++ src/generic_ui/polymer/roster.ts
@@ -1,12 +1,17 @@
 /// <reference path='../../interfaces/ui-polymer.d.ts' />
 
 Polymer({
+  model: model,
   onlineTrustedUproxyContacts: model.contacts.onlineTrustedUproxy,
   offlineTrustedUproxyContacts: model.contacts.offlineTrustedUproxy,
   onlineUntrustedUproxyContacts: model.contacts.onlineUntrustedUproxy,
   offlineUntrustedUproxyContacts: model.contacts.offlineUntrustedUproxy,
   onlineNonUproxyContacts: model.contacts.onlineNonUproxy,
   offlineNonUproxyContacts: model.contacts.offlineNonUproxy,
+  toggleSharing: function() {
+    model.globalSettings.sharing = !this.model.globalSettings.sharing;
+    core.updateGlobalSettings(model.globalSettings);
+  },
   ready: function() {
     console.log('initializing roster');
     // this.contacts.push({
@@ -17,6 +22,6 @@ Polymer({
     // this.contacts.push({ name: 'charlie' });
     // this.contacts.push({ name: 'dave' });
     // this.contacts.push({ name: 'eve' });
-  }, 
+  },
   searchQuery: ''
 });
