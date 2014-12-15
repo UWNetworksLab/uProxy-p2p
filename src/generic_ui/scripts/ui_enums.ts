295b17345217e0585baf7931a6b83efd7ec6409e
Salome Vakhtangadze <salomegeo@google.com>
2014-12-15 17:30:41 -0500

    get rid of extra files in popup.html

---
+++ src/generic_ui/scripts/ui_enums.ts
@@ -0,0 +1,22 @@
+module UI {
+  /**
+   * Enumeration of mutually-exclusive view states.
+   */
+  export enum View {
+    SPLASH = 0,
+    ROSTER,
+    USER,
+    NETWORKS,
+    SETTINGS,
+  }
+
+  /**
+   * Enumeration of mutually-exclusive UI modes.
+   */
+  export enum Mode {
+    GET = 0,
+    SHARE
+  }
+}
+
+
