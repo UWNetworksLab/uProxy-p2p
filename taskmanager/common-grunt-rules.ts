c8be02c3730d04e6fefac07182e98946d7ecb2a8
Lucas Dixon <ldixon@google.com>
2015-01-15 21:25:16 -0500

    re-added common-grunt-rules

---
+++ taskmanager/common-grunt-rules.ts
@@ -0,0 +1,37 @@
+// common-grunt-rules
+/// <reference path='../../third_party/typings/node/node.d.ts' />
+
+// Assumes that the each spec file is a fully browserified js file.
+export function jasmineSpec(name:string) {
+  return {
+    src: [
+      require.resolve('arraybuffer-slice'),
+      path.join(path.dirname(require.resolve('es6-promise/package.json')),
+                'dist/promise-1.0.0.js')
+    ],
+    options: {
+      specs: 'build/dev/' + name + '/**/*.spec.js',
+      outfile: 'build/dev/' + name + '/SpecRunner.html',
+      keepRunner: true
+    }
+  };
+}
+
+export function browserifyTypeScript(filepath:string) {
+  return {
+    src: [filepath + '.ts'],
+    dest: filepath + '.js',
+    browserifyOptions: {
+      debug: true,
+      transform: ['tsify']
+    }
+  };
+}
+
+export function copyFreedomToDest(destPath:string) {
+  return { files: [{
+    src: [require.resolve('freedom')],
+    dest: destPath,
+    onlyIf: 'modified'
+  }] };
+}
