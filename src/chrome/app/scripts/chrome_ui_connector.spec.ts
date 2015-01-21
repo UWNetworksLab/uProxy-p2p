c057a793838a8e140588955821adcb75a745afd8
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2015-01-20 20:15:29 -0500

    Add todo for ChromeUIConnector tests.

--- src/chrome/app/scripts/chrome_ui_connector.spec.ts
+++ src/chrome/app/scripts/chrome_ui_connector.spec.ts
@@ -1,6 +1,7 @@
 /// <reference path='chrome_ui_connector.ts' />
 
 describe('chrome-ui-connector', () => {
-  it('test stub', () => {
+  it('test placeholder', () => {
+    // TODO: Write tests for ChromeUIConnector.
   });
 });
