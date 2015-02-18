83880eb26e11184e5c10984a8e31f771a10e1407
Jonathan Pevarnek <jpevarnek@google.com>
2015-02-18 11:46:37 -0500

    Add a handler to send URL data to the copy+paste connection
    This adds a handler to chrome that will block loads of
    uproxy.org/(request|offer)/{payload} and instead sends the payload data
    to the copypaste instance in the core.
    
    The actual handling of the tab interception is done in browser-specific
    areas while the parsing is handled in the generic_ui component.
    
    For Chrome, when the request is intercepted, the tab is redirected to
    show the standard app interface.  This behaviour can be a bit odd (e.g.
    if the app is already open), however, it seems equally as good as the
    alternative of showing a landing page just indicating that the url is
    loaded.
    
    For Firefox, no action is taken on the request and the UProxy window is
    opened.

---
+++ src/firefox/lib/url-handler.js
@@ -0,0 +1,26 @@
+var { Cc, Ci, Cr } = require('chrome');
+
+exports.setup = function(panel) {
+  var observer = {
+    observe: function (subject, topic, data) {
+      if ('http-on-modify-request' !== topic) {
+        return;
+      }
+
+      subject.QueryInterface(Ci.nsIHttpChannel);
+      var url = subject.URI.spec
+
+      if (!/https:\/\/www.uproxy.org\/(request|offer)\/(.*)/.test(url)) {
+        return;
+      }
+
+      panel.port.emit('handleUrlData', url);
+      panel.show();
+
+      subject.cancel(Cr.NS_BINDING_ABORTED);
+    }
+  };
+
+  var service = Cc['@mozilla.org/observer-service;1'].getService(Ci.nsIObserverService);
+  service.addObserver(observer, 'http-on-modify-request', false);
+};
