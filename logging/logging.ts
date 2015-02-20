e1c0ae43ea2c5f76dc5e73f6bb30daf9c036de0c
Jonathan Pevarnek <jpevarnek@google.com>
2015-02-20 13:42:26 -0500

    Allow arguments to be passed to the logger functions without an array
    This change feels a bit excessive, however, most of it was necessary to
    get this done in a clean manner.  The objective here was to allow making
    a log call to be done as `log('%1 %2', 'foo', 'bar')` instead of
    `log('%1 %2', ['foo', 'bar'])`; not totally necessary but a bit nicer to
    work with.
    
    To do this, function signatures needed to be switched around for all
    the main logging functions to accept variable-length arguments.  There
    needed to be some code to ensure backwards compatibility and that felt
    better in a private method rather than the doLog function.  The rest of
    the code in doLog then felt redundant and the calling felt simplified by
    moving it to a private method and giving it access to the logging
    variable.
    
    In terms of testing, the module has been broken up a bit more to have
    separate tests for the tag-calling behaviour (which was previously mixed
    into the tests) and then to have the tests for actually calling the
    logging functions below that with a mockLogger object set up each time
    so that did not have to be done in each test.  This provides better test
    granuality and reduces the overhead for setting up new tests.

--- logging/logging.ts
+++ logging/logging.ts
@@ -5,37 +5,16 @@ import freedomTypes = require('freedom.types');
 
 // Perform log message formatting. Formats an array of arguments to a
 // single string.
-// TODO: switch to rest arguments.
 // TODO: move this into the provider.
-function formatStringMessageWithArgs_(msg:string, args?:any[])
+function formatStringMessageWithArgs_(msg:string, args :any[])
     : string {
   var formatted_msg = msg;
-  if (args && args.length) {
-    for (var i = 0; i < args.length; i++) {
-      formatted_msg = formatted_msg.replace('%' + (i + 1), args[i]);
-    }
+
+  for (var i = 0; i < args.length; i++) {
+    formatted_msg = formatted_msg.replace('%' + (i + 1), args[i]);
   }
+
   return formatted_msg;
-}
-
-interface loggable {
-  (logger: freedomTypes.Logger): void;
-}
-
-function doLog(level:string, msg:string, args?:any[]) : loggable {
-  var message = formatStringMessageWithArgs_(msg, args);
-
-  return (logger: freedomTypes.Logger) => {
-    if (level === 'debug') {
-      logger.debug(message);
-    } else if (level === 'info') {
-      logger.info(message);
-    } else if (level === 'warn') {
-      logger.warn(message);
-    } else if (level === 'error') {
-      logger.error(message);
-    }
-  };
 }
 
 export class Log {
@@ -43,20 +22,45 @@ export class Log {
   constructor(private tag_:string) {
     this.logger = freedom.core().getLogger(this.tag_);
   }
+
+  private log_ = (level :string, msg :string, args :any[]) :void => {
+    var message :string;
+
+    if (1 === args.length && Array.isArray(args[0])) {
+      args = args[0];
+    }
+
+    message = formatStringMessageWithArgs_(msg, args);
+
+    this.logger.then((logger :freedomTypes.Logger) => {
+      // essentially do logger[level](message) minus the type warning
+      switch (level) {
+        case 'debug':
+          return logger.debug(message);
+        case 'info':
+          return logger.info(message);
+        case 'warn':
+          return logger.warn(message);
+        case 'error':
+          return logger.error(message);
+      }
+    });
+  }
+
   // Logs message in debug level.
-  public debug = (msg: string, args?:any[]) : void => {
-    this.logger.then(doLog('debug', msg, args));
+  public debug = (msg: string, ...args :any[]) :void => {
+    this.log_('debug', msg, args);
   }
   // Logs message in info level.
-  public info = (msg: string, args?:any[]) : void => {
-    this.logger.then(doLog('info', msg, args));
+  public info = (msg: string, ...args :any[]) :void => {
+    this.log_('info', msg, args);
   }
   // Logs message in warn level.
-  public warn = (msg: string, args?:any[]) : void => {
-    this.logger.then(doLog('warn', msg, args));
+  public warn = (msg: string, ...args :any[]) :void => {
+    this.log_('warn', msg, args);
   }
   // Logs message in error level.
-  public error = (msg: string, args?:any[]) : void => {
-    this.logger.then(doLog('error', msg, args));
+  public error = (msg: string, ...args :any[]) :void => {
+    this.log_('error', msg, args);
   }
 }
