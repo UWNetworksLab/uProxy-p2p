ccc14e4c5866563fcb98e16c390720cb4f9ab834
Jonathan Pevarnek <jpevarnek@google.com>
2015-02-25 13:54:22 -0500

    Handle cases where JSON.stringify fails
    If JSON.stringify fails, we will first try to see if we are able to call
    toString on the object and if so, will do so.  If we are not, we will
    instead insert the exception into the log message.

--- logging/logging.ts
+++ logging/logging.ts
@@ -13,7 +13,15 @@ function formatStringMessageWithArgs_(args :Object[])
   for (var i = 0; i < args.length; i++) {
     var arg = args[i];
     if ('string' !== typeof(arg) && !(arg instanceof String)) {
-      arg = JSON.stringify(arg);
+      try {
+        arg = JSON.stringify(arg);
+      } catch (e) {
+        if (arg && typeof(arg.toString) === 'function') {
+          arg = arg.toString();
+        } else {
+          arg = e.message;
+        }
+      }
     }
 
     if (-1 !== msg.indexOf('%' + i)) {
