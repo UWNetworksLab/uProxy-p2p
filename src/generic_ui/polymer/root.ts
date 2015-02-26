15153f8c5c9feb75d9dfc8116107ac3cf17561eb
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2015-02-26 18:34:18 -0500

    update ui.view syntax

--- src/generic_ui/polymer/root.ts
+++ src/generic_ui/polymer/root.ts
@@ -9,17 +9,17 @@ Polymer({
     // If we're switching from the SPLASH page to the ROSTER, fire an
     // event indicating the user has logged in. roster.ts listens for
     // this event.
-    if (detail.view == UI.View.ROSTER && ui['view'] == UI.View.SPLASH) {
+    if (detail.view == UI.View.ROSTER && ui.view == UI.View.SPLASH) {
       this.fire('core-signal', {name: "login-success"});
     }
-    ui['view'] = detail.view;
+    ui.view = detail.view;
   },
   settingsView: function() {
-    ui['view'] = UI.View.SETTINGS;
+    ui.view = UI.View.SETTINGS;
   },
   rosterView: function() {
     console.log('rosterView called');
-    ui['view'] = UI.View.ROSTER;
+    ui.view = UI.View.ROSTER;
   },
   setGetMode: function() {
     ui.mode = UI.Mode.GET;
