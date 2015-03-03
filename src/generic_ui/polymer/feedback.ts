6d2100a96a6cbdde907fa597b7b944cdfde8ced4
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2015-03-02 20:18:05 -0500

    styling changes and basic sendFeedback function

--- src/generic_ui/polymer/feedback.ts
+++ src/generic_ui/polymer/feedback.ts
@@ -5,5 +5,10 @@ Polymer({
   backToSettings: function() {
     ui.view = UI.View.SETTINGS;
   },
+  sendFeedback: function() {
+    console.log(this.email);
+    console.log(this.feedback);
+    console.log(this.$.logCheckbox.checked);
+  },
   ready: function() {}
 });
