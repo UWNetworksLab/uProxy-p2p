690688574b5309308d85aefbc4686956910de9a1
lucyhe <lucyhe@lucyhe-macbookpro.roam.corp.google.com>
2015-03-05 11:14:37 -0500

    add dialog to confirm feedback sumission
    and reset form after submission

--- src/generic_ui/polymer/feedback.ts
+++ src/generic_ui/polymer/feedback.ts
@@ -1,4 +1,3 @@
-
 Polymer({
   model: model,
   ui: ui,
@@ -8,7 +7,11 @@ Polymer({
   backToSettings: function() {
     ui.view = UI.View.SETTINGS;
   },
+  backToRoster: function() {
+    ui.view = UI.View.ROSTER;
+  },
   sendFeedback: function() {
+    // TODO: Get and send real logs.
     if (this.$.logCheckbox.checked) {
       this.logs = 'placeholder';
     } else {
@@ -19,6 +22,15 @@ Polymer({
       feedback: this.feedback,
       logs: this.logs
     });
+    this.$.confirmation.toggle();
+    // Reset the placeholders, which seem to be cleared after the
+    // user types input in the input fields.
+    this.$.emailInput.placeholder = 'Email address';
+    this.$.feedbackInput.placeholder = 'Write your feedback';
+    // Clear the form.
+    this.email = '';
+    this.feedback = '';
+    this.$.logCheckbox.checked = false;
   },
   ready: function() {}
 });
