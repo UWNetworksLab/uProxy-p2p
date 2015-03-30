4193c163d51bb74192eba4b34c8ab9305515514e
Lucy He <lucydhe@gmail.com>
2015-03-30 15:17:29 -0400

    Small edits.

--- src/generic_ui/polymer/logs.ts
+++ src/generic_ui/polymer/logs.ts
@@ -11,9 +11,8 @@ Polymer({
     this.ui = ui;
     core.getLogs().then((logs) => {
       this.loadingLogs = false;
-      this.logs = logs;
       // Add browser info to logs.
-      this.logs = 'Browser Info: ' + navigator.userAgent + '\n\n' + this.logs;
+      this.logs = 'Browser Info: ' + navigator.userAgent + '\n\n' + logs;
     });
   }
 });
