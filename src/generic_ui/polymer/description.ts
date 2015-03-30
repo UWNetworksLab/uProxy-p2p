9313ec2fc082cdeaf3655ff79a5e5e2bde0c0770
Lucy He <lucydhe@gmail.com>
2015-03-30 09:58:03 -0400

    Make description look "editable"

--- src/generic_ui/polymer/description.ts
+++ src/generic_ui/polymer/description.ts
@@ -5,6 +5,19 @@ declare var core :CoreConnector;
 
 Polymer({
   model: model,
+  editing: false,
+  lastSavedDescription: model.globalSettings.description,
+  editDescription: function() {
+    this.lastSavedDescription = this.model.globalSettings.description;
+    this.editing = true;
+  },
+  saveDescription: function() {
+    this.editing = false;
+  },
+  cancelEditing: function() {
+    this.model.globalSettings.description = this.lastSavedDescription;
+    this.editing = false;
+  },
   update: function() {
     core.updateGlobalSettings(model.globalSettings);
   }
