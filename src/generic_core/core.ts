e0e3685965bc7230a9a78f2423bd335fc5a6f496
Jonathan Pevarnek <jpevarnek@google.com>
2015-04-09 14:05:56 -0400

    Add regex to remove anything in the JSON field name from logs

--- src/generic_core/core.ts
+++ src/generic_core/core.ts
@@ -581,6 +581,8 @@ class uProxyCore implements uProxy.CoreAPI {
   private formatLogs_ = (logs :string[]) : string => {
     // replace the emails with consistent tags throughout the logs
     var emails :string[] = [];
+    var names :string[] = [];
+
     var emailReplacer = (email :string) :string => {
       var id = _.indexOf(emails, email);
       if (id === -1) {
@@ -591,12 +593,23 @@ class uProxyCore implements uProxy.CoreAPI {
       return 'EMAIL_' + id;
     }
 
+    var nameReplacer = (match :string, pre :string, name :string, post :string):string => {
+      var id = _.indexOf(names, name);
+      if (id === -1) {
+        id = names.length;
+        names.push(name);
+      }
+
+      return pre + 'NAME_' + id + post;
+    }
+
     var text = logs.join('\n');
 
     // email regex taken from regular-expressions.info
     text = text.replace(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/ig,
                         emailReplacer);
     text = text.replace(/data:image\/.+;base64,[A-Za-z0-9+\/=]+/g, 'IMAGE_DATA');
+    text = text.replace(/("name":")([^"]*)(")/g, nameReplacer);
     return text;
   }
 }  // class uProxyCore
