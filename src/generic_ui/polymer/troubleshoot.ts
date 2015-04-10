99ceaff06d5ac45735ecd1f6cddf82aa7c684da7
Lucy He <lucydhe@gmail.com>
2015-04-10 01:09:00 -0400

    Comments + clean up.

--- src/generic_ui/polymer/troubleshoot.ts
+++ src/generic_ui/polymer/troubleshoot.ts
@@ -4,19 +4,15 @@ Polymer({
   },
   open: function() {
     this.analyzedNetwork = false;
+    this.analyzingNetwork = false;
     this.$.troubleshootDialog.open();
   },
   submitFeedback: function() {
     this.fire('core-signal', {name: 'open-feedback', data: {includeLogs: this.analyzedNetwork}});
     this.close();
   },
-  analyzeNetworkAndViewLogs: function() {
-    this.ui.openTab('view-logs.html');
-    this.analyzedNetwork = true;
-  },
   getNatType: function() {
     this.analyzingNetwork = true;
-
     core.getNatType().then((natType) => {
       this.natType = natType;
       if (natType === 'SymmetricNAT') {
