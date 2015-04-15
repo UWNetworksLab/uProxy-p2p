aec249980cf5cee0b713a273826aa7cd5fb61b89
Lucas Dixon <ldixon@google.com>
2015-04-15 17:40:11 -0400

    last file typescripted

--- src/generic_core/freedom-module.ts
+++ src/generic_core/freedom-module.ts
@@ -27,12 +27,15 @@ import social_network = require('./social');
 import remote_instance = require('./remote-instance');
 import remote_connection = require('./remote-connection');
 import user_interface = require('../interfaces/ui');
-import version = require('../interfaces/version');
+import version = require('../generic/version');
 import browser_connector = require('../interfaces/browser_connector');
 import ui = require('./ui_connector');
+import uproxy_core = require('./uproxy_core');
+
+import ui_connector = ui.connector;
 
 var log :logging.Log = new logging.Log('core');
-log.info('Loading core', UPROXY_VERSION);
+log.info('Loading core', version.UPROXY_VERSION);
 
 // Prepare all the social providers from the manifest.
 social_network.initializeNetworks();
@@ -40,34 +43,34 @@ social_network.initializeNetworks();
 // --------------------------------------------------------------------------
 // Register Core responses to UI commands.
 // --------------------------------------------------------------------------
-var core = new uProxyCore();
-ui.connector.onCommand(uproxy_core_api.Command.GET_INITIAL_STATE, ui.sendInitialState);
+var core = new uproxy_core.uProxyCore();
+ui_connector.onCommand(uproxy_core_api.Command.GET_INITIAL_STATE, ui_connector.sendInitialState);
 // When the login message is sent from the extension, assume it's explicit.
-ui.connector.onPromiseCommand(uproxy_core_api.Command.LOGIN, core.login);
-ui.connector.onPromiseCommand(uproxy_core_api.Command.LOGOUT, core.logout)
+ui_connector.onPromiseCommand(uproxy_core_api.Command.LOGIN, core.login);
+ui_connector.onPromiseCommand(uproxy_core_api.Command.LOGOUT, core.logout)
 
 // TODO: UI-initiated Instance Handshakes need to be made specific to a network.
 // core.onCommand(uproxy_core_api.Command.SEND_INSTANCE_HANDSHAKE_MESSAGE,
 //                core.sendInstanceHandshakeMessage);
-ui.connector.onCommand(uproxy_core_api.Command.MODIFY_CONSENT, core.modifyConsent);
+ui_connector.onCommand(uproxy_core_api.Command.MODIFY_CONSENT, core.modifyConsent);
 
-ui.connector.onPromiseCommand(uproxy_core_api.Command.START_PROXYING_COPYPASTE_GET,
+ui_connector.onPromiseCommand(uproxy_core_api.Command.START_PROXYING_COPYPASTE_GET,
                       core.startCopyPasteGet);
 
-ui.connector.onPromiseCommand(uproxy_core_api.Command.STOP_PROXYING_COPYPASTE_GET,
+ui_connector.onPromiseCommand(uproxy_core_api.Command.STOP_PROXYING_COPYPASTE_GET,
                       core.stopCopyPasteGet);
 
-ui.connector.onCommand(uproxy_core_api.Command.START_PROXYING_COPYPASTE_SHARE,
+ui_connector.onCommand(uproxy_core_api.Command.START_PROXYING_COPYPASTE_SHARE,
                core.startCopyPasteShare);
 
-ui.connector.onPromiseCommand(uproxy_core_api.Command.STOP_PROXYING_COPYPASTE_SHARE,
+ui_connector.onPromiseCommand(uproxy_core_api.Command.STOP_PROXYING_COPYPASTE_SHARE,
                       core.stopCopyPasteShare);
 
-ui.connector.onCommand(uproxy_core_api.Command.COPYPASTE_SIGNALLING_MESSAGE,
+ui_connector.onCommand(uproxy_core_api.Command.COPYPASTE_SIGNALLING_MESSAGE,
                core.sendCopyPasteSignal);
 
-ui.connector.onPromiseCommand(uproxy_core_api.Command.START_PROXYING, core.start);
-ui.connector.onCommand(uproxy_core_api.Command.STOP_PROXYING, core.stop);
+ui_connector.onPromiseCommand(uproxy_core_api.Command.START_PROXYING, core.start);
+ui_connector.onCommand(uproxy_core_api.Command.STOP_PROXYING, core.stop);
 
 // TODO: Implement this or remove it.
 // core.onCommand(uproxy_core_api.Command.CHANGE_OPTION, (data) => {
@@ -79,9 +82,9 @@ ui.connector.onCommand(uproxy_core_api.Command.STOP_PROXYING, core.stop);
 // core.onCommand(uproxy_core_api.Command.INVITE, (userId:string) => {
 // });
 
-ui.connector.onCommand(uproxy_core_api.Command.HANDLE_MANUAL_NETWORK_INBOUND_MESSAGE,
+ui_connector.onCommand(uproxy_core_api.Command.HANDLE_MANUAL_NETWORK_INBOUND_MESSAGE,
                core.handleManualNetworkInboundMessage);
-ui.connector.onCommand(uproxy_core_api.Command.UPDATE_GLOBAL_SETTINGS, core.updateGlobalSettings);
-ui.connector.onPromiseCommand(uproxy_core_api.Command.SEND_FEEDBACK, core.sendFeedback);
-ui.connector.onPromiseCommand(uproxy_core_api.Command.GET_LOGS, core.getLogsAndNetworkInfo);
-ui.connector.onPromiseCommand(uproxy_core_api.Command.GET_NAT_TYPE, core.getNatType);
+ui_connector.onCommand(uproxy_core_api.Command.UPDATE_GLOBAL_SETTINGS, core.updateGlobalSettings);
+ui_connector.onPromiseCommand(uproxy_core_api.Command.SEND_FEEDBACK, core.sendFeedback);
+ui_connector.onPromiseCommand(uproxy_core_api.Command.GET_LOGS, core.getLogsAndNetworkInfo);
+ui_connector.onPromiseCommand(uproxy_core_api.Command.GET_NAT_TYPE, core.getNatType);
