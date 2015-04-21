f9e94aefbf09e2de5552ccc436253f49dde07ce2
Lucas Dixon <ldixon@google.com>
2015-04-21 10:52:51 -0400

    review feedback

--- src/generic_core/freedom-module.ts
+++ src/generic_core/freedom-module.ts
@@ -10,23 +10,12 @@
  *  - Instances, which is a list of active uProxy installs.
  */
 
-// TODO: Create a copy rule which automatically moves all third_party
-// typescript declarations to a nicer path.
-
 /// <reference path='../../../third_party/freedom-typings/freedom-module-env.d.ts' />
 /// <reference path='../../../third_party/freedom-typings/social.d.ts' />
 
-import globals = require('./globals');
-
-import diagnose = require('./diagnose-nat');
 import logging = require('../../../third_party/uproxy-lib/logging/logging');
-import net = require('../../../third_party/uproxy-networking/net/net.types');
 import uproxy_core_api = require('../interfaces/uproxy_core_api');
-import social = require('../interfaces/social');
 import social_network = require('./social');
-import remote_instance = require('./remote-instance');
-import remote_connection = require('./remote-connection');
-import user_interface = require('../interfaces/ui');
 import version = require('../version/version');
 import browser_connector = require('../interfaces/browser_connector');
 import ui = require('./ui_connector');
@@ -55,7 +44,6 @@ ui_connector.onCommand(
     uproxy_core_api.Command.GET_INITIAL_STATE,
     ui_connector.sendInitialState);
 
-// When the login message is sent from the extension, assume it's explicit.
 ui_connector.onPromiseCommand(
     uproxy_core_api.Command.LOGIN,
     core.login);
@@ -64,9 +52,6 @@ ui_connector.onPromiseCommand(
     uproxy_core_api.Command.LOGOUT,
     core.logout);
 
-// TODO: UI-initiated Instance Handshakes need to be made specific to a network.
-// core.onCommand(uproxy_core_api.Command.SEND_INSTANCE_HANDSHAKE_MESSAGE,
-//                core.sendInstanceHandshakeMessage);
 ui_connector.onCommand(uproxy_core_api.Command.MODIFY_CONSENT,
     core.modifyConsent);
 
@@ -96,16 +81,6 @@ ui_connector.onPromiseCommand(
 
 ui_connector.onCommand(uproxy_core_api.Command.STOP_PROXYING,
     core.stop);
-
-// TODO: Implement this or remove it.
-// core.onCommand(uproxy_core_api.Command.CHANGE_OPTION, (data) => {
-//   log.warn('CHANGE_OPTION yet to be implemented');
-//   // TODO: Handle changes that might affect proxying.
-// });
-
-// TODO: make the invite mechanism an actual process.
-// core.onCommand(uproxy_core_api.Command.INVITE, (userId:string) => {
-// });
 
 ui_connector.onCommand(
     uproxy_core_api.Command.HANDLE_MANUAL_NETWORK_INBOUND_MESSAGE,
