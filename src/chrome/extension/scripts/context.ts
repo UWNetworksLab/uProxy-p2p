494f2a77faeab95d3c38fa56e8e6aacd7f32d1dd
Lucas Dixon <ldixon@google.com>
2015-04-21 15:49:11 -0400

    rename StaticInPanel to UiGlobals

--- src/chrome/extension/scripts/context.ts
+++ src/chrome/extension/scripts/context.ts
@@ -5,7 +5,7 @@ import ui_constants = require('../../../interfaces/ui');
 import user_interface = require('../../../generic_ui/scripts/ui');
 import CoreConnector = require('../../../generic_ui/scripts/core_connector');
 
-var panel :StaticInPanel = (<any>chrome.extension.getBackgroundPage()).browserified_exports;
+var panel :UiGlobals = (<any>chrome.extension.getBackgroundPage()).browserified_exports;
 export var ui :user_interface.UserInterface= panel.ui;
 export var core :CoreConnector = panel.core;
 export var chromeCoreConnector = (<any>panel).chromeCoreConnector;
