1fb592abe0ae57b65c86e84728ea6748c49ab575
Jonathan Pevarnek <jpevarnek@google.com>
2015-05-19 17:31:02 -0400

    Show an error when the browser is unable to proxy
    If the browser API indicates to the user that we are unable to proxy, we
    will now show an error.  If the user is already proxying when this
    happens, we will cut off the connection and show them a much more
    prominent error.
    
    This also (again) simplifies the logic of calculating the current height
    of the statuses.
    
    This also fixes the firing of core signals.
    
    Fixes #1533

---
+++ src/generic_ui/polymer/proxy-error.ts
@@ -0,0 +1,17 @@
+/// <reference path='./context.d.ts' />
+/// <reference path='../../../../third_party/polymer/polymer.d.ts' />
+
+Polymer({
+  error: false,
+  openWithError: function() {
+    this.wasError = true;
+    this.open();
+  },
+  open: function() {
+    this.$.dialog.open();
+  },
+  close: function() {
+    this.wasError = false;
+    this.$.dialog.close();
+  },
+});
