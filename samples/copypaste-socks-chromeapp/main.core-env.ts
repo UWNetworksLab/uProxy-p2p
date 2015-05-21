c24b70f7319872d8c8611f46e52ea0754a40738d
Trevor Johnston <trevj@google.com>
2015-05-21 17:17:10 -0400

    BridgingPeerConnection no longer batches, so revert the previous change

--- samples/copypaste-socks-chromeapp/main.core-env.ts
+++ samples/copypaste-socks-chromeapp/main.core-env.ts
@@ -22,16 +22,18 @@ module copypaste_module {
     copypaste.on('signalForPeer', (message:signals.Message) => {
       model.readyForStep2 = true;
 
-      // Base64-encode the signalling message because some communication
+      // Append the new signalling message to the previous message(s), if any.
+      // Base64-encode the concatenated messages because some communication
       // channels are likely to transform portions of the raw concatenated JSON
       // into emoticons, whereas the base64 alphabet is much less prone to such
       // unintended transformation.
-      var messageAsJson = JSON.stringify(message);
+      var oldConcatenatedJson = base64Decode(model.outboundMessageValue.trim());
+      var newConcatenatedJson = oldConcatenatedJson + '\n' + JSON.stringify(message);
       if (model.usingCrypto) {
         copypaste.emit('friendKey', model.friendPublicKey);
-        copypaste.emit('signEncrypt', base64Encode(messageAsJson));
+        copypaste.emit('signEncrypt', base64Encode(newConcatenatedJson));
       }
-      model.outboundMessageValue = base64Encode(messageAsJson);
+      model.outboundMessageValue = base64Encode(newConcatenatedJson);
     });
 
     copypaste.on('gotPeerSDP', (peerSDP:string) => {
@@ -117,9 +119,10 @@ module copypaste_module {
     totalBytesSent : 0
   };
 
-  // Stores the parsed message for use later, if & when the user clicks the
+  // Stores the parsed messages for use later, if & when the user clicks the
   // button for consuming the messages.
-  var parsedInboundMessage: Object;
+  var parsedInboundMessages :signals.Message[];
+
 
   // Define basee64 helper functions that are type-annotated and meaningfully
   // named.
@@ -136,16 +139,43 @@ module copypaste_module {
   // signalling messages. Enables/disables the corresponding form button, as
   // appropriate. Returns null if the field contents are malformed.
   export function parseInboundMessages(inboundMessageFieldValue:string) : void {
+    // Base64-decode the pasted text.
+    var signalsString :string = null;
     try {
-      // Decode and de-serialise the pasted text.
-      var messageJson = base64Decode(inboundMessageFieldValue.trim());
-      parsedInboundMessage = JSON.parse(messageJson);
-
-      // Enable the button for consuming the messages.
-      model.inputIsWellFormed = true;
+      signalsString = base64Decode(inboundMessageFieldValue.trim());
     } catch (e) {
-      console.warn('cannot parse message: ' + e.message);
+      // TODO: Notify the user that the pasted text is malformed.
+      return null;
     }
+
+    var signals :string[] = signalsString.trim().split('\n');
+
+    // Each line should be a JSON representation of a signals.Message.
+    // Parse the lines here.
+    var parsedSignals :signals.Message[] = [];
+    for (var i = 0; i < signals.length; i++) {
+      var s :string = signals[i].trim();
+
+      // TODO: Consider detecting the error if the text is well-formed JSON but
+      // does not represent a signals.Message.
+      var message :signals.Message;
+      try {
+        message = JSON.parse(s);
+      } catch (e) {
+        parsedSignals = null;
+        break;
+      }
+      parsedSignals.push(message);
+    }
+
+    // Enable/disable, as appropriate, the button for consuming the messages.
+    if (null !== parsedSignals && parsedSignals.length > 0) {
+      model.inputIsWellFormed = true;
+    } else {
+      // TODO: Notify the user that the pasted text is malformed.
+    }
+
+    parsedInboundMessages = parsedSignals;
   }
 
   // Forwards each line from the paste box to the Freedom app, which
@@ -155,7 +185,9 @@ module copypaste_module {
   export function consumeInboundMessage() : void {
     // Forward the signalling messages to the Freedom app.
     onceReady.then(function(copypaste) {
-      copypaste.emit('handleSignalMessage', parsedInboundMessage);
+      for (var i = 0; i < parsedInboundMessages.length; i++) {
+        copypaste.emit('handleSignalMessage', parsedInboundMessages[i]);
+      }
     });
     model.proxyingState = 'connecting';
     // TODO: Report success/failure to the user.
