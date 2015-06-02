19885754e14f441c1c1ead622cd68eeb7adfdac6
Jonathan Pevarnek <jpevarnek@google.com>
2015-06-02 09:30:46 -0400

    This adds the actual UI components to support multiple networks
    Under settings, we now have a panel that lists all the networks we could
    sign in through and offers the user to connect or disconnect to each of
    these networks.
    
    This does not yet add the small thingamabobbers next to the profile
    image denoting which network is signed in.

---
+++ src/generic_ui/polymer/name-for-network.ts
@@ -0,0 +1,9 @@
+/// <reference path='./context.d.ts' />
+/// <reference path='../../../../third_party/polymer/polymer.d.ts' />
+
+Polymer({
+  isEmail: function(text :string) {
+    /* regex from regular-expressions.info */
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(text);
+  }
+});
--- src/generic_ui/polymer/settings.ts
+++ src/generic_ui/polymer/settings.ts
@@ -1,10 +1,16 @@
 /// <reference path='./context.d.ts' />
+/// <reference path='../../../../third_party/polymer/polymer.d.ts' />
 
 var ui = ui_context.ui;
 var core = ui_context.core;
 var model = ui_context.model;
 
+import _ = require('lodash');
+import user_interface = require('../scripts/ui');
+
 Polymer({
+  accountChooserOpen: false,
+  networkNames: '',
   logOut: function() {
     // logout all networks asynchronously
     for (var i in model.onlineNetworks) {
@@ -25,11 +31,35 @@ Polymer({
   openAdvancedSettingsForm: function() {
     this.fire('core-signal', {name: 'open-advanced-settings'});
   },
-  observe: {
-    'model.globalSettings.statsReportingEnabled' : 'saveGlobalSettings'
+  networksChanged: function() {
+    if (!model.onlineNetworks) {
+      return;
+    }
+
+    if (model.onlineNetworks.length === 1) {
+      this.networkNames = model.onlineNetworks[0].name;
+    } else if (model.onlineNetworks.length === 2) {
+      this.networkNames = model.onlineNetworks[0].name + ' and ' + model.onlineNetworks[1].name;
+    } else {
+      this.networkNames = '';
+      for (var i in model.onlineNetworks) {
+        // the key is a string, use a soft equality checker here
+        if (i == model.onlineNetworks.length - 1) {
+          this.networkNames += 'and ' + model.onlineNetworks[i].name;
+        } else {
+          this.networkNames += model.onlineNetworks[i].name + ', ';
+        }
+      }
+    }
+  },
+  toggleAccountChooser: function() {
+    this.accountChooserOpen = !this.accountChooserOpen;
   },
   ready: function() {
     this.ui = ui;
     this.model = model;
+  },
+  observe: {
+    'model.onlineNetworks': 'networksChanged'
   }
 });
