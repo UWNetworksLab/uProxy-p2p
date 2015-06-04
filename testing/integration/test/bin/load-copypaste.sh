d6f7afc9e0e7e8437a6747eee2bec06b7d40103b
Lally Singh <lally@google.com>
2015-06-04 13:56:59 -0400

    SNAPSHOT COMMIT: Moving testing/devel to a vm for now.

--- testing/integration/test/bin/load-copypaste.sh
+++ testing/integration/test/bin/load-copypaste.sh
@@ -6,15 +6,17 @@
 export DISPLAY=:10
 Xvfb :10 -screen 0 1280x1024x24 &
 
-cd /src
-git clone https://github.com/uProxy/uproxy-lib.git .
+mkdir -p /test/src
+cd /test/src
+npm install -g bower grunt-cli
+git clone https://github.com/uProxy/uproxy-lib.git
 cd uproxy-lib
 ./setup.sh install
 grunt
 
 mkdir /tmp/chrome-data
 
-google-chrome --user-data-dir=/tmp/chrome-data --load-and-launch-app=/src/uproxy-lib/build/dev/uproxy-lib/samples/copypaste-socks-chromeapp
+google-chrome --user-data-dir=/tmp/chrome-data --load-and-launch-app=/test/src/uproxy-lib/build/dev/uproxy-lib/samples/copypaste-socks-chromeapp
 
 # Wait for the control port (9000) to open.
 while ! netstat -t | grep localhost:9000 >/dev/null; do sleep 1; done
