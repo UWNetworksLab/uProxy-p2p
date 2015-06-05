b08215e9e7754747f56346155d9f2a7e8f3b1d53
Lally Singh <lally@google.com>
2015-06-05 09:24:00 -0700

    Updates from working in a vm.

--- testing/integration/test/bin/load-copypaste.sh
+++ testing/integration/test/bin/load-copypaste.sh
@@ -5,6 +5,7 @@
 # Overlap X startup with our download and build.
 export DISPLAY=:10
 Xvfb :10 -screen 0 1280x1024x24 &
+fvwm &
 
 mkdir -p /test/src
 cd /test/src
@@ -13,11 +14,13 @@ git clone https://github.com/uProxy/uproxy-lib.git
 cd uproxy-lib
 ./setup.sh install
 grunt
+grunt samples
 
 mkdir /tmp/chrome-data
 
-google-chrome --user-data-dir=/tmp/chrome-data --load-and-launch-app=/test/src/uproxy-lib/build/dev/uproxy-lib/samples/copypaste-socks-chromeapp
+google-chrome --user-data-dir=/tmp/chrome-data --load-and-launch-app=/test/src/uproxy-lib/build/dev/uproxy-lib/samples/copypaste-socks-chromeapp --no-default-browser-check --no-first-run &
 
 # Wait for the control port (9000) to open.
-while ! netstat -t | grep localhost:9000 >/dev/null; do sleep 1; done
-
+echo -n "Waiting for control port to come up"
+while ! netstat -lt | grep localhost:9000 >/dev/null; do echo -n .;  sleep 1; done
+echo 
