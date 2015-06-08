382b3d77976ba7a2a8bde4080367a8a06aa21feb
Lally Singh <lally@google.com>
2015-06-08 11:18:02 -0400

    Added args to load-copypaste.sh.

--- testing/integration/test/bin/load-copypaste.sh
+++ testing/integration/test/bin/load-copypaste.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 # Loads the copy-paste example app.
+CLONEARGS=
+CLONESRC=https://github.com/uProxy/uproxy-lib.git
+while getopts b:r:h? opt; do
+    case $opt in
+        b)
+            CLONEARGS="$CLONEARGS -b $OPTARG"
+            ;;
+        r)
+            CLONESRC=$OPTARG
+            ;;
+        *)
+            echo "$0 [-b branch] [-r repo]"
+            echo "  -b: BRANCH is the branch to checkout instead of HEAD's referant."
+            echo "  -r: REPO is the repository to clone instead of github.com/uProxy/uproxy-lib."
+            exit 1;
+            ;;
+    esac
+done
+
 
 # Overlap X startup with our download and build.
 export DISPLAY=:10
@@ -10,7 +29,7 @@ fvwm &
 mkdir -p /test/src
 cd /test/src
 npm install -g bower grunt-cli
-git clone https://github.com/uProxy/uproxy-lib.git
+git clone $CLONEARGS $CLONESRC
 cd uproxy-lib
 ./setup.sh install
 grunt
@@ -18,9 +37,9 @@ grunt samples
 
 mkdir /tmp/chrome-data
 
-google-chrome --user-data-dir=/tmp/chrome-data --load-and-launch-app=/test/src/uproxy-lib/build/dev/uproxy-lib/samples/copypaste-socks-chromeapp --no-default-browser-check --no-first-run &
+google-chrome --user-data-dir=/tmp/chrome-data --load-and-launch-app=/test/src/uproxy-lib/build/dev/uproxy-lib/samples/copypaste-socks-chromeapp --no-default-browser-check --no-first-run >/dev/null 2>&1 &
 
 # Wait for the control port (9000) to open.
 echo -n "Waiting for control port to come up"
-while ! netstat -lt | grep localhost:9000 >/dev/null; do echo -n .;  sleep 1; done
+while ! netstat -lt | grep 9000 >/dev/null; do echo -n .;  sleep 1; done
 echo 
