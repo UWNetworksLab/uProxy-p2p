083901ac852909980593fd2d062615b20e1cae66
Trevor Johnston <trevj@google.com>
2015-06-30 15:55:47 -0400

    use giver/getter and offer/answer names

--- testing/run-scripts/connect-pair.py
+++ testing/run-scripts/connect-pair.py
@@ -6,16 +6,16 @@
 import socket
 import time
 
-first = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-first.connect(("localhost", 9000))
+getter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+getter.connect(("localhost", 9000))
 
-second = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-second.connect(("localhost", 9010))
+giver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+giver.connect(("localhost", 9010))
 
-first.send("GET\n")
+getter.send("GET\n")
 time.sleep(1.0)
-first_sdp = first.recv(4096)
-second.send("GIVE " + first_sdp + "\n")
+offer_sdp = getter.recv(4096)
+giver.send("GIVE " + offer_sdp + "\n")
 time.sleep(1.0)
-second_sdp = second.recv(4096)
-first.send(second_sdp)
+answer_sdp = giver.recv(4096)
+getter.send(answer_sdp)
