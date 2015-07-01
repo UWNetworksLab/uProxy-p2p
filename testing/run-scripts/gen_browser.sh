4fc24871e884a86531db3b6b4122c7114920feb8
Trevor Johnston <trevj@google.com>
2015-07-01 14:43:26 -0400

    download and boot jetpack, to make firefox work

--- testing/run-scripts/gen_browser.sh
+++ testing/run-scripts/gen_browser.sh
@@ -67,6 +67,7 @@ RUN echo BROWSER=firefox >/etc/test.conf
 RUN cd /tmp ; mkdir ff ; cd ff ; wget -r -l1 --no-parent -A '$PATTERN' $URL
 RUN cd /usr/share ; tar xf /tmp/ff/*/*/*/*/*/*/*/*/*.bz2
 RUN ln -s /usr/share/firefox/firefox /usr/bin/firefox
+RUN mkdir -p /tmp/jetpack ; cd /tmp/jetpack ; wget https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/jetpack-sdk-latest.tar.gz ; tar xvzf jetpack-sdk-latest.tar.gz
 EOF
 
 }
