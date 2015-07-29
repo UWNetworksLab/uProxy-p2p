bbfacb371c20d664696dfdde400589fcd16b4c6f
Lally Singh <lally@google.com>
2015-07-29 11:57:36 -0400

    Fixed docker image-name check.

--- testing/run-scripts/run_pair.sh
+++ testing/run-scripts/run_pair.sh
@@ -53,7 +53,7 @@ if $VNC; then
 fi
 
 function make_image () {
-    if docker images | grep uproxy/$1 >/dev/null
+    if [ $(docker images | tail -n +2 | awk '{print $1}' | /bin/grep uproxy/$1) == "uproxy/$1" ]
     then
         echo "Reusing existing image uproxy/$1"
     else
