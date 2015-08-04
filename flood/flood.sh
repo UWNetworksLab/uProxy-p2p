9c69987d034b93294360b08748c902d8b4e968aa
Trevor Johnston <trevj@google.com>
2015-08-04 16:56:30 -0400

    extract a script to start a flood server

--- flood/flood.sh
+++ flood/flood.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: flood.sh <size of download> <max. transfer rate>"
-  echo "Examples:"
-  echo "  10MB @ 500k/sec: flood.sh 10M 500k"
-  echo "  1GB @ 1M/sec: flood.sh 1G 10M"
-  exit 1
-fi
+###
+# DO NOT CALL THIS SCRIPT DIRECTLY -- see testing/run-scripts/flood.sh
+###
 
 ncat -l -k -p 1224 -c "dd if=/dev/zero count=1 bs=$1 status=none | pv -q -L $2"
