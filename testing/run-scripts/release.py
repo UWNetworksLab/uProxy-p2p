9321fe3fe90404529438ce8357963ba0b34e3248
Trevor Johnston <trevj@google.com>
2015-08-06 15:48:00 -0400

    add a large file transfer test to the release script

--- testing/run-scripts/release.py
+++ testing/run-scripts/release.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+# This is a major part of the uproxy-lib release process:
+#   https://github.com/uProxy/uproxy-lib
+# Currently, this performs two important tests on every
+# getter/giver combination of the stable, beta, and canary
+# releases of Chrome and Firefox:
+#  1. download http://www.example.com/, via curl
+#  2. transfer a 16MB file, via ncat
+# In both cases, the transferred data is checksummed
+# against a "known good" transfer.
+# At the end, the results are output as a table in Markdown
+# format, suitable for inclusion in Github release notes.
+
 import argparse
 import itertools
 import subprocess
@@ -15,11 +27,25 @@ versions = ['stable', 'beta', 'canary']
 test_url = 'http://www.example.com/'
 
 # Compute a hash of the page without using any proxy.
-match = subprocess.check_output(
+known_small_md5sum = subprocess.check_output(
     'curl ' + test_url + ' 2>/dev/null|md5sum -|cut -d\' \' -f1',
     shell=True,
     universal_newlines=True).strip()
-print('** known md5sum: ' + match)
+print('** small download known md5sum: ' + known_small_md5sum)
+
+# Where is flood server?
+FLOOD_SIZE_MB = 16
+FLOOD_MAX_SPEED = '5M'
+flood_ip = subprocess.check_output(['./flood.sh', str(FLOOD_SIZE_MB),
+    FLOOD_MAX_SPEED], universal_newlines=True).strip()
+print('** using flood server at ' + str(flood_ip))
+
+subprocess.call('nc ' + flood_ip + ' 1224 > /tmp/nc', shell=True)
+known_large_md5sum = subprocess.check_output(
+    'cat /tmp/nc|md5sum -|cut -d\' \' -f1',
+    shell=True,
+    universal_newlines=True).strip()
+print('** large transfer known md5sum: ' + known_large_md5sum)
 
 # Iterate through every browser/version combination.
 results = {}
@@ -35,13 +61,25 @@ for getter_spec, giver_spec in itertools.product(combos, combos):
     subprocess.call(['./run_pair.sh', '-p', args.clone_path,
         getter_spec, giver_spec], timeout=15)
 
-    md5sum = subprocess.check_output(
+    # small HTTP download.
+    small_md5sum = subprocess.check_output(
         'curl -x socks5h://localhost:9999 ' + test_url + ' 2>/dev/null|md5sum -|cut -d\' \' -f1',
         shell=True,
         universal_newlines=True).strip()
-    print('** md5sum: ' + md5sum)
+    print('** small download md5sum: ' + small_md5sum)
+    passed = (small_md5sum == known_small_md5sum)
 
-    passed = (md5sum == match)
+    # large (non-HTTP) transfer.
+    if subprocess.call(['nc', '-X', '5', '-x', 'localhost:9999',
+        flood_ip, '1224']) != 0:
+      raise Exception('nc failed, proxy probably did not start')
+    large_md5sum = subprocess.check_output(
+        'cat /tmp/nc|md5sum -|cut -d\' \' -f1',
+        shell=True,
+        universal_newlines=True).strip()
+    print('** large transfer md5sum: ' + large_md5sum)
+    passed = passed and (large_md5sum == known_large_md5sum)
+
     print('** ' + str(passed))
   except Exception as e:
     print('** failure for ' + getter_spec + ' <- ' + giver_spec + ': ' + str(e))
