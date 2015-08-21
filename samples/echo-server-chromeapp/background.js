73376952dbc90d5c3989c63b72c065936328c81b
Trevor Johnston <trevj@google.com>
2015-08-21 11:43:21 -0400

    use plain js for echo server background page

---
+++ samples/echo-server-chromeapp/background.js
@@ -0,0 +1,21 @@
+var script = document.createElement('script');
+script.src = 'freedom-for-chrome/freedom-for-chrome.js';
+document.head.appendChild(script);
+
+// Keep a background timeout running continuously, to prevent chrome from
+// putting the app to sleep.
+function keepAlive() {
+  setTimeout(keepAlive, 5000);
+}
+keepAlive();
+
+script.onload = function() {
+  freedom('uproxy-lib/echo/freedom-module.json', {
+    'logger': 'uproxy-lib/loggingprovider/freedom-module.json',
+    'debug': 'debug'
+  }).then(function(moduleFactory) {
+    moduleFactory();
+  }, (e) => {
+    console.error('could not load freedomjs module: ' + e.message);
+  });
+}
