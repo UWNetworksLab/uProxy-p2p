be73267fd6d5558a9a0a936886d06b9a492dbfb8
Brandon Wiley <bwiley@google.com>
2015-08-26 11:44:00 -0400

    Use a space before and after equals

--- fancy-transformers/encryptionShaper.ts
+++ fancy-transformers/encryptionShaper.ts
@@ -24,12 +24,12 @@ export class EncryptionShaper implements Transformer {
     log.debug("Configuring encryption shaper");
 
     try {
-      var config=JSON.parse(json);
+      var config = JSON.parse(json);
 
       // Required parameter
       if('key' in config) {
-        var encryptionConfig=<EncryptionConfig>config;
-        this.key_=arraybuffers.hexStringToArrayBuffer(encryptionConfig.key);
+        var encryptionConfig = <EncryptionConfig>config;
+        this.key_ = arraybuffers.hexStringToArrayBuffer(encryptionConfig.key);
       } else {
         log.error('Bad JSON config file');
         log.error(json);
@@ -49,9 +49,9 @@ export class EncryptionShaper implements Transformer {
     // - Generate a new random 16-byte IV for every packet
     // - Encrypt the packet contents with the random IV and symmetric key
     // - Concatenate the IV and encrypted packet contents
-    var iv :ArrayBuffer=EncryptionShaper.makeIV();
-    var encrypted :ArrayBuffer=this.encrypt_(iv, buffer);
-    var parts=[iv, encrypted]
+    var iv :ArrayBuffer = EncryptionShaper.makeIV();
+    var encrypted :ArrayBuffer = this.encrypt_(iv, buffer);
+    var parts = [iv, encrypted]
     return [arraybuffers.concat(parts)];
   }
 
@@ -62,8 +62,8 @@ export class EncryptionShaper implements Transformer {
     // - Decrypt the encrypted packet contents with the IV and symmetric key
     // - Return the decrypted packet contents
     var parts = arraybuffers.split(buffer, 16);
-    var iv=parts[0];
-    var ciphertext=parts[1];
+    var iv = parts[0];
+    var ciphertext = parts[1];
     return [this.decrypt_(iv, ciphertext)];
   }
 
@@ -71,7 +71,7 @@ export class EncryptionShaper implements Transformer {
   public dispose = () :void => {}
 
   static makeIV = () :ArrayBuffer => {
-    var randomBytes=new Uint8Array(16);
+    var randomBytes = new Uint8Array(16);
     crypto.getRandomValues(randomBytes);
     return randomBytes.buffer;
   }
@@ -81,19 +81,19 @@ export class EncryptionShaper implements Transformer {
     var remainder = (len.byteLength + buffer.byteLength) % 16;
     var plaintext:ArrayBuffer;
     if (remainder === 0) {
-      plaintext=arraybuffers.concat([len, buffer]);
+      plaintext = arraybuffers.concat([len, buffer]);
     } else {
       var padding = new Uint8Array(16-remainder);
       crypto.getRandomValues(padding);
-      plaintext=arraybuffers.concat([len, buffer, padding.buffer]);
+      plaintext = arraybuffers.concat([len, buffer, padding.buffer]);
     }
 
     var cbc = new aes.ModeOfOperation.cbc(new Uint8Array(this.key_), new Uint8Array(iv));
     var chunks = arraybuffers.chunk(plaintext, 16);
     for(var x = 0; x < chunks.length; x++) {
-      var plainChunk=arraybuffers.arrayBufferToBuffer(chunks[x]);
-      var cipherChunk=cbc.encrypt(plainChunk);
-      chunks[x]=arraybuffers.bufferToArrayBuffer(cipherChunk);
+      var plainChunk = arraybuffers.arrayBufferToBuffer(chunks[x]);
+      var cipherChunk = cbc.encrypt(plainChunk);
+      chunks[x] = arraybuffers.bufferToArrayBuffer(cipherChunk);
     }
 
     return arraybuffers.concat(chunks);
@@ -103,17 +103,17 @@ export class EncryptionShaper implements Transformer {
     var cbc = new aes.ModeOfOperation.cbc(new Uint8Array(this.key_), new Uint8Array(iv));
     var chunks = arraybuffers.chunk(ciphertext, 16);
     for(var x = 0; x < chunks.length; x++) {
-      chunks[x]=arraybuffers.bufferToArrayBuffer(cbc.decrypt(arraybuffers.arrayBufferToBuffer(chunks[x])));
+      chunks[x] = arraybuffers.bufferToArrayBuffer(cbc.decrypt(arraybuffers.arrayBufferToBuffer(chunks[x])));
     }
 
-    var plaintext=arraybuffers.concat(chunks);
+    var plaintext = arraybuffers.concat(chunks);
 
     var parts = arraybuffers.split(plaintext, 2);
     var lengthBytes = parts[0];
     var length = arraybuffers.decodeShort(lengthBytes);
     var rest = parts[1];
     if(rest.byteLength > length) {
-      parts=arraybuffers.split(rest, length);
+      parts = arraybuffers.split(rest, length);
       return parts[0];
     } else {
       return rest;
