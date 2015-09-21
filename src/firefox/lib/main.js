552b535d94f4ff33590c006eb5eebe81791bda27
Daniel Borkan <dborkan@dborkan-macbookpro2.roam.corp.google.com>
2015-09-21 11:50:05 -0400

    Undo Firefox changes for JPM

--- src/firefox/lib/main.js
+++ src/firefox/lib/main.js
@@ -36,8 +36,8 @@ var init = freedom(manifest, {
   })
 
   // Set up connection between freedom and content script.
-  require('lib/glue.js').setUpConnection(new uproxy(), panel, button);
-  require('lib/url-handler.js').setup(panel, button);
+  require('glue.js').setUpConnection(new uproxy(), panel, button);
+  require('url-handler.js').setup(panel, button);
 });
 
 
