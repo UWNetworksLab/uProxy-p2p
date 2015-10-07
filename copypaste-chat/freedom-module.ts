1a5e52aeb734b8f48447d5209a21966022210310
Trevor Johnston <trevj@google.com>
2015-10-06 21:04:01 -0400

    clean up copypaste chat common code

--- copypaste-chat/freedom-module.ts
+++ copypaste-chat/freedom-module.ts
@@ -10,66 +10,78 @@ var loggingController = freedom['loggingcontroller']();
 loggingController.setDefaultFilter(loggingTypes.Destination.console,
                                    loggingTypes.Level.debug);
 
-export var log :logging.Log = new logging.Log('copypaste chat');
+var log :logging.Log = new logging.Log('copypaste chat');
+
+var parentFreedomModule = freedom();
 
 var config :freedom.RTCPeerConnection.RTCConfiguration = {
-  iceServers: [{urls: ['stun:stun.l.google.com:19302']},
-               {urls: ['stun:stun1.l.google.com:19302']}]
+  iceServers: [
+    {
+      urls: [
+        'stun:stun.l.google.com:19302'
+      ]
+    }
+  ]
 };
-
-var freedomParentModule = freedom();
 
 var pc = peerconnection.createPeerConnection(config);
 
 // Forward signalling channel messages to the UI.
 pc.signalForPeerQueue.setSyncHandler((message:signals.Message) => {
-  // FIXME: Does signalForPeer want a ChurnSignallingMessage?  How is the stage
-  // value supposed to get filled in.
-  freedomParentModule.emit('signalForPeer', message);
+  parentFreedomModule.emit('signalForPeer', JSON.stringify(message));
 });
 
 // Receive signalling channel messages from the UI.
-freedomParentModule.on('handleSignalMessage', (message:signals.Message) => {
-  pc.handleSignalMessage(message);
+parentFreedomModule.on('handleSignalMessage', (message:string) => {
+  try {
+    var decodedMessage = JSON.parse(message);
+    pc.handleSignalMessage(decodedMessage);
+  } catch (e) {
+    log.warn('error parsing signal: %1', message);
+  }
 });
 
 export var connectDataChannel = (channel:peerconnection.DataChannel) => {
-	// Send messages over the datachannel, in response to events from the UI,
-	// and forward messages received on the datachannel to the UI.
-	freedomParentModule.on('send', (message:string) => {
-    channel.send({ str: message }).catch((e:Error) => {
-			log.error('error sending message: ' + e.message);
+  log.info('datachannel open!');
+  // Send messages over the datachannel in response to events from the UI.
+	parentFreedomModule.on('send', (message:string) => {
+    channel.send({
+      str: message
+    }).catch((e:Error) => {
+			log.error('error sending message: %1', e.message);
 		});
 	});
+  // Forward messages received on the datachannel to the UI.
 	channel.dataFromPeerQueue.setSyncHandler((d:peerconnection.Data) => {
 		if (d.str === undefined) {
 			log.error('only text messages are supported');
 			return;
 		}
-		freedomParentModule.emit('receive', d.str);
+		parentFreedomModule.emit('receive', d.str);
 	});
+  parentFreedomModule.emit('ready', {});
 };
 
-// TODO: This is messy...would be great just to have both sides
-//       call onceConnected but it doesn't seem to fire :-/
-pc.peerOpenedChannelQueue.setSyncHandler((channel:peerconnection.DataChannel) => {
-  log.info('peer opened datachannel!');
-	connectDataChannel(channel);
-  freedomParentModule.emit('ready', {});
+// Negotiate a peerconnection when the UI tells us to.
+// This runs on the initiating peer's (getter's) side.
+parentFreedomModule.on('start', () => {
+  pc.negotiateConnection().then(() => {
+    return pc.openDataChannel('text').then(
+        (channel:peerconnection.DataChannel) => {
+  	  connectDataChannel(channel);
+    });
+  }, (e:Error) => {
+    log.error('could not establish connection: %1', e.message);
+    parentFreedomModule.emit('error', {});
+  });
 });
 
-// Negotiate a peerconnection.
-freedomParentModule.on('start', () => {
-  pc.negotiateConnection().then(() => {
-      pc.openDataChannel('text').then((channel:peerconnection.DataChannel) => {
-      log.info('datachannel open!');
-		  connectDataChannel(channel);
-      freedomParentModule.emit('ready', {});
-    }, (e) => {
-      log.error('could not setup datachannel: ' + e.message);
-      freedomParentModule.emit('error', {});
-    });
-  }, (e) => {
-    log.error('could not negotiate peerconnection: ' + e.message);
-  });
+// This fires on the non-initiating peer's (giver's) side.
+pc.peerOpenedChannelQueue.setSyncHandler(
+    (channel:peerconnection.DataChannel) => {
+  connectDataChannel(channel);
+});
+
+pc.onceConnected.then(() => {
+  log.info('peerconnection established!');
 });
--- copypaste-chat/main.core-env.ts
+++ copypaste-chat/main.core-env.ts
@@ -1,12 +1,5 @@
 /// <reference path='../../../third_party/typings/freedom/freedom-core-env.d.ts' />
 
-import churn_types = require('../churn/churn.types');
-import ChurnSignallingMessage = churn_types.ChurnSignallingMessage;
-
-export interface OnEmitModule extends freedom.OnAndEmit<any,any> {};
-export interface OnEmitModuleFactory extends
-  freedom.FreedomModuleFactoryManager<OnEmitModule> {};
-
 var startButton = document.getElementById("startButton");
 var copyTextarea = <HTMLInputElement>document.getElementById("copy");
 var pasteTextarea = <HTMLInputElement>document.getElementById("paste");
@@ -16,57 +9,41 @@ var sendArea = <HTMLInputElement>document.getElementById("sendArea");
 var receiveArea = <HTMLInputElement>document.getElementById("receiveArea");
 
 // Platform-specific function to load the freedomjs module (glue.js).
-declare var loadModule: () => Promise<freedom.OnAndEmit<any, any>>;
+declare var loadModule:() => Promise<freedom.OnAndEmit<any, any>>;
 
-loadModule().then((copypasteChurnChat:freedom.OnAndEmit<any,any>) => {
+loadModule().then((chat:freedom.OnAndEmit<any,any>) => {
+  startButton.onclick = () => {
+    chat.emit('start', {});
+  }
+
+  sendButton.onclick = () => {
+    chat.emit('send', sendArea.value);
+  }
+
   // Dispatches each line from the paste box as a signalling channel message.
-  function handleSignallingMessages() {
+  receiveButton.onclick = () => {
     var signals = pasteTextarea.value.split('\n');
-    for (var i = 0; i < signals.length; i++) {
-      var s:string = signals[i];
-      // Ignore blank lines.
-      if (s) {
-        var signal:ChurnSignallingMessage = JSON.parse(s);
-        copypasteChurnChat.emit('handleSignalMessage', signal);
-      }
-    }
-
-    // "Flush" the signalling channel input.
-    pasteTextarea.value = '';
+    signals.forEach((signal:string) => {
+      chat.emit('handleSignalMessage', signal);
+    });
+    pasteTextarea.disabled = true;
   }
 
-  function start() {
-    copypasteChurnChat.emit('start', {});
-  }
-
-  startButton.onclick = start;
-  receiveButton.onclick = handleSignallingMessages;
-
-  copypasteChurnChat.on('signalForPeer', (signal:ChurnSignallingMessage) => {
-    copyTextarea.value = copyTextarea.value.trim() + '\n' + JSON.stringify(signal);
+  chat.on('signalForPeer', (signal:string) => {
+    copyTextarea.value = copyTextarea.value.trim() + '\n' + signal;
   });
 
-  copypasteChurnChat.on('ready', function() {
-    console.log('peer connection established!');
-    // Hide remaining signalling channel contents.
-    copyTextarea.value = '';
+  chat.on('ready', () => {
     sendArea.disabled = false;
   });
 
-  copypasteChurnChat.on('error', function() {
-    console.error('something went wrong with the peer connection');
-    sendArea.disabled = true;
-  });
-
-  sendButton.onclick = function() {
-    // Currently, PeerConnection does not support empty text messages:
-    //   https://github.com/freedomjs/freedom/issues/67
-    copypasteChurnChat.emit('send', sendArea.value || '(empty message)');
-  }
-
-  copypasteChurnChat.on('receive', function(message:string) {
+  chat.on('receive', (message:string) => {
     receiveArea.value = message;
   });
+
+  chat.on('error', () => {
+    sendArea.disabled = true;
+  });
 }, (e:Error) => {
-  console.error('could not load freedom: ' + e.message);
+  console.error('could not load freedom module: ' + e.message);
 });
