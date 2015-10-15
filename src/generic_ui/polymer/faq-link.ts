5aba520ae3233ee928dd4c6f17b628a365c813c6
Qi Zhang <zhang_qi@wheatoncollege.edu>
2015-10-15 01:06:11 -0500

    Fix issue #1787

--- src/generic_ui/polymer/faq-link.ts
+++ src/generic_ui/polymer/faq-link.ts
@@ -1,10 +1,34 @@
-/// <reference path='../../../../third_party/polymer/polymer.d.ts' />
 /// <reference path='./context.d.ts' />
+/// <reference path='../../../../third_party/polymer/polymer.d.ts' />
+
+import uproxy_core_api = require('../../interfaces/uproxy_core_api');
 
 Polymer({
-  anchor: '',
-  openFaq: function() {
-    ui_context.ui.openTab('generic_ui/faq.html#' + this.anchor);
+  close: function() {
+    this.$.faqPanel.close();
   },
-  ready: function() {}
+  open: function() {
+    this.$.faqPanel.open();
+  },
+  ready: function() {
+    this.ui = ui_context.ui;
+    this.model = ui_context.model;
+  }
 });
+
+console.log("0");
+
+window.onload = function() {
+  var backToTop = document.getElementsByClassName("top");
+  console.log("1");
+  console.log(backToTop);
+  for (var i = 0, j = backToTop.length; i < j; i++) {
+    console.log("2");
+
+    backToTop[i].addEventListener("click", function() {
+      window.scroll(0, 0);
+    });
+    console.log("3");
+
+  }
+};
