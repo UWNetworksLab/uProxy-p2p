31f587add09af9926aada44454343f97367ada46
Lucy He <lucydhe@gmail.com>
2015-10-23 14:45:39 -0400

    Create new view for gmail invites, port new splash page to invite-user page

--- src/generic_ui/polymer/google-invite-user.ts
+++ src/generic_ui/polymer/google-invite-user.ts
@@ -88,14 +88,10 @@ Polymer({
   onNetworkSelect: function(e :any, details :any) {
     if (details.isSelected) {
       this.selectedNetworkName = details.item.getAttribute('label');
-      //this.$.loginToInviteFriendDialog.open();
+      this.$.loginToInviteFriendDialog.open();
     }
   },
   openInviteUserPanel: function() {
-    this.setOnlineInviteNetworks();
-    // Reset selectedNetworkName in case it had been set and that network
-    // is no longer online.
-    this.$.networkSelectMenu.selectIndex(0);
     this.$.inviteUserPanel.open();
   },
   closeInviteUserPanel: function() {
@@ -105,75 +101,10 @@ Polymer({
   showAcceptUserInvite: function() {
     this.fire('core-signal', { name: 'open-accept-user-invite-dialog' });
   },
-  setOnlineInviteNetworks: function() {
-    this.onlineInviteNetworks = [];
-    for (var i = 0; i < model.networkNames.length; ++i) {
-      var name = model.networkNames[i];
-      if (ui.supportsInvites(name)) {
-        this.onlineInviteNetworks.push({
-          name: name,
-          displayName: ui.getNetworkDisplayName(name)
-        });
-      }
-    }
-  },
-  loginToQuiver: function() {
-    model.globalSettings.quiverUserName = this.userName;
-    core.updateGlobalSettings(model.globalSettings);
-    this.login('Quiver', this.userName);
-  },
-  iconTapped: function(event: Event, detail: Object, target: HTMLElement) {
-    var networkName = target.getAttribute('data-network');
-    this.selectedNetworkName = networkName;
-    this.$.loginToInviteFriendDialog.open();
-  },
-  loginTapped: function(event: Event, detail: Object, target: HTMLElement) {
-    var networkName = target.getAttribute('data-network');
-    this.login(networkName);
-  },
-  login: function(networkName :string, userName ?:string) {
-    ui.login(networkName, userName).then(() => {
-      // syncNetwork will update the view to the ROSTER.
-      ui.bringUproxyToFront();
-      this.$.loginToInviteFriendDialog.close();
-      console.log('networkName');
-      if (networkName == "GMail") {
-        console.log("firing signal");
-        this.fire('core-signal', { name: 'open-google-invite-dialog' });
-      }
-    }).catch((e: Error) => {
-      console.warn('Did not log in ', e);
-    });
-  },
-  getNetworkDisplayName: function(name :string) {
-    return ui.getNetworkDisplayName(name);
-  },
-  isExperimentalNetwork: function(name :string) {
-    return ui.isExperimentalNetwork(name);
-  },
-  updateNetworkButtonNames: function() {
-    var supportsQuiver = false;
-    this.networkButtonNames = [];
-    for (var i = 0; i < model.networkNames.length; ++i) {
-      if (model.networkNames[i] === 'Quiver') {
-        supportsQuiver = true;
-      } else {
-        this.networkButtonNames.push(model.networkNames[i]);
-      }
-    }
-    // Only set .supportsQuiver after iterating through all networks, to prevent
-    // any flicker in case we switch from true to false to true again.
-    this.supportsQuiver = supportsQuiver;
-  },
-  observe: {
-    'model.networkNames': 'updateNetworkButtonNames'
-  },
   ready: function() {
-    this.userName = model.globalSettings.quiverUserName;
-    this.updateNetworkButtonNames();
     this.inviteUrl = '';
     this.inviteUserEmail = '';
-    this.selectedNetworkName = '';
+    this.selectedNetworkName = 'GMail';
     this.model = model;
     this.userIdInput = '';
   }
