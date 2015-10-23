00d06320119cb361884d4141e4011e6a3360682c
Raymond Cheng <ryscheng@users.noreply.github.com>
2015-10-23 15:33:11 -0400

    Removing console.log

--- samples/zork-node/index.js
+++ samples/zork-node/index.js
@@ -1,10 +1,8 @@
 var freedom = require("freedom-for-node");
-console.log('Loading sample app');
 freedom.freedom('./uproxy-lib/zork/freedom-module.json', {
   'logger': './uproxy-lib/loggingprovider/freedom-module.json',
   'debug': 'debug'
 }).then(function(moduleFactory) {
-  console.log("freedomjs module created");
   moduleFactory();
 }, function() {
   console.error('could not load freedomjs module');
