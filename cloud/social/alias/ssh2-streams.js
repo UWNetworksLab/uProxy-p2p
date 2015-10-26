cf50124654aa60d783e6e88a7a620cf917b7043e
Trevor Johnston <trevj@google.com>
2015-10-26 17:48:22 -0400

    add ssh2

---
+++ cloud/social/alias/ssh2-streams.js
@@ -0,0 +1,11 @@
+// This is identical to ssh2-streams' index.js except for SFTPStream.
+// We do this because sftp.js contains a call to process.bindings
+// that raises an error in the browser. This is safe because we don't
+// actually care about using SFTP, it's just that ssh2's Client sets
+// some constants to point to some of SFTPStream's constants.
+module.exports = {
+  SFTPStream: {},
+  SSH2Stream: require('ssh2/node_modules/ssh2-streams/lib/ssh'),
+  utils: require('ssh2/node_modules/ssh2-streams/lib/utils'),
+  constants: require('ssh2/node_modules/ssh2-streams/lib/constants')
+};
