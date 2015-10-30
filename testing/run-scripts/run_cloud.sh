f162a518635c1732981e158f996a5e5a6a5c893b
Trevor Johnston <trevj@google.com>
2015-10-30 16:55:29 -0400

    bootstrap cloud instances with an invite code

--- testing/run-scripts/run_cloud.sh
+++ testing/run-scripts/run_cloud.sh
@@ -9,19 +9,28 @@ source "${BASH_SOURCE%/*}/utils.sh" || (echo "cannot find utils.sh" && exit 1)
 PREBUILT=
 SSHD_PORT=5000
 CONTAINER_PREFIX="uproxy"
+# Users testing on a LAN will want to override this with their
+# internal, non-routable IP (or, more simply, "localhost").
+# Everyone else will want their public, external, routable IP.
+# This beautiful, cross-platform one-liner gives us this.
+# Cogged from:
+#   http://unix.stackexchange.com/questions/22615/how-can-i-get-my-external-ip-address-in-bash
+CLOUD_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
 function usage () {
-  echo "$0 [-p path] browser-version"
+  echo "$0 [-p path] [-h hostname] browser-version"
   echo "  -p: path to pre-built uproxy-lib repository"
+  echo "  -i: IP or hostname of the cloud instance"
   echo "  -h, -?: this help message"
   echo
   echo "Example browser-version: chrome-stable, firefox-canary"
   exit 1
 }
 
-while getopts p:h? opt; do
+while getopts p:i:h? opt; do
   case $opt in
     p) PREBUILT="$OPTARG" ;;
+    i) CLOUD_IP="$OPTARG" ;;
     *) usage ;;
   esac
 done
@@ -67,11 +76,29 @@ remove_container cloud
 docker run --net=host $HOSTARGS --name $CONTAINER_PREFIX-cloud -d uproxy/$1 /test/bin/load-zork.sh $RUNARGS -w > /dev/null
 
 # Start a container for sshd, linked with Zork's.
+# If there is no image, bootstrap an invite code and build one.
 if [ "X$(docker images | tail -n +2 | awk '{print $1}' | grep uproxy/sshd )X" == "Xuproxy/sshdX" ]
 then
   echo "$CONTAINER_PREFIX-sshd image already exists, re-using"
 else
-  docker build -t uproxy/sshd ${BASH_SOURCE%/*}/../../sshd
+  TMP_DIR=/tmp/uproxy-sshd
+  cp -R ${BASH_SOURCE%/*}/../../sshd $TMP_DIR
+
+  # 21 characters leaves no = at the end, which is generally easier to double click.
+  GIVER_PW=`openssl rand -base64 21`
+  echo -n $GIVER_PW > $TMP_DIR/giverpw
+  docker build -t uproxy/sshd $TMP_DIR
+
+  # TODO: invoke a script on the container, duplicating this code is dumb. 
+  UNENCODED_TOKEN="{\"host\":\"$CLOUD_IP\", \"user\":\"giver\", \"pass\":\"$GIVER_PW\"}"
+  if uname|grep Darwin > /dev/null
+  then
+    ENCODED_TOKEN=`echo -n $UNENCODED_TOKEN|base64`
+  else
+    ENCODED_TOKEN=`echo -n $UNENCODED_TOKEN|base64 -w 0`
+  fi
+  echo "password, for shell access: $GIVER_PW"
+  echo "invite code, for uProxy: $ENCODED_TOKEN"
 fi
 remove_container sshd
 
