dd37ccd3069c2c9f4d29c05f552c03a63890208b
Lucy He <lucydhe@gmail.com>
2015-11-02 18:22:31 -0500

    Fix FAQ css, scroll to questions

--- src/generic_ui/polymer/faq.ts
+++ src/generic_ui/polymer/faq.ts
@@ -3,15 +3,29 @@
 
 import uproxy_core_api = require('../../interfaces/uproxy_core_api');
 
+interface AnchorElement extends Element {
+  scrollIntoView(top?: boolean): void;
+}
+
 Polymer({
   close: function() {
     this.$.faqPanel.close();
   },
-  open: function(data :{anchor :string}) {
+  open: function(e: Event, detail: {anchor: string}) {
     this.$.faqPanel.open();
-    // TODO scroll to the anchor
+    this.anchor = detail.anchor;
+  },
+  scroll: function(e: Event) {
+    var anchor = this.anchor;
+    var elemTapped :HTMLElement = <HTMLElement>e.target;
+    if (elemTapped && elemTapped.getAttribute('data-anchor')) {
+      anchor = elemTapped.getAttribute('data-anchor');
+    }
+    var anchorElem :AnchorElement = <AnchorElement>document.querySelector("html /deep/ #" + anchor);
+    anchorElem.scrollIntoView();
   },
   ready: function() {
+    this.anchor = '';
     this.ui = ui_context.ui;
     this.model = ui_context.model;
   }
