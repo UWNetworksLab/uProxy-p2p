63c99e146f144c7feaf75b69f09137657025cf21
Trevor Johnston <trevj@google.com>
2015-11-12 18:31:46 -0500

    add private keys to giver invite codes

--- sshd/set_giver_access.sh
+++ sshd/set_giver_access.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Sets the giver account's password, given an invite code.
-# If no invite code is available then one will be generated.
+# Configures giver access, given an invite code.
+# If no invite code is available then one is generated
+# and saved to the supplied path.
 #
 # Intended to be run only as part of the deploy process.
+#
+# TODO: remove passwords, they're only for super-old clients during development
 
 set -e
 
@@ -47,19 +50,43 @@ fi
 if [ -s $1 ]
 then
   INVITE=`cat $1|base64 -d`
+
   PASS=`echo $INVITE|jq -r .pass`
   if [ "$PASS" == "null" ]
   then
     echo "invite code does not contain a password" 2>&1
     exit 1
   fi
+
+  ENCODED_KEY=`echo $INVITE|jq -r .key`
+  echo "encoded: $ENCODED_KEY"
+  if [ "$ENCODED_KEY" == "null" ]
+  then
+    echo "invite code does not contain a private key" 2>&1
+    exit 1
+  fi
+  echo -n $ENCODED_KEY|base64 -d > /giver
+  cat /giver
+  chmod 600 /giver
+  ssh-keygen -y -f /giver > /giver.pub
 else
   PASS=`openssl rand -base64 20`
 
+  # TODO: 2048 bits makes for really long keys so we should use
+  #       ecdsa when ssh2-streams supports it:
+  #         https://github.com/mscdex/ssh2-streams/issues/3
+  ssh-keygen -t rsa -b 2048 -N '' -f /giver
+
   # Save a full invite code, which can be output to the user by the host.
-  INVITE="{\"host\":\"$PUBLIC_IP\", \"user\":\"giver\", \"pass\":\"$PASS\"}"
+  # TODO: Because SSH keys are already base64-encoded, re-encoding them
+  #       like this is very inefficient.
+  ENCODED_KEY=`base64 -w 0 /giver`
+  INVITE="{\"host\":\"$PUBLIC_IP\", \"user\":\"giver\", \"pass\":\"$PASS\", \"key\":\"$ENCODED_KEY\"}"
   INVITE_CODE=`echo -n $INVITE|base64 -w 0`
   echo -n "$INVITE_CODE" > $1
 fi
 
 echo giver:"$PASS" | chpasswd
+
+mkdir -p ~giver/.ssh
+cat /giver.pub > ~giver/.ssh/authorized_keys
