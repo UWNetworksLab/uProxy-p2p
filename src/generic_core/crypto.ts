fd4ba427d7ee6aff2955d27bdd28a85b6fdf1306
Laura Rokita <lrokita@lrokita1.nyc.corp.google.com>
2015-12-16 16:05:47 -0500

    added logging to crypto.ts to measure how long en/decryption takes

--- src/generic_core/crypto.ts
+++ src/generic_core/crypto.ts
@@ -8,20 +8,34 @@ import pgp = globals.pgp;
 // Sign with private key and encrypt with encryptKey
 export function signEncrypt(plainText :string, encryptKey :string) : Promise<string> {
   var ab = arraybuffers.stringToArrayBuffer(plainText);
+  var startTime = new Date();
+  var id = startTime.getTime();
+  console.log(id + " Encryption: starting at " + startTime);
   return pgp.signEncrypt(ab, encryptKey).then((cipherData :ArrayBuffer) => {
+    var endTime = new Date();
+    console.log(id + " Encryption: completed at " + endTime);
+    console.log(id + " Encryption: time elapsed " + - (id - endTime.getTime()));
     return pgp.armor(cipherData);
   }).catch((e :Error) => {
+    console.log(id + "Encryption: failed at " + new Date());
     return Promise.reject('Error in signEncrypt ' + e);
   });
 }
 
 // Decrypt with private key and verify with verifyKey
 export function verifyDecrypt(cipherText :string, verifyKey :string) : Promise<string> {
+  var startTime = new Date();
+  var id = startTime.getTime();
+  console.log(id + " Decryption: starting at " + startTime);
   return pgp.dearmor(cipherText).then((cipherData :ArrayBuffer) => {
     return pgp.verifyDecrypt(cipherData, verifyKey);
   }).then((result :freedom.PgpProvider.VerifyDecryptResult) => {
+    var endTime = new Date();
+    console.log(id + " Decryption: completed at " + endTime);
+    console.log(id + " Decryption: time elapsed " + - (id - endTime.getTime()));
     return arraybuffers.arrayBufferToString(result.data);
   }).catch((e :Error) => {
+    console.log("Decryption: failed at " + new Date());
     return Promise.reject('Error in verifyDecrypt ' + e);
   });
 }
