55497f210f343b818296fd197c8d8beb15f6a5bb
soycode <soycode@users.noreply.github.com>
2016-01-14 14:40:39 -0800

    cloud installs uproxy-lib from npm

--- testing/integration/test/bin/load-zork.sh
+++ testing/integration/test/bin/load-zork.sh
@@ -19,6 +19,9 @@ while getopts b:r:lpvh? opt; do
         p)
             PREBUILT=true
             ;;
+        n)
+            NPM=true
+            ;;
         v)
             RUNVNC=true
             ;;
@@ -30,6 +33,7 @@ while getopts b:r:lpvh? opt; do
             echo "  -b: BRANCH is the branch to checkout instead of HEAD's referant."
             echo "  -r: REPO is the repository to clone instead of github.com/uProxy/uproxy-lib."
             echo "  -p: use a pre-built uproxy-lib repo (overrides -b and -r)."
+            echo "  -n: install uproxy-lib from npm (overrides -b, -r, and -p)"
             echo "  -v: run a vncserver (port 5900 in the instance)"
             echo "  -l: wait until the extension is listening."
             exit 1;
@@ -47,14 +51,19 @@ if $RUNVNC; then
     x11vnc -display :10 -forever &
 fi
 
-if ! $PREBUILT; then
+if [[! $PREBUILT; ]] || $NPM; then
     mkdir -p /test/src
     cd /test/src
     npm install -g bower grunt-cli
-    echo git clone $CLONEARGS $CLONESRC
-    git clone $CLONEARGS $CLONESRC
-    cd uproxy-lib
-    ./setup.sh install
+    if NPM; then
+        cp -r node_modules/uproxy-lib .
+        cd uproxy-lib
+    elif ! $PREBUILT; then
+        echo git clone $CLONEARGS $CLONESRC
+        git clone $CLONEARGS $CLONESRC
+        cd uproxy-lib
+        ./setup.sh install
+    fi
     grunt zork
 fi
 
--- testing/run-scripts/run_cloud.sh
+++ testing/run-scripts/run_cloud.sh
@@ -118,7 +118,7 @@ if ! docker ps -a | grep uproxy-zork >/dev/null; then
   then
       RUNARGS="$RUNARGS -p"
   fi
-  docker run --restart=always --net=host  $HOSTARGS --name uproxy-zork -d uproxy/$1 /test/bin/load-zork.sh $RUNARGS
+  docker run --restart=always --net=host  $HOSTARGS --name uproxy-zork -d uproxy/$1 /test/bin/load-zork.sh $RUNARGS -n
 fi
 
 # Start sshd, if necessary.
