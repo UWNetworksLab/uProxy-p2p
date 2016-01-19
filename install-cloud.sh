e1df7cf4e65b0b44a70a29be9bb8dad88675ad8b
Trevor Johnston <trevj@google.com>
2016-01-19 16:07:18 -0500

    add a script for one-line install via curl

---
+++ install-cloud.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Quick and easy install via curl and wget.
+# 
+# All of the heavy lifting is done by run_cloud.sh: this
+# script just clones the uproxy-docker repo and executes
+# run_cloud.sh from there.
+#
+# Heavily based on Docker's installer at:
+#   https://get.docker.com/.
+
+set -e
+
+do_install() {
+  TMP_DIR=`mktemp -d`
+  git clone --depth 1 https://github.com/uProxy/uproxy-docker.git $TMP_DIR
+  cd $TMP_DIR/testing/run-scripts
+
+   # TODO: pass arguments, e.g. banner
+  ./run_cloud.sh firefox-stable
+}
+
+# Wrapped in a function for some protection against half-downloads.
+do_install
