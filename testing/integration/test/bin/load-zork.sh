5235f0b47aeb7e9efe42d94b6000f6291a5d3043
Trevor Johnston <trevj@google.com>
2016-01-26 21:20:07 -0500

    restrict access to zork via iptables, on cloud

--- testing/integration/test/bin/load-zork.sh
+++ testing/integration/test/bin/load-zork.sh
@@ -9,7 +9,8 @@ CLONEARGS=
 CLONESRC=https://github.com/uProxy/uproxy-lib.git
 PREBUILT=false
 NPM=false
-while getopts b:r:nlpvh? opt; do
+IPTABLES=false
+while getopts b:r:nlpvz:h? opt; do
     case $opt in
         b)
             CLONEARGS="$CLONEARGS -b $OPTARG"
@@ -29,14 +30,18 @@ while getopts b:r:nlpvh? opt; do
         l)
             LISTEN=true
             ;;
+        z)
+            IPTABLES=$OPTARG
+            ;;
         *)
-            echo "$0 [-b branch] [-r repo] [-n] [-l] [-p] [-v] [-h]"
+            echo "$0 [-b branch] [-r repo] [-z true|false] [-n] [-l] [-p] [-v] [-h]"
             echo "  -b: BRANCH is the branch to checkout instead of HEAD's referant."
             echo "  -r: REPO is the repository to clone instead of github.com/uProxy/uproxy-lib."
             echo "  -n: install uproxy-lib from npm (overrides -b, -r, and -p)"
             echo "  -l: wait until the extension is listening."
             echo "  -p: use a pre-built uproxy-lib repo (overrides -b and -r)."
             echo "  -v: run a vncserver (port 5900 in the instance)"
+            echo "  -z: restrict access to port 9000 via iptables (default: false)"
             echo "  -h, -?: this help message"
             exit 1;
             ;;
@@ -68,6 +73,22 @@ if [ $PREBUILT = false ]; then
         ./setup.sh install
         grunt zork
     fi
+fi
+
+if [ "$IPTABLES" = true ]
+then
+  if ! iptables -v -L INPUT|grep 9000|grep docker0 >/dev/null
+  then
+    # Restrict access to zork to connections originating from
+    # localhost and our own Docker containers. Note that doing
+    # this inside a Docker container is *VERY WEIRD* and
+    # potentially *DANGEROUS*. However, we do it on cloud
+    # because the Zork container runs with --net=host and
+    # without this, Zork's command port would remain publically
+    # accessible.
+    iptables -A INPUT -p tcp -i lo --dport 9000 -j ACCEPT
+    iptables -A INPUT -p tcp ! -i docker0 --dport 9000 -j REJECT
+  fi
 fi
 
 /usr/bin/supervisord -n -c /test/etc/supervisord-$BROWSER.conf
