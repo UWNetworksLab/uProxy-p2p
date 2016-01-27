a1c8964decf37b7932c1ceab1e1e3ba1d7f1dc09
soycode <soycode@users.noreply.github.com>
2016-01-27 14:07:56 -0800

    add -u argument for updating cloud installs

--- testing/run-scripts/run_cloud.sh
+++ testing/run-scripts/run_cloud.sh
@@ -14,39 +14,42 @@ PREBUILT=
 NPM=true
 INVITE_CODE=
 REFRESH=false
+UPDATE=false
 PUBLIC_IP=
 BANNER=
 
 SSHD_PORT=5000
 
 function usage () {
-  echo "$0 [-p path] [-i invite code] [-r] [-d ip] [-b banner] browser-version"
-  echo "  -p: path to pre-built uproxy-lib repository"
-  echo "  -i: invite code"
-  echo "  -r: recreate Docker images (WARNING: will invalidate invite codes)"
-  echo "  -d: override the detected public IP (for development only)"
-  echo "  -b: name to use in contacts list"
-  echo "  -h, -?: this help message"
-  echo
-  echo "Example browser-version: chrome-stable, firefox-canary"
-  exit 1
+    echo "$0 [-p path] [-i invite code] [-r] [-u] [-d ip] [-b banner] browser-version"
+    echo "  -p: path to pre-built uproxy-lib repository"
+    echo "  -i: invite code"
+    echo "  -r: recreate Docker images (WARNING: will invalidate invite codes)"
+    echo "  -u: update uProxy (overrides -r)"
+    echo "  -d: override the detected public IP (for development only)"
+    echo "  -b: name to use in contacts list"
+    echo "  -h, -?: this help message"
+    echo
+    echo "Example browser-version: chrome-stable, firefox-canary"
+    exit 1
 }
 
-while getopts p:i:rd:b:h? opt; do
-  case $opt in
-    p) PREBUILT="$OPTARG" ;;
-    i) INVITE_CODE="$OPTARG" ;;
-    r) REFRESH=true ;;
-    d) PUBLIC_IP="$OPTARG" ;;
-    b) BANNER="$OPTARG" ;;
-    *) usage ;;
-  esac
+while getopts p:i:rud:b:h? opt; do
+    case $opt in
+        p) PREBUILT="$OPTARG" ;;
+        i) INVITE_CODE="$OPTARG" ;;
+        r) REFRESH=true ;;
+        u) UPDATE=true ;;
+        d) PUBLIC_IP="$OPTARG" ;;
+        b) BANNER="$OPTARG" ;;
+        *) usage ;;
+    esac
 done
 shift $((OPTIND-1))
 
 if [ $# -lt 1 ]
 then
-  usage
+    usage
 fi
 
 # Set the cloud instance's banner.
@@ -56,16 +59,16 @@ fi
 #  - server's hostname
 if [ -z "$BANNER" ]
 then
-  if docker ps -a | grep uproxy-sshd >/dev/null
-  then
-    if [ `docker inspect --format='{{ .State.Status }}' uproxy-sshd` != "running" ]
+    if docker ps -a | grep uproxy-sshd >/dev/null
     then
-      docker start uproxy-sshd > /dev/null
+        if [ `docker inspect --format='{{ .State.Status }}' uproxy-sshd` != "running" ]
+        then
+            docker start uproxy-sshd > /dev/null
+        fi
+        BANNER=`docker exec uproxy-sshd cat /banner`
+    else
+        BANNER=`hostname`
     fi
-    BANNER=`docker exec uproxy-sshd cat /banner`
-  else
-    BANNER=`hostname`
-  fi
 fi
 
 # Set the cloud instance's hostname.
@@ -75,89 +78,95 @@ fi
 #  - pull from DNS
 if [ -z "$PUBLIC_IP" ]
 then
-  if docker ps -a | grep uproxy-sshd >/dev/null
-  then
-    if [ `docker inspect --format='{{ .State.Status }}' uproxy-sshd` != "running" ]
+    if docker ps -a | grep uproxy-sshd >/dev/null
     then
-      docker start uproxy-sshd > /dev/null
+        if [ `docker inspect --format='{{ .State.Status }}' uproxy-sshd` != "running" ]
+        then
+            docker start uproxy-sshd > /dev/null
+        fi
+        # Don't fail if the current installation has no /hostname.
+        PUBLIC_IP=`docker exec uproxy-sshd cat /hostname || echo -n ""`
     fi
-    # Don't fail if the current installation has no /hostname.
-    PUBLIC_IP=`docker exec uproxy-sshd cat /hostname || echo -n ""`
-  fi
-  if [ -z "$PUBLIC_IP" ]
-  then
-    # Beautiful cross-platform one-liner cogged from:
-    #   http://unix.stackexchange.com/questions/22615/how-can-i-get-my-external-ip-address-in-bash
-    PUBLIC_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-  fi
+    if [ -z "$PUBLIC_IP" ]
+    then
+        # Beautiful cross-platform one-liner cogged from:
+        #   http://unix.stackexchange.com/questions/22615/how-can-i-get-my-external-ip-address-in-bash
+        PUBLIC_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+    fi
+fi
+
+if [ $UPDATE = true ]
+then
+    INVITE_CODE=`docker cp uproxy-sshd:/initial-giver-invite-code -|tar xO`
+    REFRESH=true  # want to remove existing containers
 fi
 
 if [ $REFRESH = true ]
 then
-  docker rm -f uproxy-sshd || true
-  docker rm -f uproxy-zork || true
-  docker rmi uproxy/sshd || true
-  # TODO: This will fail if there are any containers using the
-  #       image, e.g. run_pair.sh. Regular cloud users won't be.
-  docker rmi uproxy/$1 || true
+    docker rm -f uproxy-sshd || true
+    docker rm -f uproxy-zork || true
+    docker rmi uproxy/sshd || true
+    # TODO: This will fail if there are any containers using the
+    #       image, e.g. run_pair.sh. Regular cloud users won't be.
+    docker rmi uproxy/$1 || true
 fi
 
 # Start Zork, if necessary.
 if ! docker ps -a | grep uproxy-zork >/dev/null; then
-  if ! docker images | grep uproxy/$1 >/dev/null; then
-    BROWSER=$(echo $1 | cut -d - -f 1)
-    VERSION=$(echo $1 | cut -d - -f 2)
-    ${BASH_SOURCE%/*}/image_make.sh $BROWSER $VERSION
-  fi
-  HOSTARGS=
-  if [ ! -z "$PREBUILT" ]
-  then
-    NPM=false
-    HOSTARGS="$HOSTARGS -v $PREBUILT:/test/src/uproxy-lib"
-  fi
-  RUNARGS=
-  if [ ! -z "$PREBUILT" ]
-  then
-      RUNARGS="$RUNARGS -p"
-  fi
-  if [ $NPM = true ]
-  then
-      RUNARGS="$RUNARGS -n"
-  fi
-  # NET_ADMIN is required to run iptables inside the container.
-  # Full list of capabilities:
-  #   https://docs.docker.com/engine/reference/run/#runtime-privilege-linux-capabilities-and-lxc-configuration
-  docker run --restart=always --net=host --cap-add NET_ADMIN $HOSTARGS --name uproxy-zork -d uproxy/$1 /test/bin/load-zork.sh $RUNARGS -z true
+    if ! docker images | grep uproxy/$1 >/dev/null; then
+        BROWSER=$(echo $1 | cut -d - -f 1)
+        VERSION=$(echo $1 | cut -d - -f 2)
+        ${BASH_SOURCE%/*}/image_make.sh $BROWSER $VERSION
+    fi
+    HOSTARGS=
+    if [ ! -z "$PREBUILT" ]
+    then
+        NPM=false
+        HOSTARGS="$HOSTARGS -v $PREBUILT:/test/src/uproxy-lib"
+    fi
+    RUNARGS=
+    if [ ! -z "$PREBUILT" ]
+    then
+        RUNARGS="$RUNARGS -p"
+    fi
+    if [ $NPM = true ]
+    then
+        RUNARGS="$RUNARGS -n"
+    fi
+    # NET_ADMIN is required to run iptables inside the container.
+    # Full list of capabilities:
+    #   https://docs.docker.com/engine/reference/run/#runtime-privilege-linux-capabilities-and-lxc-configuration
+    docker run --restart=always --net=host --cap-add NET_ADMIN $HOSTARGS --name uproxy-zork -d uproxy/$1 /test/bin/load-zork.sh $RUNARGS -z true
 fi
 
 # Start sshd, if necessary.
 if ! docker ps -a | grep uproxy-sshd >/dev/null; then
-  if ! docker images | grep uproxy/sshd >/dev/null; then
-    TMP_DIR=/tmp/uproxy-sshd
-    rm -fR $TMP_DIR
-    cp -R ${BASH_SOURCE%/*}/../../sshd/ $TMP_DIR
+    if ! docker images | grep uproxy/sshd >/dev/null; then
+        TMP_DIR=/tmp/uproxy-sshd
+        rm -fR $TMP_DIR
+        cp -R ${BASH_SOURCE%/*}/../../sshd/ $TMP_DIR
 
-    echo -n "$BANNER" > $TMP_DIR/banner
-    echo -n "$PUBLIC_IP" > $TMP_DIR/hostname
+        echo -n "$BANNER" > $TMP_DIR/banner
+        echo -n "$PUBLIC_IP" > $TMP_DIR/hostname
 
-    # Optional build args aren't very flexible...confine the messiness here.
-    ISSUE_INVITE_ARGS=
-    if [ -n "$INVITE_CODE" ]
-    then
-      ISSUE_INVITE_ARGS="$ISSUE_INVITE_ARGS -i $INVITE_CODE"
+        # Optional build args aren't very flexible...confine the messiness here.
+        ISSUE_INVITE_ARGS=
+        if [ -n "$INVITE_CODE" ]
+        then
+            ISSUE_INVITE_ARGS="$ISSUE_INVITE_ARGS -i $INVITE_CODE"
+        fi
+        docker build --build-arg issue_invite_args="$ISSUE_INVITE_ARGS" -t uproxy/sshd $TMP_DIR
     fi
-    docker build --build-arg issue_invite_args="$ISSUE_INVITE_ARGS" -t uproxy/sshd $TMP_DIR
-  fi
 
-  # Add an /etc/hosts entry to the Zork container.
-  # Because the Zork container runs with --net=host, we can't use the
-  # regular, ever-so-slightly-more-elegant Docker notation.
-  HOST_IP=`ip -o -4 addr list docker0 | awk '{print $4}' | cut -d/ -f1`
-  docker run --restart=always -d -p $SSHD_PORT:22 --name uproxy-sshd --add-host zork:$HOST_IP uproxy/sshd > /dev/null
+    # Add an /etc/hosts entry to the Zork container.
+    # Because the Zork container runs with --net=host, we can't use the
+    # regular, ever-so-slightly-more-elegant Docker notation.
+    HOST_IP=`ip -o -4 addr list docker0 | awk '{print $4}' | cut -d/ -f1`
+    docker run --restart=always -d -p $SSHD_PORT:22 --name uproxy-sshd --add-host zork:$HOST_IP uproxy/sshd > /dev/null
 
-  echo -n "Waiting for Zork to come up..."
-  while ! ((echo ping ; sleep 0.5) | nc -w 1 $HOST_IP 9000 | grep ping) > /dev/null; do echo -n .; done
-  echo "ready!"
+    echo -n "Waiting for Zork to come up..."
+    while ! ((echo ping ; sleep 0.5) | nc -w 1 $HOST_IP 9000 | grep ping) > /dev/null; do echo -n .; done
+    echo "ready!"
 fi
 
 # Output the invitation URL.
