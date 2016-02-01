33af37df115bf5bff03a033deebbd82cc5da1cad
lrokita <lrokita@lrokita-macbookair.roam.corp.google.com>
2016-02-01 11:10:38 -0500

    Removing bugfixes file and iosrtc bugfix in context.ts

--- src/cca/app/scripts/context.ts
+++ src/cca/app/scripts/context.ts
@@ -15,13 +15,6 @@ export var ui :user_interface.UserInterface;
 export var model :ui_model.Model;
 
 chrome.runtime.getBackgroundPage((bgPage) => {
-  // Workaround for iosrtc RTCDataChannel.prototype.send(); foreground 
-  // and background ArrayBuffer need to be the same so that we can 
-  // check instanceof ArrayBuffer.
-  if ((<any>cordova).plugins.iosrtc) {
-    (<any>window).ArrayBuffer = (<any>bgPage).ArrayBuffer;
-  }
-
   ui_context = (<any>bgPage).ui_context;
   ui = new user_interface.UserInterface(core, ui_context.browserApi);
   model = ui.model;
