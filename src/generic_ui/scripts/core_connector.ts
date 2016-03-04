72e2bef196a6a94ae288d06435ec9fd1783fc29f
Lucy He <lucydhe@gmail.com>
2016-03-04 14:05:07 -0500

    More merging

--- src/generic_ui/scripts/core_connector.ts
+++ src/generic_ui/scripts/core_connector.ts
@@ -214,22 +214,12 @@ class CoreConnector implements uproxy_core_api.CoreApi {
     return this.promiseCommand(uproxy_core_api.Command.GET_INVITE_URL, data);
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2e79066406c46b49ec4f5a181603d600fb88a04e
   // TODO: this should probably take the network path, including userId
   getAllUserProfiles = (networkInfo :social.SocialNetworkInfo): Promise<social.UserProfile[]> => {
     return this.promiseCommand(uproxy_core_api.Command.GET_ALL_USER_PROFILES,
         networkInfo);
   }
 
-  // TODO: this should probably take the network path, including userId
-<<<<<<< HEAD
-=======
->>>>>>> origin/master
-=======
->>>>>>> 2e79066406c46b49ec4f5a181603d600fb88a04e
   sendEmail = (emailData :uproxy_core_api.EmailData): void => {
     this.sendCommand(uproxy_core_api.Command.SEND_EMAIL, emailData);
   }
