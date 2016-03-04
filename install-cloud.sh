f2ae5490a14bd2e784b5b3716b6e7613e58387ce
Trevor Johnston <trevj@google.com>
2016-03-04 15:18:29 -0500

    remove centos-specific changes, run docker installer if necessary

--- install-cloud.sh
+++ install-cloud.sh
@@ -11,27 +11,16 @@
 
 set -e
 
-if [ -f /etc/centos-release ]
-then 
-  yum update -y
-  yum install -y git bind-utils nmap-ncat
-  if [ ! -f /usr/bin/docker ]
-  then
-    curl -fsSL https://get.docker.com/ | sh
-    service docker start
-  elif [ -f /usr/bin/docker -a $(( $(docker --version | cut -d . -f 2) < 10 )) ]
-  then
-    echo "Installed version of docker is too old.  Please upgrade it yourself, or remove"
-    echo "it (yum erase docker) and run this script again.  If you don't have other"
-    echo "docker containers/images, removing it is fine, this script can do the rest. If"
-    echo "you do have other docker containers/images, have a look here:"
-    echo " https://github.com/docker/docker/wiki/Engine-v1.10.0-content-addressability-migration"
-    echo "first, and then upgrade docker yourself before running this script again."
-    exit 1
-  fi
-fi
+command_exists() {
+  command -v "$@" > /dev/null 2>&1
+}
 
 do_install() {
+  if ! command_exists docker
+  then
+    curl -fsSL https://get.docker.com/ | sh
+fi
+
   TMP_DIR=`mktemp -d`
   git clone --depth 1 https://github.com/uProxy/uproxy-docker.git $TMP_DIR
   cd $TMP_DIR/testing/run-scripts
