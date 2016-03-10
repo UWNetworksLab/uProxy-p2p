078c96d71b7dab95a01efb7abff0c76a466351e1
Trevor Johnston <trevj@google.com>
2016-03-10 13:01:55 -0500

    use username as comment for authorized_keys lines, so we can identify who they are for later

--- sshd/issue_invite.sh
+++ sshd/issue_invite.sh
@@ -51,12 +51,12 @@ then
   fi
   echo -n $ENCODED_KEY|base64 -d > $TMP/id_rsa
   chmod 600 $TMP/id_rsa
-  ssh-keygen -y -f $TMP/id_rsa > $TMP/id_rsa.pub
+  ssh-keygen -C "$USERNAME" -y -f $TMP/id_rsa > $TMP/id_rsa.pub
 else
   # TODO: 2048 bits makes for really long keys so we should use
   #       ecdsa when ssh2-streams supports it:
   #         https://github.com/mscdex/ssh2-streams/issues/3
-  ssh-keygen -q -t rsa -b 2048 -N '' -f $TMP/id_rsa
+  ssh-keygen -C "$USERNAME" -q -t rsa -b 2048 -N '' -f $TMP/id_rsa
 
   # TODO: Because SSH keys are already base64-encoded, re-encoding them
   #       like this is very inefficient.
