7043882b0db003745af89246babb88ffff173dc8
soycode <soycode@users.noreply.github.com>
2016-04-04 13:38:17 -0700

    simplify sshd

--- sshd/makehub/build.sh
+++
@@ -1,8 +0,0 @@
-# Need to copy .sh files rather than symlink as docker cares about build context
-cp ../issue_invite.sh .
-cp ../login.sh .
-cp ../supervisord.conf .
-
-docker build .
-
-rm issue_invite.sh login.sh supervisord.conf
