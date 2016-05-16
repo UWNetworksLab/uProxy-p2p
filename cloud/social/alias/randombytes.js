5972f112ca9ac6b8107e60e7291c5c9ed2feae09
soycode <soycode@users.noreply.github.com>
2016-05-16 11:13:11 -0700

    remove crypto.getRandomValues monkey patching

--- cloud/social/alias/randombytes.js
+++ cloud/social/alias/randombytes.js
@@ -1,15 +1,16 @@
-// Alternative to crypto-browserify's randombytes shim which
-// falls back to freedom.js core.crypto when crypto.getRandomValues is
+// Alternative to crypto-browserify's randombytes shim
+// https://github.com/crypto-browserify/randombytes
+// Falls back to freedom.js core.crypto when crypto.getRandomValues is
 // unavailable, which is currently the case in Firefox web workers.
 // TODO: post FF48 (2016-08-02) remove fill and just use built-in crypto
 
 const cryptoAvailable = typeof crypto !== 'undefined';
-var refreshBuffer, offset;
+var getBufferedRandomBytes, refreshBuffer, offset;
 if (!cryptoAvailable) {
   // Filling with freedom.js core.crypto, which requires a buffer due to async
   var rand = freedom['core.crypto'](),
-      buf,
-  refreshBuffer = function (size) {
+      buf;
+  refreshBuffer = function(size) {
     return rand.getRandomBytes(size).then(function (bytes) {
       buf = new Uint8Array(bytes);
       offset = 0;
@@ -20,8 +21,7 @@ if (!cryptoAvailable) {
   refreshBuffer(10000);  // initial randomness
   offset = 0;
 
-  crypto = {};
-  crypto.getRandomValues = function (buffer) {
+  getBufferedRandomBytes = function(buffer) {
     if (buffer.buffer) {
       buffer = buffer.buffer;
     }
@@ -29,7 +29,7 @@ if (!cryptoAvailable) {
         view = new Uint8Array(buffer),
         i;
     if (offset + size > buf.length) {
-      throw new Error("Insufficient Randomness Allocated.");
+      throw new Error('Insufficient Randomness Allocated.');
     }
     for (i = 0; i < size; i += 1) {
       view[i] = buf[offset + i];
@@ -39,15 +39,26 @@ if (!cryptoAvailable) {
 }
 
 module.exports = function(size, cb) {
-  var buffer = new Buffer(size);
   if (!cryptoAvailable && (size*100) - offset < size*10) {
     // using freedom.js core.crypto, may need to refresh buffer
     refreshBuffer(size*100);
   }
-  crypto.getRandomValues(buffer);
-  if (cb) {
-    cb(undefined, buffer);
+  if (cryptoAvailable) {
+    const buffer = new Buffer(size);
+    crypto.getRandomValues(buffer);
+    if (cb) {
+      cb(undefined, buffer);
+    } else {
+      return buffer;
+    }
   } else {
-    return buffer;
+    if (cb) {
+      rand.getRandomBytes(size).then(function(ab) {
+        cb(new Buffer(ab));
+      });
+    } else {
+      const buffer = new Buffer(size);
+      getBufferedRandomBytes(buffer);
+    }
   }
 };
