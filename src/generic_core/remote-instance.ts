4253b4126b774cfd20ea32598e891df919be8c2c
Lally Singh <lally@google.com>
2016-05-17 13:59:19 -0400

    More logging.

--- src/generic_core/remote-instance.ts
+++ src/generic_core/remote-instance.ts
@@ -217,6 +217,7 @@ import ui = ui_connector.connector;
           globals.settings.crypto) {
         return crypto.verifyDecrypt(<string>msg.data, this.publicKey)
         .then((plainText :string) => {
+          console.log("RemoteInstance.HandleSignal.verifyDecrypt(): ", msg);
           return this.handleDecryptedSignal_(
               msg.type, msg.version, JSON.parse(plainText));
         }).catch((e) => {
