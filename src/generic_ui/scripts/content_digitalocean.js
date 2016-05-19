b023d35ce99c5cc09b64a8732e20ab242b18c244
jab <jab@users.noreply.github.com>
2016-05-19 16:38:21 +0000

    overlay styles + handle promo success

--- src/generic_ui/scripts/content_digitalocean.js
+++ src/generic_ui/scripts/content_digitalocean.js
@@ -7,7 +7,7 @@
   console.log('pmntAdded:', pmntAdded);
 
   if (pageId === 'welcome' && pmntAdded) {
-    if (shouldPromptPromoCode()) {
+    if (shouldPromptPromo()) {
       document.location.href = 'https://cloud.digitalocean.com/settings/billing';
     }
   }
@@ -17,76 +17,64 @@
 
   function showOverlay() {
     var overlay = document.createElement('div');
-    overlay.id = '__uProxyOverlay';
     overlay.style.position   = 'fixed';
     overlay.style.top        = '0';
     overlay.style.left       = '0';
     overlay.style.bottom     = '0';
     overlay.style.right      = '0';
-    overlay.style.opacity    = '.97';
+    overlay.style.opacity    = '.95';
     overlay.style.background = '#12A391';
-    overlay.style.zIndex   = '10000';
+    overlay.style.zIndex     = '10000';
     overlay.style.textAlign  = 'center';
+    overlay.style.padding    = '180px 0 0';
+
+    var closeBtn = document.createElement('img');
+    closeBtn.src = chrome.extension.getURL('icons/cloud/ic_close_white_24dp.svg'); 
+    closeBtn.style.cursor = 'pointer';
+    closeBtn.style.position = 'fixed';
+    closeBtn.style.top = '15px';
+    closeBtn.style.right = '20px';
+    closeBtn.style.width = '40px';
+    closeBtn.onclick = function (evt) { overlay.style.display = 'none'; };
+    overlay.appendChild(closeBtn);
+
+    var img = document.createElement('img');
+    img.src = chrome.extension.getURL('icons/cloud/overlay_uproxy_do.svg');
+    img.style.width = '218px';
+    overlay.appendChild(img);
 
     var h1 = document.createElement('h1');
+    h1.innerText = _('Authorize uProxy to connect\u000Awith DigitalOcean');
+    h1.style.color = '#fff';
+    h1.style.fontFamily = 'Roboto';
+    h1.style.fontSize = '24px';
     h1.style.fontWeight = 'bold';
-    h1.innerText = _('Authorize uProxy to connect with DigitalOcean');
+    h1.style.lineHeight = '36px';
+    h1.style.margin = '26px auto';
+    h1.style.maxWidth = '600px';
+    h1.style.minWidth = '360px';
     overlay.appendChild(h1);
 
-    if (shouldPromptPromoCode()) {
-      var promoContainer = document.createElement('div');
-      var promoInput = document.createElement('input');
-      promoInput.style.width = '300px';
-      promoInput.placeholder = _('Have a promo code? Enter it here.');
-      var applyButton = document.createElement('button');
-      applyButton.innerText = 'Apply';
+    if (shouldPromptPromo()) {
       var DOtoken = document.querySelector('input[name="authenticity_token"]');
-      if (!DOtoken) {
+      if (DOtoken) {
+        addPromoUI();
+      } else {
         console.log('Missing DOtoken. DigitalOcean UI changed?');
-        return;
       }
-      applyButton.onclick = function (evt) {
-        var data = new FormData();
-        data.append('code', promoInput.value);
-        data.append('authenticity_token', DOtoken.value);
-        var req = new Request('/promos', {
-          method: 'post',
-          headers: {
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'x-csrf-token': DOtoken.value,
-            'x-requested-with': 'XMLHttpRequest'
-          },
-          body: data,
-          credentials: 'include'
-        });
-        fetch(req).then(function (resp) {
-          return resp.json();
-        }).then(function (data) {
-          if (typeof data !== 'object' || data.status === 'invalid') {
-            // TODO: handle failure
-            return;
-          }
-          debugger;
-          // TODO: call `setPromoCodeApplied(1)` if the submission succeeds.
-          //setPromoCodeApplied(1);
-        }).catch(function (e) {
-          debugger;
-          // TODO: handle failure
-        });
-      };
-
-      promoContainer.appendChild(promoInput);
-      promoContainer.appendChild(applyButton);
-      overlay.appendChild(promoContainer);
     }
 
     var proceedContainer = document.createElement('div');
+    proceedContainer.style.marginTop = '46px';
     var proceedLink = document.createElement('a');
-    proceedLink.style.color = '#fff';
-    proceedLink.style.fontSize = '48px';
+    proceedLink.style.backgroundColor = '#fff';
+    proceedLink.style.borderRadius = '2px';
+    proceedLink.style.color = '#155160';
+    proceedLink.style.fontFamily = 'Roboto';
+    proceedLink.style.fontSize = '16px';
     proceedLink.style.fontWeight = 'bold';
-    proceedLink.style.textDecoration = 'underline';
-    proceedLink.innerText = 'Proceed';
+    proceedLink.style.padding = '16px 36px';
+    proceedLink.innerText = _('Proceed to uProxy');
     // This url should close the tab and open the uProxy extension, which
     // instructs the user to click 'sign in' after creating the DO account.
     // Requires: https://github.com/uproxy/uproxy/tree/bemasc-autoclose
@@ -97,28 +85,113 @@
     overlay.appendChild(proceedContainer);
 
     document.body.appendChild(overlay);
+
+    function addPromoUI() {
+      var promoContainer = document.createElement('div');
+
+      var promoInput = document.createElement('input');
+      promoInput.autofocus = true;
+      promoInput.required = true;
+      promoInput.style.backgroundColor = 'transparent';
+      promoInput.style.border = '1px solid #fff';
+      promoInput.style.color = '#fff';
+      promoInput.style.fontFamily = 'Roboto';
+      promoInput.style.fontSize = '16px';
+      promoInput.style.padding = '12px';
+      promoInput.style.width = '300px';
+      promoInput.style.width = '300px';
+      promoInput.placeholder = _('Have a promo code? Enter it here.');
+      promoContainer.appendChild(promoInput);
+
+      var style = document.createElement('style');
+      style.appendChild(document.createTextNode('')); // webkit hack
+      document.head.appendChild(style);
+      try {
+        style.sheet.insertRule('::-webkit-input-placeholder { color: #fff; }', 0);
+      } catch (e) { }
+      try {
+        style.sheet.insertRule('::-moz-placeholder { color: #fff; }', 0);
+      } catch (e) { }
+
+      var applyButton = document.createElement('button');
+      applyButton.innerText = 'Apply';
+      applyButton.style.backgroundColor = '#155160';
+      applyButton.style.border = '0';
+      applyButton.style.borderRadius = '2px';
+      applyButton.style.color = '#fff';
+      applyButton.style.fontFamily = 'Roboto';
+      applyButton.style.fontSize = '14px';
+      applyButton.style.fontWeight = 'bold';
+      applyButton.style.height = '46px';
+      applyButton.style.margin = '0 0 0 12px';
+      applyButton.style.padding = '12px 32px';
+      promoContainer.appendChild(applyButton);
+
+      var resultText = document.createElement('div');
+      promoInput.style.fontFamily = 'Roboto';
+      promoInput.style.fontSize = '16px';
+      resultText.style.color = '#fff';
+      promoInput.style.margin = '16px auto';
+      resultText.innerText = ' ';  // no-break space
+      promoContainer.appendChild(resultText);
+
+      applyButton.onclick = handleApply;
+      overlay.appendChild(promoContainer);
+
+      function handleApply(evt) {
+        var body = 'code=' + encodeURIComponent(promoInput.value) +
+          '&authenticity_token=' + encodeURIComponent(DOtoken.value);
+
+        var req = new Request('/promos', {
+          method: 'post',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'x-csrf-token': DOtoken.value,
+            'x-requested-with': 'XMLHttpRequest'
+          },
+          body: body,
+          credentials: 'include'
+        });
+        fetch(req).then(function (resp) {
+          return resp.json();
+        }).then(function (data) {
+          if (typeof data !== 'object' || data.status === 'invalid') {
+            return handlePromoFailure();
+          }
+          resultText.innerText = _('Promo code accepted!');
+          promoInput.disabled = true;
+          applyButton.disabled = true;
+          promoInput.style.opacity = '0.5';
+          applyButton.style.opacity = '0.5';
+          setPromoApplied(1);
+        }).catch(function (e) {
+          handlePromoFailure();
+        });
+        function handlePromoFailure() {
+          resultText.innerText = _('Could not apply promo.');
+        }
+      }
+    }
   }
 
-  function shouldPromptPromoCode() {
-    return havePromoCode() && !isPromoCodeApplied();
+  function shouldPromptPromo() {
+    return havePromo() && !isPromoApplied();
   }
 
-  function havePromoCode() {
+  function havePromo() {
     // TODO: grab this from ui_context.model.globalSettings.promo
     return true;
   }
 
-  var _PROMO_CODE_APPLIED_KEY = '__uProxyPromoCodeApplied';
-
-  function isPromoCodeApplied() {
-    return !!localStorage[_PROMO_CODE_APPLIED_KEY];
+  function isPromoApplied() {
+    return !!localStorage['__uProxyPromoApplied'];
   }
 
-  function setPromoCodeApplied(val) {
+  function setPromoApplied(val) {
     if (val) {
-      localStorage[_PROMO_CODE_APPLIED_KEY] = 1;
+      localStorage['__uProxyPromoApplied'] = 1;
     } else {
-      delete localStorage[_PROMO_CODE_APPLIED_KEY];
+      delete localStorage['__uProxyPromoApplied'];
     }
   }
 
@@ -157,6 +230,7 @@
   }
 
   function _(s) {
+    // TODO: i18n
     return s;
   }
 
