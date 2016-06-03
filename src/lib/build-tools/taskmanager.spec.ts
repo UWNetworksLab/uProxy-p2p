0d0b00739bb4c95dc7e3ebea08314bb039e262ac
Trevor Johnston <trevj@google.com>
2016-06-03 12:10:20 -0400

    add NPMs and typings from uproxy-lib, make setup.sh work

--- src/lib/build-tools/taskmanager.spec.ts
+++ src/lib/build-tools/taskmanager.spec.ts
@@ -1,4 +1,4 @@
-/// <reference path='../../../third_party/typings/browser.d.ts' />
+/// <reference path='../third_party/typings/browser.d.ts' />
 
 import TaskManager = require('./taskmanager');
 import Manager = TaskManager.Manager;
