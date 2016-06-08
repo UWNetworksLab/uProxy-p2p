85bbab0771c2ae10f9e42d6a3740405f57048211
Trevor Johnston <trevj@google.com>
2016-06-08 13:14:32 -0400

    remove weird return statement

--- src/lib/build-tools/alias/randombytes.js
+++ src/lib/build-tools/alias/randombytes.js
@@ -8,39 +8,38 @@ const cryptoAvailable = typeof crypto !== 'undefined';
 var getBufferedRandomBytes, refreshBuffer, offset;
 if (!cryptoAvailable) {
   // Filling with freedom.js core.crypto, which requires a buffer due to async
-  var rand;
   try {
-    rand = freedom['core.crypto']();
+    const rand = freedom['core.crypto']();
+
+    var buf;
+    var refreshBuffer = function(size) {
+      return rand.getRandomBytes(size).then(function (bytes) {
+        buf = new Uint8Array(bytes);
+        offset = 0;
+      }, function (err) {
+        console.log(err);
+      });
+    }.bind(this);
+    refreshBuffer(10000);  // initial randomness
+    offset = 0;
+
+    getBufferedRandomBytes = function(buffer) {
+      if ((size * 100) - offset < size * 10) {
+        refreshBuffer(size*100);
+      }
+      var size = buffer.byteLength,
+          view = new Uint8Array(buffer),
+          i;
+      if (offset + size > buf.length) {
+        throw new Error('Insufficient Randomness Allocated.');
+      }
+      view = buf.slice(offset, offset + size);
+      offset += size;
+    };
   } catch (e) {
     console.error('could not instantiate core.crypto: ', e);
     return;
   }
-
-  var buf;
-  var refreshBuffer = function(size) {
-    return rand.getRandomBytes(size).then(function (bytes) {
-      buf = new Uint8Array(bytes);
-      offset = 0;
-    }, function (err) {
-      console.log(err);
-    });
-  }.bind(this);
-  refreshBuffer(10000);  // initial randomness
-  offset = 0;
-
-  getBufferedRandomBytes = function(buffer) {
-    if ((size * 100) - offset < size * 10) {
-      refreshBuffer(size*100);
-    }
-    var size = buffer.byteLength,
-        view = new Uint8Array(buffer),
-        i;
-    if (offset + size > buf.length) {
-      throw new Error('Insufficient Randomness Allocated.');
-    }
-    view = buf.slice(offset, offset + size);
-    offset += size;
-  };
 }
 
 module.exports = function(size, cb) {
