90ac347ac5b2ece8dfaa68b024b6adf1ccf7d8c1
Trevor Johnston <trevj@google.com>
2016-06-10 16:41:15 -0400

    move to getLogs global functions into the polymer JS, to avoid defining these twice for firefox and chrome

--- src/generic_ui/polymer/logs.ts
+++ src/generic_ui/polymer/logs.ts
@@ -4,26 +4,63 @@ import translator_module = require('../scripts/translator');
 declare var PolymerExpressions: any;
 PolymerExpressions.prototype.$$ = translator_module.i18n_t;
 
-declare var bringUproxyToFront :() => void;
-declare var getLogs :() => Promise<string>;
+const extensionId = 'pjpcdnccaekokkkeheolmpkfifcbibnj';
 
 Polymer({
   logs: '',
   loadingLogs: true,
   openUproxy: function() {
-    // TODO: add a pop-out icon that calls this function.
-    bringUproxyToFront();
+    if (window.chrome) {
+      chrome.runtime.sendMessage(extensionId, {
+        openWindow: true
+      });
+    } else {
+      window.postMessage({
+        showPanel: true,
+        data: false
+      }, '*');
+    }
   },
   created: function() {
     // Default language to English.
     var language = window.location.href.split('lang=')[1] || 'en';
-    translator_module.i18n_setLng(language.substring(0,2));
+    translator_module.i18n_setLng(language.substring(0, 2));
   },
   ready: function() {
     // Expose global ui object in this context.
-    getLogs().then((logs) => {
+
+    let fulfill: Function;
+    let reject: Function;
+    const getLogs = new Promise<string>((F, R) => {
+      fulfill = F;
+      reject = R;
+    });
+
+    if (window.chrome) {
+      chrome.runtime.sendMessage(extensionId, {
+        getLogs: true
+      }, (reply) => {
+        if (reply) {
+          fulfill(reply.logs);
+        } else {
+          reject('Could not get logs');
+        }
+      });
+    } else {
+      window.postMessage({
+        getLogs: true,
+        data: false
+      }, '*');
+      // TODO: reject after a timeout
+      window.addEventListener('message', (event) => {
+        if (event.data.logs) {
+          fulfill(event.data.logs);
+        }
+      }, false);
+    }
+
+    getLogs.then((logs) => {
       this.loadingLogs = false;
-      // Add browser info to logs.
       this.logs = 'Browser Info: ' + navigator.userAgent + '\n\n' + logs;
     });
   }
