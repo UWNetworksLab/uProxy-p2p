37f49c97324c9c1dc8d5794ac5635993139627d6
Vinicius Fortuna <fortuna@users.noreply.github.com>
2016-06-14 16:55:52 -0400

    Make hermetic grunt use setup.sh

--- tools/hermetic_grunt/entrypoint.sh
+++ tools/hermetic_grunt/entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-set -e
-
 # Clean the tscache while we figure out how to make it work across calls.
 # TODO(fortuna): Fix this.
-rm -rf $REPOSITORY_ROOT/out/.tscache
+rm -rf $REPOSITORY_ROOT/out/.tscache &&
 
 # We can't link the Gruntfile because grunt resolves the link before using its
 # directory as the base to run.
-cp $REPOSITORY_ROOT/Gruntfile.coffee .
-mkdir -p $REPOSITORY_ROOT/out/.tscache
-ln -s $REPOSITORY_ROOT/{version.py,.git,src,out/.tscache} .
+cp $REPOSITORY_ROOT/Gruntfile.coffee . &&
+mkdir -p $REPOSITORY_ROOT/out/.tscache &&
+ln -s $REPOSITORY_ROOT/{version.py,.git,src,out/.tscache} . &&
 
-grunt "$@"
+grunt "$@" ||
 
 # Copy build output to the out/ directory.
 # TODO(fortuna): Make grunt output to out/ directly.
-rm --recursive build/{third_party,tools}
-cp --archive --recursive build/* $REPOSITORY_ROOT/out/
+rm --recursive build/{third_party,tools} &&
+cp --archive --recursive build/* $REPOSITORY_ROOT/out/ &&
 chown --recursive $(stat -c "%u:%g" $REPOSITORY_ROOT) $REPOSITORY_ROOT/out
