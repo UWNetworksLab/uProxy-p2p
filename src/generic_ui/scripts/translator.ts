c6ea34c0a6d11cbe024e9ce871e184158783c5f4
Jonathan Pevarnek <jpevarnek@google.com>
2016-07-12 17:53:52 -0400

    Move the i18nSanitizeHtml function to translator
    This allows the function to be accessed even when the ui may not be in
    scope.  There was no reason why this needed to be within UserInterface.
    An export of it is left on UserInterface for legacy reasons.

--- src/generic_ui/scripts/translator.ts
+++ src/generic_ui/scripts/translator.ts
@@ -59,3 +59,8 @@ export const i18n_t = (placeholder :string, params ?:any): string => {
 };
 
 export const i18n_setLng = i18next.setLng;
+
+export const i18nSanitizeHtml = (i18nMessage: string) => {
+  // Remove all HTML other than supported tags like strong, a, p, etc.
+  return i18nMessage.replace(/<((?!(\/?(strong|a|p|br|uproxy-faq-link)))[^>]+)>/g, '');
+}
