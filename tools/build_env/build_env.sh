7087b91c6dc687c7f2a67f6d9690edc7423fc1dc
trevj <trevj@users.noreply.github.com>
2016-08-12 11:05:42 -0400

    microcontainer Docker build images (#2665)
    * use alpine linux base for docker builds
    
    * re-introduce build_env.sh helper
    
    * add some background
    
    * use readonly where possible
    
    * use double square brackets throughout
    
    * thens on same line
    
    * use arithmetic operators for arithmetic operations

--- tools/build_env/build_env.sh
+++ tools/build_env/build_env.sh
@@ -1,107 +1,57 @@
 #!/bin/bash
+
 set -eu
+
+readonly IMAGE_NAME="uproxy/build"
+BUILD=false
+PUBLISH=false
 
 function error() {
   echo "$@" >&2
 }
 
-function help() {
+function usage() {
   cat <<-EOM
-usage: $0 [<command>]
+Usage: $0 [-b] [-p] commands
+  -b (re-)build Docker image
+  -p publish Docker image to Docker Hub (you must be logged in)
 
-The <command> will fall in a few categories:
-
-Build-related
-  enter            Starts a bash session in the build environment
-  run <command>    Runs the given command in the build environment and returns
-
-Image management
-  update_image     Rebuilds the build environment image
-  publish_image    Publishes the current build environment image to Docker hub.
-                   Needs to be logged in to the Docker hub (docker login).
-  get_image_version    Outputs the name and tag of the build environment
-                   Docker image
-  set_image_version <version>    Sets the name and tag of the Docker image to be
-                   used
-
-If no command is specified, it will assume the enter command.
+Examples:
+  $0 -b
+  $0 npm run clean
+  $0 npm install
+  $0 grunt build_chrome
 EOM
+exit 1
 }
 
-function check_prerequisites() {
-  if ! which docker > /dev/null; then
-    error "You must install docker first. See https://docs.docker.com/engine/installation/"
-    return 1
-  fi
+while getopts bph? opt; do
+  case $opt in
+    b) BUILD=true ;;
+    p) PUBLISH=true ;;
+    *) usage ;;
+  esac
+done
+shift $((OPTIND-1))
 
-  if [[ ! -d .git ]]; then
-    error "You must run $(basename $0) from the repository root"
-    return 2
-  fi
-}
+if ! which docker > /dev/null; then
+  error "You must install docker first. See https://docs.docker.com/engine/installation/"
+  exit 1
+fi
 
-function get_image_version() {
-  if (( $# > 0 )); then
-    error "[get_image_version] takes no argument"
-    return 3
-  fi
-  cat $IMAGE_DIR/IMAGE_VERSION.txt
-}
+if [[ $BUILD = true ]]; then
+  readonly DOCKER_ROOT="$(dirname $0)"
+  docker build --rm -t $IMAGE_NAME $DOCKER_ROOT
+fi
 
-function set_image_version() {
-  echo "$1" > $IMAGE_DIR/IMAGE_VERSION.txt
-}
+if [[ $PUBLISH = true ]]; then
+  docker push $IMAGE_NAME
+  echo "Find the new image at https://hub.docker.com/r/$IMAGE_NAME/tags/"
+fi
 
-function update_image() {
-  local image_version=uproxy/build:$(git log --pretty=format:'%h' -n 1)
-  docker build --rm -f $IMAGE_DIR/Dockerfile -t $image_version .
-  set_image_version $image_version
-}
-
-function publish_image() {
-  docker push $(get_image_version)
-  echo "Find the new image at https://hub.docker.com/r/uproxy/build/tags/"
-}
-
-function enter() {
-  if (( $# > 0 )); then
-    error "[enter] takes no argument"
-    return 3
-  fi
-  run bash
-}
-
-function run() {
-  if (( $# == 0 )); then
-    error "[run] expects the command to be run in the build environment"
-    return 3
-  fi
-  docker run --rm \
-      -v $(pwd)/Gruntfile.coffee:/root/build_root/Gruntfile.coffee:ro \
-      -v $(pwd)/version.py:/root/build_root/version.py:ro \
-      -v $(pwd)/.git:/root/build_root/.git:ro \
-      -v $(pwd)/src:/root/build_root/src:ro \
-      -v $(pwd)/out/.tscache:/root/build_root/.tscache \
-      -v $(pwd)/out/.grunt:/root/build_root/.grunt \
-      -v $(pwd)/out/dev:/root/build_root/build/dev \
-      -v $(pwd)/out/dist:/root/build_root/build/dist \
-      -v $(pwd)/out:/root/build_root/out \
-      -it $(get_image_version) "$@"
-}
-
-function main() {
-  check_prerequisites || return
-  (
-    IMAGE_DIR="$(dirname $0)"
-    if (( $# == 0 )); then
-      enter
-    elif ! type -t $1 > /dev/null; then
-      error -e "Invalid command $1\n"
-      help
-    else
-      "$@"
-    fi
-  )
-}
-
-main "$@"
+if (( $# > 0 )); then
+  readonly GIT_ROOT=`git rev-parse --show-toplevel`
+  docker run --rm -ti -v "$GIT_ROOT":/worker -w /worker $IMAGE_NAME "$@"
+  # TODO: Don't spin up a second container just to chown.
+  docker run --rm -ti -v "$GIT_ROOT":/worker -w /worker $IMAGE_NAME chown -R $(stat -c "%u:%g" .git) /worker
+fi
