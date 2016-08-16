fa1ecfe24b63aeb83efc1e590c2b248919b71952
Lucy He <lucyhe@google.com>
2016-08-16 17:29:37 -0400

    Fix spacing for typescript types

--- src/generic_ui/polymer/i18n-filter.ts
+++ src/generic_ui/polymer/i18n-filter.ts
@@ -4,6 +4,6 @@ import translator = require('../scripts/translator');
 var i18n_t = translator.i18n_t;
 
 declare var PolymerExpressions: any;
-PolymerExpressions.prototype.$$ = (placeholder :string, language :string, params?: any) :string => {
+PolymerExpressions.prototype.$$ = (placeholder: string, language: string, params?: any) :string => {
   return i18n_t(placeholder, params);
 }
