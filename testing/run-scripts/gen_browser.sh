ef9bfe5fd089b294d7af674cba1769f7b378235c
trevj <trevj@users.noreply.github.com>
2016-08-16 13:09:21 -0400

    use unbranded firefox builds (#99)
    * move to unbranded firefox builds

--- testing/run-scripts/gen_browser.sh
+++ testing/run-scripts/gen_browser.sh
@@ -71,33 +71,58 @@ RUN npm install -g jpm
 
 # Firefox dependencies (apt-get install -f handles this for Chrome).
 RUN apt-get -qq install libasound2 libdbus-glib-1-2 libgtk2.0.0 libgtk-3-0
-EOF
-  case $1 in
-    stable)
-      cat <<EOF
-RUN cd /tmp ; mkdir ff ; cd ff ; wget -O firefox-stable.tar.bz2 'https://download.mozilla.org/?product=firefox-latest&os=linux64'
-EOF
-            ;;
-        beta)
-            cat <<EOF
-RUN cd /tmp ; mkdir ff ; cd ff ; wget -O firefox-beta.tar.bz2 'https://download.mozilla.org/?product=firefox-beta-latest&os=linux64'
-EOF
-      ;;
-    canary)
-      cat <<EOF
-RUN cd /tmp ; mkdir ff ; cd ff ; wget -r -l1 -nd -A '*linux-x86_64.tar.bz2' https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/
-EOF
-      ;;
-    *)
-      log "Unknown firefox version $1. Options are stable, beta, and canary."
-      ;;
-  esac
-  cat <<EOF
-  # Sometimes there are >1 versions in the folder, e.g. following a release.
-RUN cd /usr/share ; ls /tmp/ff/*.bz2|sort|tail -1|xargs tar xf
-RUN ln -s /usr/share/firefox/firefox /usr/bin/firefox
+
 EOF
 
+  # From Firefox 48 onwards, jpm only works with *unbranded Firefox builds*.
+  # These are available for Firefox stable and beta.
+  #
+  # At the time of writing (August 2016), there's *very little* documentation
+  # online about these builds. Even though add-on signing has been a
+  # multi-year project, jpm's documentation barely mentions the unbranded
+  # builds - this is the only relevant doc I could find:
+  #   https://wiki.mozilla.org/Add-ons/Extension_Signing#Unbranded_Builds
+  #
+  # From examining the folders linked to in that page it's clear that the
+  # unbranded builds are built nightly. However, in the absence of a useful
+  # symlink such as "latest", we have to do a bunch of work to find the latest
+  # build.
+  #
+  # Note: We *could* use the regular Firefox builds with the help of jpm sign.
+  # However, since that would require us to create, manage, and somehow include
+  # in the build API keys for a round-trip to addons.mozilla.org, we instead
+  # use the unbranded builds.
+
+  # The builds are stored in one of two directories:
+  #   http://archive.mozilla.org/pub/firefox/tinderbox-builds/mozilla-release-linux64-add-on-devel/
+  #   http://archive.mozilla.org/pub/firefox/tinderbox-builds/mozilla-beta-linux64-add-on-devel/
+  case $1 in
+    stable)
+      readonly NIGHTLY_TAG=release
+      ;;
+    beta)
+       readonly NIGHTLY_TAG=beta
+      ;;
+    *)
+      log "Unknown firefox version $1. Options are stable and beta."
+      ;;
+  esac
+  readonly NIGHTLY_TOP_LEVEL=http://archive.mozilla.org/pub/firefox/tinderbox-builds/mozilla-$NIGHTLY_TAG-linux64-add-on-devel/
+
+  # Inside this directory we'll find a bunch of directories, each containing a
+  # nightly build. We want to find the *last*. This will be the *most recent*.
+  # In the absence of an FTP server, we use wget to mirror that directory's
+  # immediate children so that we can figure out the latest build's directory.
+  # Note that this can be slow when there are a lot of nightly builds.
+  readonly TMP=$(mktemp -d)
+  wget -r -np -l 1 -nH --cut-dirs=4 -P $TMP $NIGHTLY_TOP_LEVEL
+  readonly LATEST_NIGHTLY=$(basename $(find $TMP -mindepth 1 -maxdepth 1 -type d|sort|tail -1))
+
+  cat <<EOF
+RUN cd /tmp ; mkdir ff ; cd ff ; wget -r -l1 -nd -A '*add-on-devel.tar.bz2' $NIGHTLY_TOP_LEVEL/$LATEST_NIGHTLY/
+RUN cd /usr/share ; tar xf /tmp/ff/*add-on-devel.tar.bz2
+RUN ln -s /usr/share/firefox/firefox /usr/bin/firefox
+EOF
 }
 
 function get_node () {
