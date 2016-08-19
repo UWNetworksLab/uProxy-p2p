10a27f1f0926820034c6d17833a80512f8746297
Shilpa Kumar <shilpakumar@shilpakumar-macbookpro2.roam.corp.google.com>
2016-08-19 14:45:15 -0400

    comment in bandwidth-config

--- src/lib/rtc-to-net/bandwidth-config.ts
+++ src/lib/rtc-to-net/bandwidth-config.ts
@@ -1,6 +1,8 @@
 import uproxy_core_api = require('../../interfaces/uproxy_core_api');
 
 interface BandwidthConfig {
+  // Includes a number for the limit, as well as a boolean for whether
+  // or not the bandwidth should be limited.
   settings ?: uproxy_core_api.bandwidthSettings;
 }
 
