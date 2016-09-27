62e09b6d3f787c898e172799946f335b574567dc
Daniel Borkan <dborkan@dborkan-macbookpro2.roam.corp.google.com>
2016-09-27 18:33:42 -0400

    Update permissions

--- scraps/logs-server.py
+++ scraps/logs-server.py
@@ -3,22 +3,20 @@ import BaseHTTPServer
 import sys
 
 restricted_file = ''  # TODO: don't use global
-access_token = ''  # TODO: don't use global
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
   def end_headers(self):
     self.send_header('Access-Control-Allow-Origin', '*')
     SimpleHTTPRequestHandler.end_headers(self)
   def do_GET(self):
-    if self.path == restricted_file + '/accessToken=' + access_token:
+    if self.path == restricted_file:
       return SimpleHTTPRequestHandler.do_GET(self)
     else:
       return self.wfile.write('cannot access path %s' % (self.path))
 
 if __name__ == '__main__':
   if len(sys.argv) != 4:
-    print 'Usage: python logs-server.py <PORT> <FILE> <ACCESS_TOKEN>'
+    print 'Usage: python logs-server.py <PORT> <FILE>'
   else:
     restricted_file = sys.argv[2]
-    access_token = sys.argv[3]
     BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
