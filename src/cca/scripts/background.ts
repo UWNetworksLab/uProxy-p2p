34b9595ca45ed4a5127e73962f014e67b51e77db
Vinicius Fortuna <fortuna@users.noreply.github.com>
2016-11-09 18:20:30 -0500

    Fix url interception

--- src/cca/scripts/background.ts
+++ src/cca/scripts/background.ts
@@ -16,28 +16,13 @@ import { UproxyServerRepository } from './uproxy_server';
 import { MakeCoreConnector } from './cordova_core_connector';
 import { GetGlobalTun2SocksVpnDevice } from './tun2socks_vpn_device';
 import * as vpn_device from '../model/vpn_device';
+import * as intents from './intents';
 
 declare const freedom: freedom.FreedomInCoreEnv;
 
 export interface OnEmitModule extends freedom.OnAndEmit<any,any> {};
 export interface OnEmitModuleFactory extends
   freedom.FreedomModuleFactoryManager<OnEmitModule> {};
-
-let intentUrl = new Promise((resolve, reject) => {
-  window.top.document.addEventListener('deviceready', () => {
-    if (!window.top.webintent) {
-      reject('windomw.top.webintent not found (Not running on Android?)');
-      return;
-    }
-    window.top.webintent.getUri((url: string) => {
-      resolve(url);
-    });
-  });
-});
-
-intentUrl.then((url) => {
-  console.debug(`[Background] Url is [${url}]`);
-});
 
 console.log('Loading core');
 export var uProxyAppChannel = freedom(
@@ -67,18 +52,24 @@ let serversPromise = GetGlobalTun2SocksVpnDevice().then((vpnDevice) => {
   return new UproxyServerRepository(core, vpnDevice);
 });
 
-chrome.app.runtime.onLaunched.addListener(function () {
-  console.debug('Chrome onLaunched fired');
-  chrome.app.window.create('index.html', null, (appWindow) => {
-    let document = appWindow.contentWindow.document;
-    document.addEventListener('DOMContentLoaded', function (event) {
-      serversPromise.then((servers) => {
-        let serverListPage = new ServerListPage(appWindow.contentWindow.document.body, servers);
-        intentUrl.then((url: string) => {
-          console.debug(`[App] Url: ${url}`);
-          serverListPage.enterAccessCode(url);
+let serversListPagePromise: Promise<ServerListPage> = new Promise((resolve, reject) => {
+  chrome.app.runtime.onLaunched.addListener(() => {
+    console.debug('Chrome onLaunched fired');
+    chrome.app.window.create('index.html', null, (appWindow) => {
+      let document = appWindow.contentWindow.document;
+      document.addEventListener('DOMContentLoaded', function (event) {
+        serversPromise.then((servers) => {
+          resolve(new ServerListPage(appWindow.contentWindow.document.body, servers));
         });
       });
     });
+  });
+});
+
+Promise.all([serversListPagePromise, intents.GetGlobalIntentInterceptor()])
+.then(([serverListPage, intentInterceptor]) => {
+  intentInterceptor.addIntentListener((url: string) => {
+    console.debug(`[App] Url: ${url}`);
+    serverListPage.enterAccessCode(url);
   });
 });
