e2f6506e945b62bc84fc267fd377deb3c180a1d2
trevj <trevj@users.noreply.github.com>
2016-11-16 13:06:40 -0500

    use socat for flood server to avoid weird issues with ncat on docker, move to alpine for smaller image (#106)

--- flood/flood.sh
+++ flood/flood.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-###
-# DO NOT CALL THIS SCRIPT DIRECTLY -- see testing/run-scripts/flood.sh
-###
-
-ncat -l -k -p 1224 -c "dd if=/dev/zero count=1 bs=$1 status=none"
+#!/bin/sh
+socat -U TCP-LISTEN:1224,reuseaddr,fork EXEC:"dd if=/dev/zero bs=$1 count=1"
